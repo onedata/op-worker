0d208e2e6a49a20082e881b54d15129e1c895d87
Rafal Slota <roxeon@gmail.com>
2013-03-14 16:56:54 +0100

    VFS-44: Reformat kodu

--- src/veil_modules/dao/dao_json.erl
+++ src/veil_modules/dao/dao_json.erl
@@ -1,7 +1,7 @@
 %% ===================================================================
 %% @author Rafal Slota
 %% @copyright (C): 2013 ACK CYFRONET AGH
-%% This software is released under the MIT license 
+%% This software is released under the MIT license
 %% cited in 'LICENSE.txt'.
 %% @end
 %% ===================================================================
@@ -62,10 +62,10 @@ mk_bin(Term) ->
 %% ====================================================================
 %% @doc Inserts new field into given document or JSON object
 -spec mk_field(DocOrObj, Name :: string(), Value :: term()) -> DocOrObj when
-    DocOrObj :: #doc{} | json_object().
+      DocOrObj :: #doc{} | json_object().
 %% ====================================================================
-mk_field(Doc=#doc{body=Body}, Name, Value) ->
-    Doc#doc{body=mk_field(Body, Name, Value)};
+mk_field(Doc = #doc{body = Body}, Name, Value) ->
+    Doc#doc{body = mk_field(Body, Name, Value)};
 mk_field({Fields}, Name, Value) ->
     {NewFields} = rm_field({Fields}, Name),
     {[{name(Name), Value} | NewFields]}.
@@ -75,10 +75,10 @@ mk_field({Fields}, Name, Value) ->
 %% ====================================================================
 %% @doc Inserts new fields into given document or JSON object
 -spec mk_fields(DocOrObj, [Names :: string()], [Values :: term()]) -> DocOrObj when
-    DocOrObj :: #doc{} | json_object().
+      DocOrObj :: #doc{} | json_object().
 %% ====================================================================
-mk_fields(Doc=#doc{body=Body}, Names, Values) when is_list(Names), is_list(Values) ->
-    Doc#doc{body=mk_fields(Body, Names, Values)};
+mk_fields(Doc = #doc{body = Body}, Names, Values) when is_list(Names), is_list(Values) ->
+    Doc#doc{body = mk_fields(Body, Names, Values)};
 mk_fields({Fields}, Names, Values) when is_list(Names), is_list(Values) ->
     {NewFields} = rm_fields({Fields}, Names),
     {[{name(X), Y} || {X, Y} <- lists:zip(Names, Values)] ++ NewFields}.
@@ -88,34 +88,34 @@ mk_fields({Fields}, Names, Values) when is_list(Names), is_list(Values) ->
 %% ====================================================================
 %% @doc Removes field from given document or JSON object
 -spec rm_field(DocOrObj, Name :: string()) -> DocOrObj when
-    DocOrObj :: #doc{} | json_object().
+      DocOrObj :: #doc{} | json_object().
 %% ====================================================================
-rm_field(Doc=#doc{body=Body}, Name) ->
-    Doc#doc{body=rm_field(Body, Name)};
+rm_field(Doc = #doc{body = Body}, Name) ->
+    Doc#doc{body = rm_field(Body, Name)};
 rm_field({Fields}, Name) ->
-    {[X || {FName, _}=X <- Fields, name(FName) =/= name(Name)]}.
+    {[X || {FName, _} = X <- Fields, name(FName) =/= name(Name)]}.
 
 
 %% rm_fields/2
 %% ====================================================================
 %% @doc Removes fields from given document or JSON object
 -spec rm_fields(DocOrObj, [Name :: string()]) -> DocOrObj when
-    DocOrObj :: #doc{} | json_object().
+      DocOrObj :: #doc{} | json_object().
 %% ====================================================================
-rm_fields(Doc=#doc{body=Body}, Names) when is_list(Names) ->
-    Doc#doc{body=rm_fields(Body, Names)};
+rm_fields(Doc = #doc{body = Body}, Names) when is_list(Names) ->
+    Doc#doc{body = rm_fields(Body, Names)};
 rm_fields({Fields}, Names) when is_list(Names) ->
     Names1 = [name(X) || X <- Names],
-    {[X || {FName, _}=X <- Fields, not lists:member(name(FName), Names1)]}.
+    {[X || {FName, _} = X <- Fields, not lists:member(name(FName), Names1)]}.
 
 
 %% get_field/2
 %% ====================================================================
 %% @doc Returns field's value from given document or JSON object
 -spec get_field(DocOrObj, Name :: string()) -> any() | {error, not_found} | {error, invalid_object} when
-    DocOrObj :: #doc{} | json_object().
+      DocOrObj :: #doc{} | json_object().
 %% ====================================================================
-get_field(_Doc=#doc{body=Body}, Name) ->
+get_field(_Doc = #doc{body = Body}, Name) ->
     get_field(Body, Name);
 get_field({Fields}, Name) ->
     case [Value || {X, Value} <- Fields, name(X) =:= name(Name)] of
@@ -124,8 +124,8 @@ get_field({Fields}, Name) ->
         _ -> {error, invalid_object}
     end.
 
-    
+
 %% ===================================================================
 %% Internal functions
 %% ===================================================================
-    
+
--- test/veil_modules/dao/dao_json_tests.erl
+++ test/veil_modules/dao/dao_json_tests.erl
@@ -1,7 +1,7 @@
 %% ===================================================================
 %% @author Rafal Slota
 %% @copyright (C): 2013 ACK CYFRONET AGH
-%% This software is released under the MIT license 
+%% This software is released under the MIT license
 %% cited in 'LICENSE.txt'.
 %% @end
 %% ===================================================================
@@ -44,19 +44,19 @@ mk_field_test() ->
     Obj4 = #doc{id = <<"Test">>, body = {[{<<"Int">>, 1}]}}.
 
 mk_fields_test() ->
-    Obj1 = dao_json:mk_fields(dao_json:mk_doc("Test"), ["Int", "Bool", "List"], [1, true, [1,2,3]]),
-    Obj1 = #doc{id = <<"Test">>, body = {[{<<"Int">>, 1}, {<<"Bool">>, true}, {<<"List">>, [1,2,3]}]}}.
+    Obj1 = dao_json:mk_fields(dao_json:mk_doc("Test"), ["Int", "Bool", "List"], [1, true, [1, 2, 3]]),
+    Obj1 = #doc{id = <<"Test">>, body = {[{<<"Int">>, 1}, {<<"Bool">>, true}, {<<"List">>, [1, 2, 3]}]}}.
 
 rm_field_test() ->
-    Doc1 = dao_json:rm_field(#doc{body={[{<<"test">>, 1}, {<<"test2">>, 1}, {<<"test2">>, 1}]}}, "test2"),
-    Doc1 = #doc{body={[{<<"test">>, 1}]}}.
+    Doc1 = dao_json:rm_field(#doc{body = {[{<<"test">>, 1}, {<<"test2">>, 1}, {<<"test2">>, 1}]}}, "test2"),
+    Doc1 = #doc{body = {[{<<"test">>, 1}]}}.
 
 rm_fields_test() ->
-    Doc1 = dao_json:rm_fields(#doc{body={[{<<"test">>, 1}, {<<"test2">>, 1}, {<<"test2">>, 1},{<<"test3">>, 1}]}}, ["test2", "test3"]),
-    Doc1 = #doc{body={[{<<"test">>, 1}]}}.
+    Doc1 = dao_json:rm_fields(#doc{body = {[{<<"test">>, 1}, {<<"test2">>, 1}, {<<"test2">>, 1}, {<<"test3">>, 1}]}}, ["test2", "test3"]),
+    Doc1 = #doc{body = {[{<<"test">>, 1}]}}.
 
 get_field_test() ->
-    4 = dao_json:get_field(#doc{body={[{<<"test">>, 1}, {<<"test2">>, 2}, {<<"test2">>, 3},{<<"test3">>, 4}]}}, "test3").
+    4 = dao_json:get_field(#doc{body = {[{<<"test">>, 1}, {<<"test2">>, 2}, {<<"test2">>, 3}, {<<"test3">>, 4}]}}, "test3").
 
 
 -endif.
