b6d9a97947b6c4d43b49e82d7cc1ab99a500e120
plglopiola <lukasz.opiola@gmail.com>
2013-05-07 17:31:18 +0200

    VFS-49: Integracja CMT w najprostszym mozliwym wydaniu. Strona (na razie pusta) dostepna na porcie :8000 node'a, na którym działa control_panel.

---
+++ src/veil_modules/control_panel/nitrogen_handler.erl
@@ -0,0 +1,73 @@
+%% ===================================================================
+%% @author Lukasz Opiola
+%% @copyright (C): 2013 ACK CYFRONET AGH
+%% This software is released under the MIT license
+%% cited in 'LICENSE.txt'.
+%% @end
+%% ===================================================================
+%% @doc: This is the callback module for cowboy to handle requests
+%% by passing them to the nitrogen engine
+%% @end
+%% ===================================================================
+
+-module(nitrogen_handler).
+-include_lib("nitrogen_core/include/wf.hrl").
+
+-export([init/3, handle/2, terminate/3]).
+
+-record(state, {headers, body}).
+
+%% init/3
+%% ====================================================================
+%% @doc Initializes a request-response procedure
+-spec init(Protocol, Request :: term(), Options :: term()) -> Result when
+  Protocol :: {Transport :: term(), http},
+  Result :: {ok, Request :: term(), #state{}}.
+%% ====================================================================
+init({Transport, http}, Req, Opts) ->
+    io:format("~p~n~n~n", [Transport]),
+    Headers = proplists:get_value(headers, Opts, []),
+    Body = proplists:get_value(body, Opts, "http_handler"),
+    {ok, Req, #state{headers=Headers, body=Body}}.
+
+
+%% handle/2
+%% ====================================================================
+%% @doc Handles a request producing a response with use of Nitrogen engine
+-spec handle(Request, Options) -> Result when
+  Result :: {ok, Response, Options},
+  Request :: term(),
+  Options :: term(),
+  Response :: term().
+%% ====================================================================
+handle(Req, _Opts) ->
+    DocRoot = "./site",
+    RequestBridge = simple_bridge:make_request(cowboy_request_bridge,
+                                               {Req, DocRoot}),
+
+    %% Because Cowboy uses the same "Req" record, we can pass the
+    %% previously made RequestBridge to make_response, and it'll
+    %% parse out the relevant bits to keep both parts (request and 
+    %% response) using the same "Req"
+    ResponseBridge = simple_bridge:make_response(cowboy_response_bridge,
+                                                 RequestBridge),
+
+    %% Establishes the context with the Request and Response Bridges
+    nitrogen:init_request(RequestBridge, ResponseBridge),
+    
+    {ok, NewReq} = nitrogen:run(),
+
+    %% This will be returned back to cowboy
+    {ok, NewReq, _Opts}.
+
+%% terminate/3
+%% ====================================================================
+%% @doc Cowboy handler callback
+-spec terminate(Reason, Request, State) -> Result when
+  Result :: ok,
+  Reason :: term(),
+  Request :: term(),
+  State :: term().
+%% ====================================================================
+terminate(_Reason, _Req, _State) ->
+  ok.
