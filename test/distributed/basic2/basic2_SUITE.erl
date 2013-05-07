1f58b00568ee43f4f30049b9cae870fe79d0038b
Michał Wrzeszcz <wrzeszcz@agh.edu.pl>
2013-05-07 22:22:01 +0200

    VFS-67, ct tests

---
+++ test/distributed/basic2/basic2_SUITE.erl
@@ -0,0 +1,15 @@
+-module(basic2_SUITE).
+-include_lib("common_test/include/ct.hrl").
+-export([all/0]).
+-export([test1/1, test2/1, test3/1]).
+
+all() -> [test1,test2,test3].
+
+test1(_Config) ->
+  1 == 1.
+
+test2(_Config) ->
+  1 == 1.
+
+test3(_Config) ->
+  1 == 1.
---
+++ test/distributed/dist.spec
@@ -0,0 +1,2 @@
+{alias, basic2, "./basic2"}.
+{suites, basic2, all}.
