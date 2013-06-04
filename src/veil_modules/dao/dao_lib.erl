4b3524ca2935dc6063820bcc750f4ec5b2d57a22
Rafał Słota <roxeon@RoXeon-Desktop.lan>
2013-06-04 14:37:50 +0200

    Revert "VFS-82: worker_host proxy call dla cast'ow"
    This reverts commit a4c9dcee00ce9fd492172e92d3af2a7970f32228.

--- src/veil_modules/dao/dao_lib.erl
+++ src/veil_modules/dao/dao_lib.erl
@@ -92,18 +92,16 @@ apply(_Module, _Method, _Args, _ProtocolVersion, _Timeout, []) ->
     {error, no_active_workers_found};
 apply(Module, {asynch, Method}, Args, ProtocolVersion, Timeout, Workers) ->
     {Head, [ Worker | Tail ]} = lists:split(crypto:rand_uniform(0, length(Workers)), Workers), %% Complexity O(length(Workers))
-    try gen_server:call({dao, Worker}, {asynch, ProtocolVersion, {Module, Method, Args}}) of
+    try gen_server:cast({dao, Worker}, {asynch, ProtocolVersion, {Module, Method, Args}}) of
         _ -> ok
     catch
-        Type:Error ->
-            lager:error("Cannot call worker ~p on node ~p. Reason: ~p", [dao, Worker, {Type, Error}]),
-            apply(Module, {synch, Method}, Args, ProtocolVersion, Timeout, lists:append(Head, Tail))
+        _:_ -> apply(Module, {asynch, Method}, Args, ProtocolVersion, Timeout, lists:append(Head, Tail)) %% Complexity O(length(Workers))
     end;
 apply(Module, {synch, Method}, Args, ProtocolVersion, Timeout, Workers) ->
     PPid = self(),
-    {Head, [ Worker | Tail ]} = lists:split(crypto:rand_uniform(0, length(Workers)), Workers), %% Complexity O(length(Workers))
+    {Head, [ Worker | Tail ]} = lists:split(crypto:rand_uniform(0, length(Workers)), Workers),
     Pid = spawn(fun() -> receive Response -> PPid ! {self(), Response} after Timeout -> exit end end),
-    try gen_server:call({dao, Worker}, {synch, ProtocolVersion, {Module, Method, Args}, dao_lib_call, {proc, Pid}}) of
+    try gen_server:cast({dao, Worker}, {synch, ProtocolVersion, {Module, Method, Args}, dao_lib_call, {proc, Pid}}) of
         _ ->
             receive
                 {Pid, Resp} -> Resp
@@ -111,9 +109,7 @@ apply(Module, {synch, Method}, Args, ProtocolVersion, Timeout, Workers) ->
                 {error, timeout}
             end
     catch
-        Type:Error ->
-            lager:error("Cannot call worker ~p on node ~p. Reason: ~p", [dao, Worker, {Type, Error}]),
-            apply(Module, {synch, Method}, Args, ProtocolVersion, Timeout, lists:append(Head, Tail))
+        _ -> apply(Module, {synch, Method}, Args, ProtocolVersion, Timeout, lists:append(Head, Tail))
     end;
 apply(Module, Method, Args, ProtocolVersion, Timeout, Workers) ->
     apply(Module, {synch, Method}, Args, ProtocolVersion, Timeout, Workers).
