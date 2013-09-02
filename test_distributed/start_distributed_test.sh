23aeaff0039061213f6aa8d8b4b041ab9c8200dd
Łukasz Dutka <lukasz.dutka@cyfronet.pl>
2013-09-02 14:34:49 +0200

    Merge pull request #77 in VFS/veilcluster from VFS-95 to master
    Squashed commit of the following:
    
    commit 037e8be906fe765273e378c7e250184cee7dc0e9
    Author: Michał Wrzeszcz <michalwrzeszcz@gmail.com>
    Date:   Sun Sep 1 19:45:22 2013 +0200
    
        VFS-95, output of distributed tests filtered

--- test_distributed/start_distributed_test.sh
+++ test_distributed/start_distributed_test.sh
@@ -6,47 +6,7 @@
 ## This software is released under the MIT license
 ## cited in 'LICENSE.txt'.
 ## ===================================================================
-## @doc: This script starts distributed tests.
+## @doc: This script starts distributed tests and parse output.
 ## ===================================================================
 
-mkdir -p distributed_tests_out
-cp -R test_distributed/* distributed_tests_out
-cd distributed_tests_out
-cp -R ../cacerts .
-cp -R ../c_lib/ .
-cp -R ../src/veil_modules/dao/views .
-
-TESTS=$(find . -name "*.spec")
-erl -make
-
-IFCONFIG_LINE=`ifconfig | grep "inet addr:.*Bcast:"`
-COLON_INDEX=`awk -v a="$IFCONFIG_LINE" -v b=":" 'BEGIN{print index(a, b)}'`
-BCAST_INDEX=`awk -v a="$IFCONFIG_LINE" -v b="Bcast" 'BEGIN{print index(a, b)}'`
-COOKIE=${IFCONFIG_LINE:COLON_INDEX:((BCAST_INDEX - COLON_INDEX - 3))}
-
-for TEST in $TESTS
-do
-    echo "Test: " $TEST
-    if [[ "`cat $TEST` | grep cth_surefire" != "" ]]; then
-        echo "" >> $TEST
-        TEST_NAME=`basename "$TEST" ".spec"`
-        echo "{ct_hooks, [{cth_surefire, [{path,\"TEST-$TEST_NAME-report.xml\"}]}]}." >> $TEST
-    fi
-    ct_run -noshell -name tester -setcookie $COOKIE -spec  $TEST
-done
-
-find . -name "*.beam" -exec rm -rf {} \;
-find . -name "*.erl" -exec rm -rf {} \;
-find . -name "*.hrl" -exec rm -rf {} \;
-
-for TEST in $TESTS
-do
-    rm -f $TEST
-done
-rm -f Emakefile
-rm -f start_distributed_test.sh
-rm -rf cacerts
-rm -rf c_lib
-rm -rf views
-
-cd ..
+./test_distributed/verbose_distributed_test.sh | ./test_distributed/filter_output.sh
---
+++ test_distributed/verbose_distributed_test.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+## ===================================================================
+## @author Michal Wrzeszcz
+## @copyright (C): 2013 ACK CYFRONET AGH
+## This software is released under the MIT license
+## cited in 'LICENSE.txt'.
+## ===================================================================
+## @doc: This script starts distributed tests.
+## ===================================================================
+
+mkdir -p distributed_tests_out
+cp -R test_distributed/* distributed_tests_out
+cd distributed_tests_out
+cp -R ../cacerts .
+cp -R ../c_lib/ .
+cp -R ../src/veil_modules/dao/views .
+
+TESTS=$(find . -name "*.spec")
+erl -make
+
+IFCONFIG_LINE=`ifconfig | grep "inet addr:.*Bcast:"`
+COLON_INDEX=`awk -v a="$IFCONFIG_LINE" -v b=":" 'BEGIN{print index(a, b)}'`
+BCAST_INDEX=`awk -v a="$IFCONFIG_LINE" -v b="Bcast" 'BEGIN{print index(a, b)}'`
+COOKIE=${IFCONFIG_LINE:COLON_INDEX:((BCAST_INDEX - COLON_INDEX - 3))}
+
+for TEST in $TESTS
+do
+    if [[ "`cat $TEST` | grep cth_surefire" != "" ]]; then
+        echo "" >> $TEST
+        TEST_NAME=`basename "$TEST" ".spec"`
+        echo "{ct_hooks, [{cth_surefire, [{path,\"TEST-$TEST_NAME-report.xml\"}]}]}." >> $TEST
+    fi
+    ct_run -noshell -name tester -setcookie $COOKIE -spec  $TEST
+done
+
+find . -name "*.beam" -exec rm -rf {} \;
+find . -name "*.erl" -exec rm -rf {} \;
+find . -name "*.hrl" -exec rm -rf {} \;
+
+for TEST in $TESTS
+do
+    rm -f $TEST
+done
+rm -f Emakefile
+rm -f start_distributed_test.sh
+rm -rf cacerts
+rm -rf c_lib
+rm -rf views
+
+cd ..
