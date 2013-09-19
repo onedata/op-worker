5cdfa05463f17c78c73cd2499e619927ea355c8e
Darin NIkolow <darin@agh.edu.pl>
2013-09-19 15:02:32 +0200

    Merge pull request #5 in VFS/veilhelpers from VFS-152 to master
    Squashed commit of the following:
    
    commit 38d42ba96e37e129761cadaf3fe2476863b5db27
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Thu Sep 12 13:21:00 2013 +0200
    
        VFS-152: set fd
    
    commit 96c66b06f1d3cf13298d12b63bc463f2413ea6c7
    Merge: 39aebb9 0dd1430
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Tue Sep 3 17:50:40 2013 +0200
    
        Merge branch 'master' into VFS-145
    
        Conflicts:
        	CMakeLists.txt
    
    commit 39aebb97549843b791f9cc6dded76dff6c537e89
    Merge: 27e1632 0481ccf
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Tue Sep 3 17:47:33 2013 +0200
    
        Merge branch 'VFS-145' of ssh://git.plgrid.pl:7999/vfs/veilhelpers into VFS-145
    
    commit 27e16328f6db61054e427a1bc138869723467ca5
    Merge: a77a809 343052a
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Tue Sep 3 17:46:59 2013 +0200
    
        Merge branch 'master' into VFS-145
    
        Conflicts:
        	CMakeLists.txt
        	README.md
    
    commit 0481ccfa622c9d073f4ab1ccdd75feb5c526b4b1
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Sat Aug 31 19:45:33 2013 +0200
    
        exclude tests from docs
    
    commit ec1be239f2968ebf7ddaf55a02b33e7fca3928ff
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Sat Aug 31 19:33:18 2013 +0200
    
        VFS-145: doc moved
    
    commit ab72e13cac512d43063fe703b1741839339db4a8
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Fri Aug 30 23:32:53 2013 +0200
    
        VFS-145: build also shared lib
    
    commit 404d626245d6240298788a3c81ad23d44ba70aa6
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Fri Aug 30 20:09:28 2013 +0200
    
        VFS-145: fd fix
    
    commit 3a54531a46ea8259d862fb541772cf2021ceece1
    Merge: a77a809 343052a
    Author: root <root@172.16.67.215>
    Date:   Fri Aug 30 15:02:01 2013 +0200
    
        Merge branch 'master' into VFS-145
    
    commit a77a809c76a5113f7f9f27a833f2f1a3965837ec
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Fri Aug 30 13:57:40 2013 +0200
    
        VFS-145: Makefile added
    
    commit f9139661fc2d5ef8e5b3ffd3b9b419d3cca471da
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Fri Aug 30 13:41:17 2013 +0200
    
        VFS-145: readme update
    
    commit 2a6ad284812595c9faf25856a9555db52cddc5d3
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Fri Aug 30 13:39:34 2013 +0200
    
        VFS-145: readme update
    
    commit 79a9879039a789f3090f30a0ca86cd1ed07c064d
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Fri Aug 30 13:31:32 2013 +0200
    
        VFS-145: test moved to veilhelpers
    
    commit aadfb15f3b60f901f7ffa1fab6f53e43177100e4
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Fri Aug 30 12:33:58 2013 +0200
    
        VFS-145: VeilHelpers init commit

--- src/directIOHelper.cc
+++ src/directIOHelper.cc
@@ -39,7 +39,7 @@ static int root_path_len;
 
 static void free_path(const char *path)
 {
-	free((void*) path);
+    free((void*) path);
 }
 
 const char * get_full_path(const char *path)
@@ -57,405 +57,411 @@ const char * get_full_path(const char *path)
 
 int DirectIOHelper::sh_getattr(const char *path, struct stat *stbuf)
 {
-	int res;
-	path = get_full_path(path);
-	res = lstat(path, stbuf);
-	free_path(path);
-	if (res == -1)
-		return -errno;
-	return 0;
+    int res;
+    path = get_full_path(path);
+    res = lstat(path, stbuf);
+    free_path(path);
+    if (res == -1)
+        return -errno;
+    return 0;
 }
 
 int DirectIOHelper::sh_access(const char *path, int mask)
 {
-	int res;
-	path = get_full_path(path);
+    int res;
+    path = get_full_path(path);
 
-	res = access(path, mask);
-	free_path(path);
-	if (res == -1)
-		return -errno;
+    res = access(path, mask);
+    free_path(path);
+    if (res == -1)
+        return -errno;
 
-	return 0;
+    return 0;
 }
 
 int DirectIOHelper::sh_readlink(const char *path, char *buf, size_t size)
 {
-	int res;
-	path = get_full_path(path);
+    int res;
+    path = get_full_path(path);
 
-	res = readlink(path, buf, size - 1);
-	free_path(path);
-	if (res == -1)
-		return -errno;
+    res = readlink(path, buf, size - 1);
+    free_path(path);
+    if (res == -1)
+        return -errno;
 
-	buf[res] = '\0';
-	return 0;
+    buf[res] = '\0';
+    return 0;
 }
 
 
 int DirectIOHelper::sh_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-		       off_t offset, struct fuse_file_info *fi)
+               off_t offset, struct fuse_file_info *fi)
 {
-	DIR *dp;
-	struct dirent *de;
+    DIR *dp;
+    struct dirent *de;
 
-	(void) offset;
-	(void) fi;
-	path = get_full_path(path);
+    (void) offset;
+    (void) fi;
+    path = get_full_path(path);
 
-	dp = opendir(path);
-	free_path(path);
-	if (dp == NULL)
-		return -errno;
+    dp = opendir(path);
+    free_path(path);
+    if (dp == NULL)
+        return -errno;
 
-	while ((de = readdir(dp)) != NULL) {
-		struct stat st;
-		memset(&st, 0, sizeof(st));
-		st.st_ino = de->d_ino;
-		st.st_mode = de->d_type << 12;
-		if (filler(buf, de->d_name, &st, 0))
-			break;
-	}
+    while ((de = readdir(dp)) != NULL) {
+        struct stat st;
+        memset(&st, 0, sizeof(st));
+        st.st_ino = de->d_ino;
+        st.st_mode = de->d_type << 12;
+        if (filler(buf, de->d_name, &st, 0))
+            break;
+    }
 
-	closedir(dp);
-	return 0;
+    closedir(dp);
+    return 0;
 }
 
 int DirectIOHelper::sh_mknod(const char *path, mode_t mode, dev_t rdev)
 {
-	int res;
-	path = get_full_path(path);
+    int res;
+    path = get_full_path(path);
 
-	/* On Linux this could just be 'mknod(path, mode, rdev)' but this
-	   is more portable */
-	if (S_ISREG(mode)) {
-		res = open(path, O_CREAT | O_EXCL | O_WRONLY, mode);
-		if (res >= 0)
-			res = close(res);
-	} else if (S_ISFIFO(mode))
-		res = mkfifo(path, mode);
-	else
-		res = mknod(path, mode, rdev);
+    /* On Linux this could just be 'mknod(path, mode, rdev)' but this
+       is more portable */
+    if (S_ISREG(mode)) {
+        res = open(path, O_CREAT | O_EXCL | O_WRONLY, mode);
+        if (res >= 0)
+            res = close(res);
+    } else if (S_ISFIFO(mode))
+        res = mkfifo(path, mode);
+    else
+        res = mknod(path, mode, rdev);
 
-	free_path(path);
-	if (res == -1)
-		return -errno;
+    free_path(path);
+    if (res == -1)
+        return -errno;
 
-	return 0;
+    return 0;
 }
 
 int DirectIOHelper::sh_mkdir(const char *path, mode_t mode)
 {
-	int res;
-	path = get_full_path(path);
+    int res;
+    path = get_full_path(path);
 
-	res = mkdir(path, mode);
-	free_path(path);
-	if (res == -1)
-		return -errno;
+    res = mkdir(path, mode);
+    free_path(path);
+    if (res == -1)
+        return -errno;
 
-	return 0;
+    return 0;
 }
 
 int DirectIOHelper::sh_unlink(const char *path)
 {
-	int res;
-	path = get_full_path(path);
+    int res;
+    path = get_full_path(path);
 
-	res = unlink(path);
-	free_path(path);
-	if (res == -1)
-		return -errno;
+    res = unlink(path);
+    free_path(path);
+    if (res == -1)
+        return -errno;
 
-	return 0;
+    return 0;
 }
 
 int DirectIOHelper::sh_rmdir(const char *path)
 {
-	int res;
-	path = get_full_path(path);
+    int res;
+    path = get_full_path(path);
 
-	res = rmdir(path);
-	free_path(path);
-	if (res == -1)
-		return -errno;
+    res = rmdir(path);
+    free_path(path);
+    if (res == -1)
+        return -errno;
 
-	return 0;
+    return 0;
 }
 
 int DirectIOHelper::sh_symlink(const char *from, const char *to)
 {
-	int res;
-	from = get_full_path(from);
-	to = get_full_path(to);
+    int res;
+    from = get_full_path(from);
+    to = get_full_path(to);
 
-	res = symlink(from, to);
-	free_path(from);
-	free_path(to);
-	if (res == -1)
-		return -errno;
+    res = symlink(from, to);
+    free_path(from);
+    free_path(to);
+    if (res == -1)
+        return -errno;
 
-	return 0;
+    return 0;
 }
 
 int DirectIOHelper::sh_rename(const char *from, const char *to)
 {
-	int res;
-	from = get_full_path(from);
-	to = get_full_path(to);
+    int res;
+    from = get_full_path(from);
+    to = get_full_path(to);
 
-	res = rename(from, to);
-	free_path(from);
-	free_path(to);
-	if (res == -1)
-		return -errno;
+    res = rename(from, to);
+    free_path(from);
+    free_path(to);
+    if (res == -1)
+        return -errno;
 
-	return 0;
+    return 0;
 }
 
 int DirectIOHelper::sh_link(const char *from, const char *to)
 {
-	int res;
-	from = get_full_path(from);
-	to = get_full_path(to);
+    int res;
+    from = get_full_path(from);
+    to = get_full_path(to);
 
-	res = link(from, to);
-	free_path(from);
-	free_path(to);
-	if (res == -1)
-		return -errno;
+    res = link(from, to);
+    free_path(from);
+    free_path(to);
+    if (res == -1)
+        return -errno;
 
-	return 0;
+    return 0;
 }
 
 int DirectIOHelper::sh_chmod(const char *path, mode_t mode)
 {
-	int res;
-	path = get_full_path(path);
+    int res;
+    path = get_full_path(path);
 
-	res = chmod(path, mode);
-	free_path(path);
-	if (res == -1)
-		return -errno;
+    res = chmod(path, mode);
+    free_path(path);
+    if (res == -1)
+        return -errno;
 
-	return 0;
+    return 0;
 }
 
 int DirectIOHelper::sh_chown(const char *path, uid_t uid, gid_t gid)
 {
-	int res;
-	path = get_full_path(path);
+    int res;
+    path = get_full_path(path);
 
-	res = lchown(path, uid, gid);
-	free_path(path);
-	if (res == -1)
-		return -errno;
+    res = lchown(path, uid, gid);
+    free_path(path);
+    if (res == -1)
+        return -errno;
 
-	return 0;
+    return 0;
 }
 
 int DirectIOHelper::sh_truncate(const char *path, off_t size)
 {
-	int res;
-	path = get_full_path(path);
+    int res;
+    path = get_full_path(path);
 
-	res = truncate(path, size);
-	free_path(path);
-	if (res == -1)
-		return -errno;
+    res = truncate(path, size);
+    free_path(path);
+    if (res == -1)
+        return -errno;
 
-	return 0;
+    return 0;
 }
 
 #ifdef HAVE_UTIMENSAT
 int DirectIOHelper::sh_utimens(const char *path, const struct timespec ts[2])
 {
-	int res;
-	path = get_full_path(path);
+    int res;
+    path = get_full_path(path);
 
-	/* don't use utime/utimes since they follow symlinks */
-	res = utimensat(0, path, ts, AT_SYMLINK_NOFOLLOW);
+    /* don't use utime/utimes since they follow symlinks */
+    res = utimensat(0, path, ts, AT_SYMLINK_NOFOLLOW);
 
-	free_path(path);
-	if (res == -1)
-		return -errno;
+    free_path(path);
+    if (res == -1)
+        return -errno;
 
-	return 0;
+    return 0;
 }
 #endif /* HAVE_UTIMENSAT */
 
 int DirectIOHelper::sh_open(const char *path, struct fuse_file_info *fi)
 {
-	int res;
-	path = get_full_path(path);
+    int res;
+    path = get_full_path(path);
 
-	res = open(path, fi->flags);
+    res = open(path, fi->flags);
 
-	free_path(path);
-	if (res == -1)
-		return -errno;
+    free_path(path);
+    if (res == -1)
+        return -errno;
 
-	close(res);
-	return 0;
+    fi->fh = res;
+    return 0;
 }
 
 int DirectIOHelper::sh_read(const char *path, char *buf, size_t size, off_t offset,
-		    struct fuse_file_info *fi)
+            struct fuse_file_info *fi)
 {
-	int fd;
-	int res;
-	path = get_full_path(path);
+    int fd;
+    int res;
+    path = get_full_path(path);
 
-	(void) fi;
-	fd = open(path, O_RDONLY);
+    if(fi->fh > 0)
+        fd = fi->fh;
+    else
+        fd = open(path, O_RDONLY);
 
-	free_path(path);
-	if (fd == -1)
-		return -errno;
+    free_path(path);
+    if (fd == -1)
+        return -errno;
 
-	res = pread(fd, buf, size, offset);
-	if (res == -1)
-		res = -errno;
+    res = pread(fd, buf, size, offset);
+    if (res == -1)
+        res = -errno;
 
-	close(fd);
-	return res;
+    if(fi->fh <= 0)
+        close(fd);
+    return res;
 }
 
 int DirectIOHelper::sh_write(const char *path, const char *buf, size_t size,
-		     off_t offset, struct fuse_file_info *fi)
+             off_t offset, struct fuse_file_info *fi)
 {
-	int fd;
-	int res;
-	path = get_full_path(path);
+    int fd;
+    int res;
+    path = get_full_path(path);
 
-	(void) fi;
-	fd = open(path, O_WRONLY);
+    if(fi->fh > 0)
+        fd = fi->fh;
+    else
+       fd = open(path, O_WRONLY);
 
-	free_path(path);
-	if (fd == -1)
-		return -errno;
+    free_path(path);
+    if (fd == -1)
+        return -errno;
 
-	res = pwrite(fd, buf, size, offset);
-	if (res == -1)
-		res = -errno;
+    res = pwrite(fd, buf, size, offset);
+    if (res == -1)
+        res = -errno;
 
-	close(fd);
-	return res;
+    if(fi->fh <= 0)
+        close(fd);
+    return res;
 }
 
 int DirectIOHelper::sh_statfs(const char *path, struct statvfs *stbuf)
 {
-	int res;
-	path = get_full_path(path);
+    int res;
+    path = get_full_path(path);
 
-	res = statvfs(path, stbuf);
+    res = statvfs(path, stbuf);
 
-	free_path(path);
-	if (res == -1)
-		return -errno;
+    free_path(path);
+    if (res == -1)
+        return -errno;
 
-	return 0;
+    return 0;
 }
 
 int DirectIOHelper::sh_release(const char *path, struct fuse_file_info *fi)
 {
-	/* Just a stub.	 This method is optional and can safely be left
-	   unimplemented */
+    (void) path;
 
-	(void) path;
-	(void) fi;
-	return 0;
+    if(fi->fh > 0)
+       return close(fi->fh);
+    else
+       return 0;
 }
 
 int DirectIOHelper::sh_fsync(const char *path, int isdatasync,
-		     struct fuse_file_info *fi)
+             struct fuse_file_info *fi)
 {
-	/* Just a stub.	 This method is optional and can safely be left
-	   unimplemented */
+    /* Just a stub.     This method is optional and can safely be left
+       unimplemented */
 
-	(void) path;
-	(void) isdatasync;
-	(void) fi;
-	return 0;
+    (void) path;
+    (void) isdatasync;
+    (void) fi;
+    return 0;
 }
 
 #ifdef HAVE_POSIX_FALLOCATE
 int DirectIOHelper::sh_fallocate(const char *path, int mode,
-			off_t offset, off_t length, struct fuse_file_info *fi)
+            off_t offset, off_t length, struct fuse_file_info *fi)
 {
-	int fd;
-	int res;
-	path = get_full_path(path);
+    int fd;
+    int res;
+    path = get_full_path(path);
 
-	(void) fi;
+    (void) fi;
 
-	if (mode)
-		return -EOPNOTSUPP;
+    if (mode)
+        return -EOPNOTSUPP;
 
-	fd = open(path, O_WRONLY);
+    fd = open(path, O_WRONLY);
 
-	free_path(path);
-	if (fd == -1)
-		return -errno;
+    free_path(path);
+    if (fd == -1)
+        return -errno;
 
-	res = -posix_fallocate(fd, offset, length);
+    res = -posix_fallocate(fd, offset, length);
 
-	close(fd);
-	return res;
+    close(fd);
+    return res;
 }
 #endif  /* HAVE_POSIX_FALLOCATE */
 
 #ifdef HAVE_SETXATTR
 /* xattr operations are optional and can safely be left unimplemented */
 int DirectIOHelper::sh_setxattr(const char *path, const char *name, const char *value,
-			size_t size, int flags)
+            size_t size, int flags)
 {
-	path = get_full_path(path);
+    path = get_full_path(path);
 
-	int res = lsetxattr(path, name, value, size, flags);
+    int res = lsetxattr(path, name, value, size, flags);
 
-	free_path(path);
-	if (res == -1)
-		return -errno;
-	return 0;
+    free_path(path);
+    if (res == -1)
+        return -errno;
+    return 0;
 }
 
 int DirectIOHelper::sh_getxattr(const char *path, const char *name, char *value,
-			size_t size)
+            size_t size)
 {
-	path = get_full_path(path);
+    path = get_full_path(path);
 
-	int res = lgetxattr(path, name, value, size);
+    int res = lgetxattr(path, name, value, size);
 
-	free_path(path);
-	if (res == -1)
-		return -errno;
-	return res;
+    free_path(path);
+    if (res == -1)
+        return -errno;
+    return res;
 }
 
 int DirectIOHelper::sh_listxattr(const char *path, char *list, size_t size)
 {
-	path = get_full_path(path);
+    path = get_full_path(path);
 
-	int res = llistxattr(path, list, size);
+    int res = llistxattr(path, list, size);
 
-	free_path(path);
-	if (res == -1)
-		return -errno;
-	return res;
+    free_path(path);
+    if (res == -1)
+        return -errno;
+    return res;
 }
 
 int DirectIOHelper::sh_removexattr(const char *path, const char *name)
 {
-	path = get_full_path(path);
+    path = get_full_path(path);
 
-	int res = lremovexattr(path, name);
+    int res = lremovexattr(path, name);
 
-	free_path(path);
-	if (res == -1)
-		return -errno;
-	return 0;
+    free_path(path);
+    if (res == -1)
+        return -errno;
+    return 0;
 }
 
 #endif /* HAVE_SETXATTR */
