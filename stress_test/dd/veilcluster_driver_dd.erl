81911e5f4d7343a4e13c858d9ce1f6cc46d211d9
Łukasz Dutka <lukasz.dutka@cyfronet.pl>
2013-10-03 23:06:32 +0200

    Merge pull request #96 in VFS/veilcluster from VFS-168 to master
    Squashed commit of the following:
    
    commit f8183b583256f4748602992bba777bf898759dfb
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Thu Oct 3 23:02:44 2013 +0200
    
        VFS-168: some cleanup
    
    commit d7cf41250d7d0eea68956885ec10b848202709da
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Thu Oct 3 22:59:53 2013 +0200
    
        VFS-168: some cleanup
    
    commit e9f0b0b8e32b81e2a753f89b43d295f9d062bfae
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Thu Oct 3 22:39:31 2013 +0200
    
        VFS-168: bit longer test
    
    commit 784c70ecadb239f378ba79ea83b6dd0d970a2190
    Merge: bf60010 c968712
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Thu Oct 3 22:10:41 2013 +0200
    
        Merge remote-tracking branch 'origin/master' into VFS-168
    
        Conflicts:
        	stress_test/ping/ping.config
    
    commit bf6001048728c86e972393f7cb76c32ada74ad32
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Thu Oct 3 22:07:58 2013 +0200
    
        VFS-168: error slow down
    
    commit 0f4780414956f662785b8f1ec752f19430c4935e
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Thu Oct 3 20:41:04 2013 +0200
    
        VFS-168:
    
    commit 9128630dede09e7cbf31491bc534745c57871111
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Thu Oct 3 19:33:13 2013 +0200
    
        VFS-168:
    
    commit d1d7464599a38f9ef2da8e6f4a725eb72570914a
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Thu Oct 3 18:22:38 2013 +0200
    
        VFS-168:
    
    commit ada2db9614f70596acea5ff4a4875bcbebab964f
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Thu Oct 3 18:02:43 2013 +0200
    
        VFS-168
    
    commit 83f021c6b99d2f5a785d91bf27735cdbd3e8c93c
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Thu Oct 3 17:06:57 2013 +0200
    
        VFS-168: switch to pwrite
    
    commit 55952d6a198e6213e58fc9c597ee2cb01c094e21
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Thu Oct 3 16:03:17 2013 +0200
    
        VFS-168: small fix
    
    commit 6195ba7b789bfe61ca4ef07db6092ca390b9860c
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Thu Oct 3 15:33:12 2013 +0200
    
        VFS-168:
    
    commit 7b959cf74a43827d2a36c9632c4889c027031de2
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Thu Oct 3 15:06:52 2013 +0200
    
        VFS-168
    
    commit 297583eedbaa704091be031e4c108bfd5ceff892
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Thu Oct 3 14:27:14 2013 +0200
    
        VFS-168
    
    commit 6393f238057d5ceef2d45804363f5706f1820ecc
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Thu Oct 3 13:58:15 2013 +0200
    
        VFS-168
    
    commit 6bd68584debadea8eead1a43b91e016e989d9be1
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Wed Oct 2 21:16:44 2013 +0200
    
        VFS-168: compile fix
    
    commit b33cfd50be3f371bc1f40f35afb195a74419a024
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Tue Oct 1 14:07:28 2013 +0200
    
        VFS-168: disable ping test
    
    commit 61b599b424ac08a43e3904194965face479b92fd
    Merge: 3ff2d8f 104e08b
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Tue Oct 1 14:06:30 2013 +0200
    
        Merge branch 'VFS-168' of ssh://git.plgrid.pl:7999/vfs/veilcluster into VFS-168
    
    commit 3ff2d8f30c81e7b96cca6f99adf2681a97136bab
    Merge: 3212c9f a988c99
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Tue Oct 1 14:06:08 2013 +0200
    
        Merge branch 'master' into VFS-168
    
        Conflicts:
        	veilprotocol
    
    commit 104e08bc7abf71b9ca5c5d48965b5580a1de8288
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Mon Sep 30 19:23:58 2013 +0200
    
        VFS-168: protocol version update
    
    commit 3212c9f8a2f73942980e235ef4b497f5aca93e0a
    Author: Rafal Slota <roxeon@gmail.com>
    Date:   Mon Sep 30 16:52:35 2013 +0200
    
        VFS-168: initial commit

---
+++ stress_test/dd/veilcluster_driver_dd.erl
@@ -0,0 +1,101 @@
+-module(veilcluster_driver_dd).
+-export([new/1, run/4, setup/0]).
+
+-include("basho_bench.hrl").
+-include("registered_names.hrl").
+-include("communication_protocol_pb.hrl").
+-include("veil_modules/dao/dao.hrl").
+-include("veil_modules/fslogic/fslogic.hrl").
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% TEST DRIVER CALLBACKS %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Runs once per each test node at begging of a test (before any new/1 is called)
+setup() ->
+    try 
+        %% Init net kernet in order to connect to cluster
+        NetKernel = net_kernel:start([list_to_atom("tester@" ++ net_adm:localhost()), longnames]),
+        ?DEBUG("net_kernel ans: ~p~n", [NetKernel]),
+        erlang:set_cookie(node(), veil_cluster_node),
+        ?INFO("SETUP: ~p", [basho_bench_config:get(client_id)]),
+        InitRes = 
+            case basho_bench_config:get(client_id) of 
+                1 -> catch setup_storages(); %% If its the first test node, initialize cluster 
+                _ -> timer:sleep(2000) %% Otherwise wait for main node to finish
+                                       %% TODO: implement better, more deterministic way of synchronising test nodes (e.g. via ready-ping)
+            end,
+        ?DEBUG("Setup: ~p~n", [InitRes])
+    catch
+      E1:E2 -> ?DEBUG("setup error: ~p:~p~n", [E1, E2])
+    end.
+
+new(Id) -> 
+    VFSRoot = basho_bench_config:get(veilfs_root),
+    Dir = VFSRoot ++ "/stress_test_" ++ basho_bench_config:get(build_id),
+    file:make_dir(Dir),
+    File = Dir ++ "/file_" ++ integer_to_list(Id), 
+
+    %% Open test file (each test process gets different file name like "file_Id")
+    Device = 
+        case open_helper(File, {error, first_try}, 20) of 
+            {error, Reason} ->
+                ?ERROR("new/1 error: ~p", [Reason]),
+                Reason;
+            IO -> IO
+    end,
+    ?DEBUG("dd file: ~p~n", [File]),
+    BlockSize = basho_bench_config:get(block_size),
+    Data = [0 || _X <- lists:seq(1, 1024*BlockSize)],
+    {ok, {Device, 0, Data}}.
+
+
+%% Only 'write' action is implemented right now
+run(write, _KeyGen, _ValueGen, {Dev, _Offset, _Data} = State) when is_atom(Dev) ->
+    timer:sleep(1000), %% Dont generate more then one error per sec when open/2 is failing
+    {error, {open, Dev}, State};
+run(write, _KeyGen, _ValueGen, {Dev, Offset, Data}) ->
+    NewState = {Dev, (Offset + length(Data)) rem (basho_bench_config:get(max_filesize) * 1024*1024), Data},
+    case file:pwrite(Dev, 0, Data) of 
+        ok -> {ok, NewState};
+        {error, Reason} -> 
+            ?DEBUG("Error (file: ~p, offset: ~p): ~p", [Dev, Offset, Reason]),
+            {error, Reason, NewState}
+    end;
+run(Action, _KeyGen, _ValueGen, State) ->
+    ?ERROR("Unknown action ~p with state ~p", [Action, State]),
+    {error, unknown_action}.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% PRIVETE HELPER METHODS %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Register test user and configure storages on cluster
+setup_storages() ->
+    Cert = basho_bench_config:get(cert_file),
+    [Host | _] = basho_bench_config:get(cluster_hosts),
+    rpc:call(list_to_atom("worker@" ++ Host), user_logic, create_user, ["test_user", "Test Name", [], "test@test.com", [get_dn(Cert)]]),
+    Groups = #fuse_group_info{name = ?CLUSTER_FUSE_ID, storage_helper = #storage_helper_info{name = "DirectIO", init_args = ["/mnt/gluster"]}},
+    rpc:call(list_to_atom("worker@" ++ Host), fslogic_storage, insert_storage, ["ClusterProxy", [], [Groups]]),
+    rpc:call(list_to_atom("worker@" ++ Host), os, cmd, ["rm -rf /mnt/gluster/*"]).
+
+%% Gets rDN list compatibile user_logic:create_user from PEM file
+get_dn(PEMFile) ->
+    {ok, PemBin} = file:read_file(PEMFile),
+    Cert = public_key:pem_decode(PemBin),
+    [Leaf | Chain] = [public_key:pkix_decode_cert(DerCert, otp) || {'Certificate', DerCert, _} <- Cert],
+    {ok, EEC} = gsi_handler:find_eec_cert(Leaf, Chain, gsi_handler:is_proxy_certificate(Leaf)), 
+    {rdnSequence, Rdn} = gsi_handler:proxy_subject(EEC),
+    {ok, DnString} = user_logic:rdn_sequence_to_dn_string(Rdn),
+    DnString.
+
+%% Opens requested file retring when error occurs
+open_helper(_, {ok, IO}, _) ->
+    IO;
+open_helper(File, {error, _Error}, Retry) when Retry > 0 ->
+    {_, _, M} = now(),
+    timer:sleep(M rem 100),
+    ?DEBUG("open error ~p (~p), retring in ~p", [_Error, Retry, M rem 100]),
+    open_helper(File, file:open(File, [write, raw]), Retry - 1);
+open_helper(_, _, _) ->
+    {error, open_failed}.
