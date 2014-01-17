28ccc732e8be33bd0cc73558c29d969be06de012
plgpalys <palys@student.agh.edu.pl>
2014-01-17 17:06:44 +0100

    Added checking if ports 55, 443 and 5555 are free.

--- src/veil_cluster_node_app.erl
+++ src/veil_cluster_node_app.erl
@@ -35,10 +35,50 @@
                 | term().
 %% ====================================================================
 start(_StartType, _StartArgs) ->
-  {ok, NodeType} = application:get_env(?APP_Name, node_type),
-  fprof:start(), %% Start fprof server. It doesnt do enything unless it's used.
-  veil_cluster_node_sup:start_link(NodeType).
+  Free_ports_info = are_ports_free([55,443,5555]),
+  case Free_ports_info of
+    {ok, free} ->
+        {ok, NodeType} = application:get_env(?APP_Name, node_type),
+        fprof:start(), %% Start fprof server. It doesnt do enything unless it's used.
+        veil_cluster_node_sup:start_link(NodeType);
+    {_,_} ->
+        {error, "Some ports are in use."}
+  end.
 
+%% are_ports_free/1
+%% ====================================================================
+%% @doc Checks if ports are free. If some ports are in use, logs and writes to stderr.
+-spec are_ports_free(Ports :: list()) -> Result when
+    Result :: {ok, free}
+                | {error, occupied}.
+%% ====================================================================
+are_ports_free([]) ->
+    {ok, free};
+are_ports_free([Port|Ports])->
+    Port_result = is_port_free(Port),
+    case Port_result of
+        {ok, free} -> are_ports_free(Ports);
+        {error,_} ->
+            lager:error("Port ~w is in use. Starting aborted.~n", [Port]),
+            io:fwrite(standard_error, "Port ~w is in use.~n", [Port]),
+            {error, occupied}
+    end.
+
+%% is_port_free/1
+%% ====================================================================
+%% @doc Checks if port is free.
+-spec is_port_free(Ports :: integer()) -> Result when
+    Result :: {ok, free}
+                | {error, occupied}.
+%% ====================================================================
+is_port_free(Port) ->
+    Cmd_result = os:cmd("netstat -tlun | awk '{print $4}' | tail -n +3 | grep -E -o ':[0-9]+' | cut -d: -f 2 | grep ^" ++ integer_to_list(Port) ++ "$"),
+    case Cmd_result of
+        "" ->
+            {ok, free};
+        _ ->
+            {error, occupied}
+    end.
 
 %% stop/1
 %% ====================================================================
