48720cd1e2b22593f8d1187bfdbb5d0cf5aa59de
Michał Wrzeszcz <michalwrzeszcz@gmail.com>
2014-01-24 10:43:19 +0100

    BambooTest - fslogic verbose mode

--- test_distributed/fslogic_test/fslogic_test_SUITE.erl
+++ test_distributed/fslogic_test/fslogic_test_SUITE.erl
@@ -2010,7 +2010,7 @@ init_per_testcase(_, Config) ->
   ?INIT_DIST_TEST,
   nodes_manager:start_deps_for_tester_node(),
 
-  NodesUp = nodes_manager:start_test_on_nodes(1),
+  NodesUp = nodes_manager:start_test_on_nodes(1, true),
   [FSLogicNode | _] = NodesUp,
 
   DB_Node = nodes_manager:get_db_node(),
