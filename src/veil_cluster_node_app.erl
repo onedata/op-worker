295d3d4104aa23effcc937e9cf0e4c30a3632761
Tomasz Lichon <lichon@student.agh.edu.pl>
2014-01-26 01:45:07 +0100

    -Port checking added to veil_cluster_node startup -Db hostname configuration moved from bigouch start script to installator

--- src/veil_cluster_node_app.erl
+++ src/veil_cluster_node_app.erl
@@ -35,9 +35,14 @@
                 | term().
 %% ====================================================================
 start(_StartType, _StartArgs) ->
-  {ok, NodeType} = application:get_env(?APP_Name, node_type),
-  fprof:start(), %% Start fprof server. It doesnt do enything unless it's used.
-  veil_cluster_node_sup:start_link(NodeType).
+	case ports_ok() of
+		true ->
+			{ok, NodeType} = application:get_env(?APP_Name, node_type),
+			fprof:start(), %% Start fprof server. It doesnt do enything unless it's used.
+			veil_cluster_node_sup:start_link(NodeType);
+		false ->
+			{error, "Not all ports are free"}
+	end.
 
 
 %% stop/1
@@ -48,3 +53,43 @@ start(_StartType, _StartArgs) ->
 %% ====================================================================
 stop(_State) ->
   ok.
+
+%% ports_ok/0
+%% ====================================================================
+%% @doc Get port list from environment and check if they're free
+-spec ports_ok() -> Result when
+	Result :: true | false.
+%% ====================================================================
+ports_ok() ->
+	GetEnvResult = application:get_env(veil_cluster_node,ports_in_use),
+	case GetEnvResult of
+		{ok,PortList} ->
+			ports_are_free(PortList);
+		undefined ->
+			lager:error("Could not get 'ports_in_use' environment variable."),
+			io:format(standard_error, "Could not get 'ports_in_use' environment variable.~n"),
+			false
+	end.
+
+%% ports_are_free/1
+%% ====================================================================
+%% @doc Returns true if all listed ports are free, if not - log error
+%% and print to stderr
+-spec ports_are_free(Ports :: list() | integer()) -> Result when
+	Result :: true | false.
+%% ====================================================================
+ports_are_free([]) ->
+	true;
+ports_are_free([FirstPort | Rest])->
+	ports_are_free(FirstPort) and ports_are_free(Rest);
+ports_are_free(Port)->
+	{Status, Socket} = gen_tcp:listen(Port, []),
+	case Status of
+		ok ->
+			gen_tcp:close(Socket),
+			true;
+		error ->
+			lager:error("Port ~w is in use. Starting aborted.~n", [Port]),
+			io:format(standard_error, "Port ~w is in use. Starting aborted.~n", [Port]),
+			false
+	end.
