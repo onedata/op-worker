ff898b88945569d5b1fca677815032a12c199708
Tomasz Lichon <lichon@student.agh.edu.pl>
2014-02-12 15:57:35 +0100

    handling and reporting timeouts during calls to: cmm, node_manager,  request_dispatcher and workers

--- src/veil_modules/control_panel/handlers/nagios_handler.erl
+++ src/veil_modules/control_panel/handlers/nagios_handler.erl
@@ -33,9 +33,7 @@ init(_Type, Req, _Opts) ->
 handle(Req, State) ->
 	%get data from ccm and env
 	{ok, Timeout} = application:get_env(veil_cluster_node, nagios_healtcheck_timeout),
-	Nodes = gen_server:call({global, ?CCM}, get_nodes, Timeout),
-	{Workers, StateNum} = gen_server:call({global, ?CCM}, get_workers, Timeout),
-	{_, CStateNum} = gen_server:call({global, ?CCM}, get_callbacks, Timeout),
+	{Nodes,Workers,StateNum,CStateNum,CcmConnError} = get_data_from_ccm(Timeout),
 
 	%check workers
 	WorkersStatus = pmap(fun(Worker) -> worker_status(Worker,Timeout) end,Workers),
@@ -45,10 +43,17 @@ handle(Req, State) ->
 	NodesStatus = pmap(fun(Node) -> node_status(Node,StateNum,CStateNum,WorkersOk,Timeout) end, Nodes),
 
 	%check if errors occured
-	{HealthStatus, HttpStatusCode} = case contains_errors(NodesStatus) or contains_errors(WorkersStatus) of
-		true -> {"error",500};
-		false -> {"ok",200}
-	end,
+	{HealthStatus, HttpStatusCode} =
+		case CcmConnError/=none of
+			true ->
+				ErrorString = io_lib:format("~p", [{error,CcmConnError}]),
+				{ErrorString,500};
+			false ->
+				case contains_errors(NodesStatus) or contains_errors(WorkersStatus) of
+					true -> {"{error,unhealthy_member}",500};
+					false -> {"ok",200}
+				end
+		end,
 
 	%prepare current date
 	{{YY, MM, DD}, {Hour, Min, Sec}} = calendar:now_to_local_time(now()),
@@ -76,6 +81,27 @@ terminate(_Reason, _Req, _State) ->
 %% Internal Functions
 %% ====================================================================
 
+%% get_data_from_ccm/1
+%% ====================================================================
+%% @doc Get from ccm: nodes, workers, state number, callbak state number,
+%% error (if error occured, 'none' otherwise)
+%% @end
+-spec get_data_from_ccm(Timeout :: integer()) -> Result when
+	Result :: {Nodes :: list(),Worers :: list(),StateNum :: integer(),CStateNum :: integer(),Error},
+	Error :: term() | none.
+%% ====================================================================
+get_data_from_ccm(Timeout) ->
+	try
+		Nodes = gen_server:call({global, ?CCM}, get_nodes, Timeout),
+		{Workers, StateNum} = gen_server:call({global, ?CCM}, get_workers, Timeout),
+		{_, CStateNum} = gen_server:call({global, ?CCM}, get_callbacks, Timeout),
+		{Nodes,Workers,StateNum,CStateNum,none}
+	catch
+		Type:Error  ->
+			lager:error("Ccm connection error: ~p:~p",[Type,Error]),
+			{[],[],none,none,Error}
+	end.
+
 %% node_status/5
 %% ====================================================================
 %% @doc Checks if callbacks num and state num on dispatcher and node manager are same as in ccm,
@@ -88,29 +114,49 @@ terminate(_Reason, _Req, _State) ->
 %% ====================================================================
 node_status(Node,CcmStateNum,CcmCStateNum,WorkersOk,Timeout) ->
 	lager:debug("Healthcheck on node:~p",[Node]),
-	%get state nuber and callback number from node manager and dispatcher
-	{_, DispCStateNum} = gen_server:call({?Dispatcher_Name, Node}, get_callbacks, Timeout),
-	DispStateNum = gen_server:call({?Dispatcher_Name, Node}, get_state_num, Timeout),
-	{ManagerCStateNum,ManagerStateNum} = gen_server:call({?Node_Manager_Name, Node}, get_callback_and_state_num, Timeout),
+	try
+		%get state nuber and callback number from node manager and dispatcher
+		{_, DispCStateNum} = gen_server:call({?Dispatcher_Name, Node}, get_callbacks, Timeout),
+		DispStateNum = gen_server:call({?Dispatcher_Name, Node}, get_state_num, Timeout),
+		{ManagerCStateNum,ManagerStateNum} = gen_server:call({?Node_Manager_Name, Node}, get_callback_and_state_num, Timeout),
 
-	%compare them with numbers from ccm and prepare result
-	case (CcmStateNum == DispStateNum) and (CcmCStateNum == DispCStateNum) and (CcmStateNum==ManagerStateNum) and (CcmCStateNum==ManagerCStateNum)  of
-	true ->
-		{veil_cluster_node,[{name,atom_to_list(Node)},{status,"ok"}],[]};
-	false ->
-		case WorkersOk of
-			true ->
-				lager:warning("Healthcheck on node ~p, callbacks/state number of ccm doesn't match values from node_manager and dispatcher," ++
-					"but all workers are fine, cluster is probably initializing",[Node]),
-				lager:warning("ccm_state_num: ~p, ccm_callback_num: ~p,disp_state_num: ~p, disp_callback_num: ~p,manager_state_num: ~p, manager_callback_num: ~p",
-					[CcmStateNum,CcmCStateNum,DispStateNum,DispCStateNum,ManagerStateNum,ManagerCStateNum]),
-				{veil_cluster_node,[{name,atom_to_list(Node)},{status,"initializing"}],[]};
-			false ->
-				lager:error("Healthcheck on node ~p failed, callbacks/state number of ccm doesn't match values from node_manager and dispatcher",[Node]),
-				lager:error("ccm_state_num: ~p, ccm_callback_num: ~p,disp_state_num: ~p, disp_callback_num: ~p,manager_state_num: ~p, manager_callback_num: ~p",
-					[CcmStateNum,CcmCStateNum,DispStateNum,DispCStateNum,ManagerStateNum,ManagerCStateNum]),
-				{veil_cluster_node,[{name,atom_to_list(Node)},{status,"error"}],[]}
+		%compare them with numbers from ccm and prepare result
+		AllStateNumbersOk = (CcmStateNum == DispStateNum) and (CcmCStateNum == DispCStateNum) and (CcmStateNum==ManagerStateNum) and (CcmCStateNum==ManagerCStateNum),
+		case AllStateNumbersOk  of
+		true ->
+			{veil_cluster_node,[{name,atom_to_list(Node)},{status,"ok"}],[]};
+		false ->
+			case WorkersOk of
+				true ->
+					%log
+					lager:warning("Healthcheck on node ~p, callbacks/state number of ccm doesn't match values from node_manager and dispatcher," ++
+						"but all workers are fine, cluster is probably initializing",[Node]),
+					lager:warning("ccm_state_num: ~p, ccm_callback_num: ~p,disp_state_num: ~p, disp_callback_num: ~p,manager_state_num: ~p, manager_callback_num: ~p",
+						[CcmStateNum,CcmCStateNum,DispStateNum,DispCStateNum,ManagerStateNum,ManagerCStateNum]),
+					%return
+					{veil_cluster_node,[{name,atom_to_list(Node)},{status,"initializing"}],[]};
+				false ->
+					%log
+					lager:error("Healthcheck on node ~p failed, callbacks/state number of ccm doesn't match values from node_manager and dispatcher",[Node]),
+					lager:error("ccm_state_num: ~p, ccm_callback_num: ~p,disp_state_num: ~p, disp_callback_num: ~p,manager_state_num: ~p, manager_callback_num: ~p",
+						[CcmStateNum,CcmCStateNum,DispStateNum,DispCStateNum,ManagerStateNum,ManagerCStateNum]),
+					%prepare error
+					ErrorString1 =
+						case {(CcmStateNum == DispStateNum),(CcmCStateNum == DispCStateNum),(CcmStateNum==ManagerStateNum),(CcmCStateNum==ManagerCStateNum)} of
+							{false,_,_,_} -> "{error,invalid_dispatcher_state_num}";
+							{_,false,_,_} -> "{error,invalid_dispatcher_callback_state_num}";
+							{_,_,false,_} -> "{error,invalid_manager_state_num}";
+							{_,_,_,false} -> "{error,invalid_manager_callback_state_num}"
+						end,
+					%return
+					{veil_cluster_node,[{name,atom_to_list(Node)},{status,ErrorString1}],[]}
+			end
 		end
+	catch
+	    Type:Error ->
+				lager:error("Node ~p connection error: ~p:~p",[Node,Type,Error]),
+				ErrorString2 = io_lib:format("~p", [{error,Error}]),
+				{veil_cluster_node,[{name,atom_to_list(Node)},{status,ErrorString2}],[]}
 	end.
 
 %% worker_status/2
@@ -126,25 +172,31 @@ node_status(Node,CcmStateNum,CcmCStateNum,WorkersOk,Timeout) ->
 worker_status(Worker,Timeout) ->
 	lager:debug("Healthcheck on worker: ~p",[Worker]),
 	{WorkerNode,WorkerName} = Worker,
-
-	%do healthcheck
-	gen_server:call({?Dispatcher_Name, WorkerNode}, {WorkerName, 1,self(), healthcheck}, Timeout),
-	Ans = receive
-			Any -> Any
-		after Timeout ->
-			{error, worker_response_timeout}
-	end,
-
-	%check healthcheck answer and prepare result
 	NameString = atom_to_list(WorkerName),
 	NodeString = atom_to_list(WorkerNode),
-	case Ans of
-		ok ->
-			{worker,[{name,NameString},{node,NodeString},{status,"ok"}],[]};
-		Error ->
-			lager:error("Healthcheck on worker ~p failed with error: ~p",[Worker,Error]),
-			ErrorString = io_lib:format("~p", [Error]),
-			{worker,[{name,NameString},{node,NodeString},{status,ErrorString}],[]}
+	try
+		%do healthcheck
+		gen_server:call({?Dispatcher_Name, WorkerNode}, {WorkerName, 1,self(), healthcheck}, Timeout),
+		Ans = receive
+				Any -> Any
+			after Timeout ->
+				{error, worker_response_timeout}
+		end,
+
+		%check healthcheck answer and prepare result
+		case Ans of
+			ok ->
+				{worker,[{name,NameString},{node,NodeString},{status,"ok"}],[]};
+			ErrorAns ->
+				lager:error("Healthcheck on worker ~p failed with error: ~p",[Worker,ErrorAns]),
+				ErrorAnsString = io_lib:format("~p", [ErrorAns]),
+				{worker,[{name,NameString},{node,NodeString},{status,ErrorAnsString}],[]}
+		end
+	catch
+	    Type:Error ->
+				lager:error("Worker ~p connection error: ~p:~p",[Worker,Type,Error]),
+				ErrorString = io_lib:format("~p", [{error,Error}]),
+				{worker,[{name,NameString},{node,NodeString},{status,ErrorString}],[]}
 	end.
 
 %% contains_errors/1
