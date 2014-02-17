35393ac906cef124c4974e3718eb932b0c29a9ee
Michał Wrzeszcz <michalwrzeszcz@gmail.com>
2014-02-17 11:50:03 +0100

    VFS-285, ping driver test

--- stress_test/ping/veilcluster_driver_ping.erl
+++ stress_test/ping/veilcluster_driver_ping.erl
@@ -31,11 +31,16 @@ new(_Id) ->
 run(Action, KeyGen, _ValueGen, {Hosts, CertFile, PongAnsBytes}) ->
     Host = lists:nth((KeyGen() rem length(Hosts)) + 1 , Hosts),
     NewState = {Hosts, CertFile, PongAnsBytes},
-    case wss:connect(Host, 5555, [{certfile, CertFile}]) of
-%%     case wss:connect(Host, 5555, [{certfile, CertFile}, {cacertfile, CertFile}, auto_handshake]) of
+    case wss:connect(Host, 5555, [{certfile, CertFile}, {cacertfile, CertFile}, auto_handshake]) of
         {ok, Socket} ->
+            Res =
+                try ping(Action, Socket, PongAnsBytes) of
+                    ok -> {ok, NewState}
+                catch
+                    Reason -> {error, Reason, NewState}
+                end,
             wss:close(Socket),
-            {ok, NewState};
+            Res;
         {error, Error} -> {error, {connect, Error}, NewState};
         Other -> {error, {unknown_error, Other}, NewState}
     end.
