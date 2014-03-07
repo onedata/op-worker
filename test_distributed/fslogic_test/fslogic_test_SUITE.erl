73de208dd1bd351c626042d1afb78334d05bde81
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2014-03-07 08:25:29 +0100

    VFS-444 Change arguments for insert storage function in user_file_size_test for fslogic.

--- test_distributed/fslogic_test/fslogic_test_SUITE.erl
+++ test_distributed/fslogic_test/fslogic_test_SUITE.erl
@@ -866,7 +866,7 @@ user_file_size_test(Config) ->
   end,
 
   %% Init storage
-  {InsertStorageAns, _} = rpc:call(Node, fslogic_storage, insert_storage, [?SH, ?TEST_ROOT]),
+  {InsertStorageAns, _} = rpc:call(Node, fslogic_storage, insert_storage, [?SH, ?ARG_TEST_ROOT]),
   ?assertEqual(ok, InsertStorageAns),
 
   %% Init users
