2578486bc097ed02de97ded6c7e5949e17069541
Tomasz Lichon <xorver@gmail.com>
2014-03-15 15:58:36 +0100

    changed websocket_terminate to include all close types

--- test_distributed/wss.erl
+++ test_distributed/wss.erl
@@ -46,7 +46,7 @@ websocket_info({close, Payload}, _ConnState, State) ->
 websocket_terminate({close, Code, _Payload}, _ConnState, State) ->
     State ! {self(), {closed, Code}},
     ok;
-websocket_terminate({normal, _Payload}, _ConnState, State) ->
+websocket_terminate({_Code, _Payload}, _ConnState, State) ->
 %% 		lager:info("websocket_terminate(~p, ~p, ~p) ",[{normal, _Payload}, _ConnState, State]),
 		ok.
 
