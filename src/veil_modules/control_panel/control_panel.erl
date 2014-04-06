426e15406a5f0ab8307cd895c167a10cc368d021
Łukasz Opioła <lukasz.opiola@gmail.com>
2014-04-06 17:11:17 +0200

    VFS-498, add redirect handler for redirection from http to https

--- src/veil_modules/control_panel/control_panel.erl
+++ src/veil_modules/control_panel/control_panel.erl
@@ -76,7 +76,7 @@ init(_Args) ->
     % Start the listener that will redirect all requests of http to https
     RedirectDispatch = [
         {'_', [
-            {":path", redirect_handler, []}
+            {"/:path", redirect_handler, []}
         ]}
     ],
     {ok, _} = cowboy:start_http(?http_redirector_listener, RedirectNbAcceptors,
