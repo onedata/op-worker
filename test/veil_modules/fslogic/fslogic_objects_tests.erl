dbf4b1dd48def5f91c3510c097748ab0eb9564c6
Rafał Słota <roxeon@gmail.com>
2014-05-14 20:33:56 +0200

    VFS-553: add unit test files

---
+++ test/veil_modules/fslogic/fslogic_objects_tests.erl
@@ -0,0 +1,20 @@
+%% ===================================================================
+%% @author Rafal Slota
+%% @copyright (C): 2013, ACK CYFRONET AGH
+%% This software is released under the MIT license 
+%% cited in 'LICENSE.txt'.
+%% @end
+%% ===================================================================
+%% @doc: This module tests the functionality of fslogic_objects.
+%% It contains unit tests that base on eunit.
+%% @end
+%% ===================================================================
+-module(fslogic_objects_tests).
+-author("Rafal Slota").
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+
+
+-endif.
---
+++ test/veil_modules/fslogic/fslogic_perms_tests.erl
@@ -0,0 +1,20 @@
+%% ===================================================================
+%% @author Rafal Slota
+%% @copyright (C): 2013, ACK CYFRONET AGH
+%% This software is released under the MIT license 
+%% cited in 'LICENSE.txt'.
+%% @end
+%% ===================================================================
+%% @doc: This module tests the functionality of fslogic_perms.
+%% It contains unit tests that base on eunit.
+%% @end
+%% ===================================================================
+-module(fslogic_perms_tests).
+-author("Rafal Slota").
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+
+
+-endif.
