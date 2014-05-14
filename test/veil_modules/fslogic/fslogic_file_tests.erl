a73ec7c44f519ccdf27ba490b7495c4f144e9a7c
Rafał Słota <roxeon@gmail.com>
2014-05-15 01:40:23 +0200

    VFS-553: add fslogic_file_tests template

---
+++ test/veil_modules/fslogic/fslogic_file_tests.erl
@@ -0,0 +1,22 @@
+%% ===================================================================
+%% @author Rafal Slota
+%% @copyright (C): 2013, ACK CYFRONET AGH
+%% This software is released under the MIT license 
+%% cited in 'LICENSE.txt'.
+%% @end
+%% ===================================================================
+%% @doc: This module tests the functionality of fslogic_file.
+%% It contains unit tests that base on eunit.
+%% @end
+%% ===================================================================
+-module(fslogic_file_tests).
+-author("Rafal Slota").
+
+-include("veil_modules/fslogic/fslogic.hrl").
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+
+
+-endif.
