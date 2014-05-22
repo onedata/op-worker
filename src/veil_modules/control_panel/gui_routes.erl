e87c139f18f29a12891d639da89c5286398e9182
Tomasz Lichon <lichon@student.agh.edu.pl>
2014-05-22 17:17:33 +0200

    test callbacks for gui and rest added

--- src/veil_modules/control_panel/gui_routes.erl
+++ src/veil_modules/control_panel/gui_routes.erl
@@ -33,4 +33,5 @@ route(<<"/logs">>) -> page_logs;
 route(<<"/manage_account">>) -> page_manage_account;
 route(<<"/about">>) -> page_about;
 route(<<"/error">>) -> page_error;
+route(<<"/test">>) -> page_test;
 route(_) -> page_404.
