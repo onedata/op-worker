2d40cf0cceea3b3d7ab84ca2947cf37be0c9c166
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2014-06-06 06:45:49 +0200

    VFS-531 Fix test description.

--- test/cluster_elements/rrderlang/rrderlang_tests.erl
+++ test/cluster_elements/rrderlang/rrderlang_tests.erl
@@ -1,6 +1,6 @@
 %% ===================================================================
-%% @author Michal Wrzeszcz
-%% @copyright (C): 2013 ACK CYFRONET AGH
+%% @author Krzysztof Trzepla
+%% @copyright (C): 2014 ACK CYFRONET AGH
 %% This software is released under the MIT license 
 %% cited in 'LICENSE.txt'.
 %% @end
