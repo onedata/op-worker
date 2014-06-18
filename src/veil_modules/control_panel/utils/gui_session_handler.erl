83821de1f3c2c2f0ddc1e807fdb36ad68cd23a7b
Łukasz Opioła <lukasz.opiola@gmail.com>
2014-06-18 23:25:21 +0200

    VFS-596, move some code to session_logic

--- src/veil_modules/control_panel/utils/gui_session_handler.erl
+++ src/veil_modules/control_panel/utils/gui_session_handler.erl
@@ -15,8 +15,6 @@
 -include("veil_modules/control_panel/common.hrl").
 -include("logging.hrl").
 
--compile(export_all).
-
 %% n2o session_handler API, with exception of create/0
 -export([init/2, finish/2, get_value/2, set_value/2, create/0, clear/0]).
 
@@ -27,7 +25,7 @@
 % ETS name for cookies
 -define(ets_name, cookies).
 % 1 day TTL
--define(cookie_max_age, 86400).
+-define(cookie_max_age, 5).
 
 % Key for process dictionary, holding information if there is a valid session
 -define(session_valid, session_valid).
@@ -50,19 +48,21 @@ init(State, Ctx) ->
     {Megaseconds, Seconds, _} = now(),
     Till = Megaseconds * 1000000 + Seconds + ?cookie_max_age,
 
-    SessionID = case lookup_session(Cookie) of
+    Module = get_session_logic_module(),
+
+    SessionID = case Module:lookup_session(Cookie) of
                     undefined ->
                         put(?session_valid, false),
                         % Creates a new session and allows storing data,
                         % but if create/0 is not called in the scope of this request,
                         % the session is discarded.
                         NewSessionID = random_id(),
-                        save_session(NewSessionID, [], Till),
+                        Module:save_session(NewSessionID, [], Till),
                         NewSessionID;
                     Props ->
                         put(?session_valid, true),
                         % Refreshes the expiration time of current session
-                        save_session(Cookie, Props, Till),
+                        Module:save_session(Cookie, Props, Till),
                         Cookie
                 end,
     {ok, State, Ctx#context{session = SessionID}}.
@@ -77,6 +77,7 @@ init(State, Ctx) ->
 -spec finish(State :: term(), Ctx :: #context{}) -> {ok, NewState :: term(), NewCtx :: #context{}}.
 %% ====================================================================
 finish(_State, Ctx) ->
+    Module = get_session_logic_module(),
     SessionID = Ctx#context.session,
     NewReq = case get(?session_valid) of
                  true ->
@@ -91,7 +92,7 @@ finish(_State, Ctx) ->
                  false ->
                      % Session is not valid, discard current session and set "no_session" cookie value
                      % as well as set max_age to 0, which should delete the cookie on client's side.
-                     delete_session(SessionID),
+                     Module:delete_session(SessionID),
                      Options = [
                          {path, <<"/">>},
                          {max_age, 0},
@@ -112,9 +113,10 @@ finish(_State, Ctx) ->
 -spec set_value(Key :: term(), Value :: term()) -> Result :: term().
 %% ====================================================================
 set_value(Key, Value) ->
+    Module = get_session_logic_module(),
     SessionID = ?CTX#context.session,
-    Props = lookup_session(SessionID),
-    save_session(SessionID, [{Key, Value} | proplists:delete(Key, Props)], undefined),
+    Props = Module:lookup_session(SessionID),
+    Module:save_session(SessionID, [{Key, Value} | proplists:delete(Key, Props)], undefined),
     Value.
 
 
@@ -128,7 +130,8 @@ set_value(Key, Value) ->
 %% ====================================================================
 get_value(Key, DefaultValue) ->
     try
-        Props = lookup_session(?CTX#context.session),
+        Module = get_session_logic_module(),
+        Props = Module:lookup_session(?CTX#context.session),
         proplists:get_value(Key, Props, DefaultValue)
     catch
         _:_ ->
@@ -157,91 +160,15 @@ create() ->
 -spec clear() -> ok.
 %% ====================================================================
 clear() ->
+    Module = get_session_logic_module(),
     put(?session_valid, false),
-    delete_session(?CTX#context.session),
+    Module:delete_session(?CTX#context.session),
     ok.
+
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
-
-%% save_session/3
-%% ====================================================================
-%% @doc Saves session data under SessionID key (Props), the entry is valid up to given moment (Till).
-%% If Till arg is undefined, the one currently associated with SessionID will be used.
-%% If there is no record of session with id SessionID and Till is unspecified, exception will be thrown.
-%% Till is expressed in number of seconds since epoch.
-%% @end
--spec save_session(SessionID :: binary(), Props :: [tuple()] | undefined, ValidTill :: integer() | undefined) -> ok | no_return().
-%% ====================================================================
-save_session(SessionID, Props, TillArg) ->
-    Till = case TillArg of
-               undefined ->
-                   case ets:lookup(?ets_name, SessionID) of
-                       [{SessionID, _, CurrentTill}] ->
-                           CurrentTill;
-                       _ ->
-                           throw("session expiration not specified")
-                   end;
-               _ ->
-                   TillArg
-           end,
-
-    delete_session(SessionID),
-    ets:insert(?ets_name, {SessionID, Props, Till}),
-    ok.
-
-
-%% lookup_session/1
-%% ====================================================================
-%% @doc Lookups a session by given SessionID key. On success, returns a proplist -
-%% session data, or undefined if given session does not exist.
-%% @end
--spec lookup_session(SessionID :: binary()) -> Props :: [tuple()] | undefined.
-%% ====================================================================
-lookup_session(SessionID) ->
-    ?dump({lookup_session, SessionID}),
-    case SessionID of
-        ?no_session_cookie ->
-            undefined;
-        undefined ->
-            undefined;
-        _ ->
-            case ets:lookup(?ets_name, SessionID) of
-                [{SessionID, Props, Till}] ->
-                    % Check if the session isn't outdated
-                    {Megaseconds, Seconds, _} = now(),
-                    Now = Megaseconds * 1000000 + Seconds,
-                    case Till > Now of
-                        true ->
-                            Props;
-                        false ->
-                            delete_session(SessionID),
-                            undefined
-                    end;
-                _ ->
-                    undefined
-            end
-    end.
-
-
-%% delete_session/1
-%% ====================================================================
-%% @doc Deletes a session by SessionID key.
-%% @end
--spec delete_session(SessionID :: binary()) -> ok.
-%% ====================================================================
-delete_session(SessionID) ->
-    case SessionID of
-        ?no_session_cookie ->
-            ok;
-        undefined ->
-            ok;
-        _ ->
-            ets:delete(?ets_name, SessionID),
-            ok
-    end.
-
 
 %% random_id/0
 %% ====================================================================
@@ -251,3 +178,18 @@ delete_session(SessionID) ->
 %% ====================================================================
 random_id() ->
     base64:encode(<<(erlang:md5(term_to_binary(now())))/binary, (erlang:md5(term_to_binary(make_ref())))/binary>>).
+
+
+%% get_session_logic_module/0
+%% ====================================================================
+%% @doc Retrieves session_logic module from env.
+%% @end
+-spec get_session_logic_module() -> atom() | no_return().
+%% ====================================================================
+get_session_logic_module() ->
+    case application:get_env(veil_cluster_node, session_logic_module) of
+        {ok, Module} ->
+            Module;
+        _ ->
+            throw("No session logic module in env")
+    end.
---
+++ src/veil_modules/control_panel/utils/session_logic.erl
@@ -0,0 +1,114 @@
+%% ===================================================================
+%% @author Lukasz Opiola
+%% @copyright (C): 2014 ACK CYFRONET AGH
+%% This software is released under the MIT license
+%% cited in 'LICENSE.txt'.
+%% @end
+%% ===================================================================
+%% @doc: This module implements session_logic_behaviour and exports an
+%% API for persisting GUI sessions.
+%% @end
+%% ===================================================================
+
+-module(session_logic).
+-behaviour(session_logic_behaviour).
+-include_lib("stdlib/include/ms_transform.hrl").
+-include("veil_modules/control_panel/common.hrl").
+-include("logging.hrl").
+
+%% n2o session_handler API, with exception of create/0
+-export([save_session/3, lookup_session/1, delete_session/1, clear_expired_sessions/0]).
+
+% ETS name for cookies
+-define(SESSION_ETS, cookies).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+
+%% save_session/3
+%% ====================================================================
+%% @doc Saves session data under SessionID key (Props), the entry is valid up to given moment (Till).
+%% If Till arg is undefined, the one currently associated with SessionID will be used.
+%% If there is no record of session with id SessionID and Till is unspecified, exception will be thrown.
+%% Till is expressed in number of seconds since epoch.
+%% @end
+-spec save_session(SessionID :: binary(), Props :: [tuple()], ValidTill :: integer() | undefined) -> ok | no_return().
+%% ====================================================================
+save_session(SessionID, Props, TillArg) ->
+    Till = case TillArg of
+               undefined ->
+                   case ets:lookup(?SESSION_ETS, SessionID) of
+                       [{SessionID, _, CurrentTill}] ->
+                           CurrentTill;
+                       _ ->
+                           throw("session expiration not specified")
+                   end;
+               _ ->
+                   TillArg
+           end,
+
+    delete_session(SessionID),
+    ets:insert(?SESSION_ETS, {SessionID, Props, Till}),
+    ok.
+
+
+%% lookup_session/1
+%% ====================================================================
+%% @doc Lookups a session by given SessionID key. On success, returns a proplist -
+%% session data, or undefined if given session does not exist.
+%% @end
+-spec lookup_session(SessionID :: binary()) -> Props :: [tuple()] | undefined.
+%% ====================================================================
+lookup_session(SessionID) ->
+    case SessionID of
+        undefined ->
+            undefined;
+        _ ->
+            case ets:lookup(?SESSION_ETS, SessionID) of
+                [{SessionID, Props, Till}] ->
+                    % Check if the session isn't outdated
+                    {Megaseconds, Seconds, _} = now(),
+                    Now = Megaseconds * 1000000 + Seconds,
+                    case Till > Now of
+                        true ->
+                            Props;
+                        false ->
+                            delete_session(SessionID),
+                            undefined
+                    end;
+                _ ->
+                    undefined
+            end
+    end.
+
+
+%% delete_session/1
+%% ====================================================================
+%% @doc Deletes a session by SessionID key.
+%% @end
+-spec delete_session(SessionID :: binary()) -> ok.
+%% ====================================================================
+delete_session(SessionID) ->
+    case SessionID of
+        undefined ->
+            ok;
+        _ ->
+            ets:delete(?SESSION_ETS, SessionID),
+            ok
+    end.
+
+
+%% clear_expired_sessions/0
+%% ====================================================================
+%% @doc Deletes all sessions that have expired.
+%% @end
+-spec clear_expired_sessions() -> ok.
+%% ====================================================================
+clear_expired_sessions() ->
+    {Megaseconds, Seconds, _} = now(),
+    Now = Megaseconds * 1000000 + Seconds,
+    ?dump(Now),
+    ets:select_delete(?SESSION_ETS, [{{'$1', '$2', '$3'}, [{'<', '$3', Now}], ['$_']}]).
+
+ets:fun2ms(fun(N = #food{calories=C}) when C < 600 -> N end
