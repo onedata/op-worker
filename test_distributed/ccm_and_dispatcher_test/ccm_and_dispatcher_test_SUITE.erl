dd8220b7fcc8003c886d4d77d6dc5787d7390cc8
Łukasz Opioła <lukasz.opiola@gmail.com>
2014-06-19 00:13:29 +0200

    VFS-cookies-hotfix, remove debug

--- test_distributed/ccm_and_dispatcher_test/ccm_and_dispatcher_test_SUITE.erl
+++ test_distributed/ccm_and_dispatcher_test/ccm_and_dispatcher_test_SUITE.erl
@@ -487,8 +487,6 @@ validation_test(Config) ->
   {ConAns4, _} = wss:connect('localhost', Port, [{certfile, ?TEST_FILE("certs/proxy_unknown_ca.pem")}, {cacertfile, ?TEST_FILE("certs/proxy_valid.pem")}]),
   ?assertEqual(error, ConAns4),
   {ConAns5, Socket1} = wss:connect('localhost', Port, [{certfile, ?TEST_FILE("certs/proxy_valid.pem")}, {cacertfile, ?TEST_FILE("certs/proxy_valid.pem")}]),
-
-  ct:print("nie lubie websoketow: ~p~n", [{ConAns5, Socket1}]),
   ?assertEqual(ok, ConAns5),
   wss:close(Socket1).
 
