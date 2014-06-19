4b1a910e6c0628908e3a25c3011e726f9825dc9b
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2014-06-19 11:36:03 +0200

    VFS-692 Refactor stress tests code.

--- stress_test/dd/veilcluster_driver_dd.erl
+++ stress_test/dd/veilcluster_driver_dd.erl
@@ -1,5 +1,17 @@
+%% ===================================================================
+%% @author Michał Wrzeszcz
+%% @copyright (C): 2014 ACK CYFRONET AGH
+%% This software is released under the MIT license
+%% cited in 'LICENSE.txt'.
+%% @end
+%% ===================================================================
+%% @doc: This test checks correctness and amount of write operations
+%% using FUSE.
+%% @end
+%% ===================================================================
+
 -module(veilcluster_driver_dd).
--export([new/1, run/4, setup/0]).
+-export([setup/0, new/1, run/4]).
 
 -include("basho_bench.hrl").
 -include("registered_names.hrl").
@@ -7,48 +19,76 @@
 -include("veil_modules/dao/dao.hrl").
 -include("veil_modules/fslogic/fslogic.hrl").
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% TEST DRIVER CALLBACKS %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% ====================================================================
+%% Test driver callbacks
+%% ====================================================================
 
-%% Runs once per each test node at begging of a test (before any new/1 is called)
+%% setup/0
+%% ====================================================================
+%% @doc Runs once per each test node at begging of a test (before any new/1 is called)
+-spec setup() -> no_return().
+%% ====================================================================
 setup() ->
-    ?DEBUG("Test setup~n", []),
     try
+        ?INFO("Test setup~n", []),
         %% Init net kernet in order to connect to cluster
-        NetKernel = net_kernel:start([list_to_atom("tester@" ++ net_adm:localhost()), longnames]),
+        net_kernel:start([list_to_atom("tester@" ++ net_adm:localhost()), longnames]),
         erlang:set_cookie(node(), veil_cluster_node),
-        InitRes =
-            case basho_bench_config:get(client_id) of
-                1 -> catch setup_storages(); %% If its the first test node, initialize cluster
-                _ -> timer:sleep(2000) %% Otherwise wait for main node to finish
-            %% TODO: implement better, more deterministic way of synchronising test nodes (e.g. via ready-ping)
-            end
+        case basho_bench_config:get(client_id) of
+            1 -> catch setup_storages(); %% If its the first test node, initialize cluster
+            _ -> timer:sleep(2000) %% Otherwise wait for main node to finish
+        %% TODO: implement better, more deterministic way of synchronising test nodes (e.g. via ready-ping)
+        end
     catch
-        E1:E2 -> ?DEBUG("Setup error: ~p:~p~n", [E1, E2])
+        E1:E2 -> ?ERROR("Setup error: ~p:~p~n", [E1, E2])
     end.
 
-%% Test initialization
+
+%% new/1
+%% ====================================================================
+%% @doc Creates new worker with integer id
+-spec new(Id :: integer()) -> Result when
+    Result :: {ok, term()} | {error, Reason :: term()}.
+%% ====================================================================
 new(Id) ->
-    ?DEBUG("Test initialization~n", []),
-    VFSRoot = basho_bench_config:get(veilfs_root),
-    Dir = VFSRoot ++ "/stress_test_" ++ basho_bench_config:get(build_id),
-    make_dir(filename:split(Dir)),
-    File = Dir ++ "/file_" ++ integer_to_list(Id),
+    try
+        ?INFO("Initializing worker with id: ~p~n", [Id]),
+        VFSRoot = basho_bench_config:get(veilfs_root),
+        Dir = VFSRoot ++ "/stress_test_" ++ basho_bench_config:get(build_id),
+        File = Dir ++ "/file_" ++ integer_to_list(Id),
 
-    %% Open test file (each test process gets different file name like "file_Id")
-    Device = case open_helper(File, {error, first_try}, 20) of
-                 {error, Reason} ->
-                     ?ERROR("Initialization error: ~p", [Reason]),
-                     Reason;
-                 IO -> IO
-             end,
-    BlockSize = basho_bench_config:get(block_size),
-    Data = [0 || _X <- lists:seq(1, 1024 * BlockSize)],
-    {ok, {Device, 0, Data}}.
+        case make_dir(Dir) of
+            ok ->
+                %% Open test file (each test process gets different file name like "file_Id")
+                Device = case open_helper(File, {error, first_try}, 20) of
+                             {error, Reason} ->
+                                 ?ERROR("Can not open file: ~p: ~p", [File, Reason]),
+                                 Reason;
+                             IO -> IO
+                         end,
+                BlockSize = basho_bench_config:get(block_size),
+                Data = [0 || _X <- lists:seq(1, 1024 * BlockSize)],
+                Args = {Device, 0, Data},
+                ?INFO("Worker with id: ~p initialized successfully with arguments: ~p", [Id, Args]),
+                {ok, {Device, 0, Data}};
+            {error, Reason} ->
+                ?ERROR("Can not create directory: ~p: ~p~n", [Dir, Reason]),
+                {error, Reason}
+        end
+    catch
+        E1:E2 ->
+            ?ERROR("Initialization error for worker with id: ~p: ~p", [Id, {E1, E2}]),
+            {error, {E1, E2}}
+    end.
 
 
-%% Only 'write' action is implemented right now
+%% run/4
+%% ====================================================================
+%% @doc Runs an operation using one of workers
+%% Currently only 'write' operation is implemented
+-spec run(Operation :: atom(), KeyGen :: fun(), ValueGen :: fun(), State :: term()) -> Result when
+    Result :: {ok, NewState :: term()} | {error, Reason :: term(), NewState :: term()}.
+%% ====================================================================
 run(write, _KeyGen, _ValueGen, {Dev, _Offset, _Data} = State) when is_atom(Dev) ->
     timer:sleep(1000), %% Dont generate more then one error per sec when open/2 is failing
     {error, {open, Dev}, State};
@@ -57,58 +97,115 @@ run(write, _KeyGen, _ValueGen, {Dev, Offset, Data}) ->
     case file:pwrite(Dev, 0, Data) of
         ok -> {ok, NewState};
         {error, Reason} ->
-            ?DEBUG("Run error: (file: ~p, offset: ~p): ~p", [Dev, Offset, Reason]),
+            ?ERROR("Run error: (file: ~p, offset: ~p): ~p", [Dev, Offset, Reason]),
             {error, Reason, NewState}
     end;
-run(Action, _KeyGen, _ValueGen, State) ->
-    ?ERROR("Unknown action ~p with state ~p", [Action, State]),
-    {error, unknown_action}.
+run(Operation, _KeyGen, _ValueGen, State) ->
+    ?ERROR("Unknown operation ~p in state ~p", [Operation, State]),
+    {error, unknown_operation, State}.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% PRIVETE HELPER METHODS %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% Register test user and configure storages on cluster
+%% ====================================================================
+%% Helper functions
+%% ====================================================================
+
+%% setup_storages/0
+%% ====================================================================
+%% @doc Register test user and configure storages on cluster
+-spec setup_storages() -> no_return().
+%% ====================================================================
 setup_storages() ->
-    ?DEBUG("Storage setup~n", []),
     Cert = basho_bench_config:get(cert_file),
     [Host | _] = basho_bench_config:get(cluster_hosts),
     Groups = #fuse_group_info{name = ?CLUSTER_FUSE_ID, storage_helper = #storage_helper_info{name = "DirectIO", init_args = ["/mnt/gluster"]}},
-    rpc:call(map_hostname(Host), os, cmd, ["rm -rf /mnt/gluster/*"]),
-    rpc:call(map_hostname(Host), fslogic_storage, insert_storage, ["ClusterProxy", [], [Groups]]),
-    rpc:call(map_hostname(Host), user_logic, create_user, ["veilfstestuser", "Test Name", [], "test@test.com", [get_dn(Cert)]]).
+    Worker = map_hostname(Host),
+    {ok, DN} = get_dn(Cert),
 
-%% Gets rDN list compatibile user_logic:create_user from PEM file
+    case rpc:call(Worker, os, cmd, ["rm -rf /mnt/gluster/*"]) of
+        "" -> ok;
+        Other -> throw(io_lib:fwrite("Can not remove files from storage: ~p", [Other]))
+    end,
+
+    case rpc:call(Worker, fslogic_storage, insert_storage, ["ClusterProxy", [], [Groups]]) of
+        {ok, _} -> ok;
+        Other -> throw(io_lib:fwrite("Can not insert storage: ~p", [Other]))
+    end,
+
+    case rpc:call(Worker, user_logic, create_user, ["veilfstestuser", "Test Name", [], "test@test.com", [DN]]) of
+        {ok, _} -> ok;
+        Other -> throw(io_lib:fwrite("Can not add test user: ~p", [Other]))
+    end.
+
+
+%% get_dn/1
+%% ====================================================================
+%% @doc Gets rDN list compatibile user_logic:create_user from PEM file
+-spec get_dn(PEMFile :: string()) -> Result when
+    Result :: {ok, DN :: string()}.
+%% ====================================================================
 get_dn(PEMFile) ->
-    {ok, PemBin} = file:read_file(PEMFile),
-    Cert = public_key:pem_decode(PemBin),
-    [Leaf | Chain] = [public_key:pkix_decode_cert(DerCert, otp) || {'Certificate', DerCert, _} <- Cert],
-    {ok, EEC} = gsi_handler:find_eec_cert(Leaf, Chain, gsi_handler:is_proxy_certificate(Leaf)),
-    {rdnSequence, Rdn} = gsi_handler:proxy_subject(EEC),
-    {ok, DnString} = user_logic:rdn_sequence_to_dn_string(Rdn),
-    DnString.
+    try
+        {ok, PemBin} = file:read_file(PEMFile),
+        Cert = public_key:pem_decode(PemBin),
+        [Leaf | Chain] = [public_key:pkix_decode_cert(DerCert, otp) || {'Certificate', DerCert, _} <- Cert],
+        {ok, EEC} = gsi_handler:find_eec_cert(Leaf, Chain, gsi_handler:is_proxy_certificate(Leaf)),
+        {rdnSequence, Rdn} = gsi_handler:proxy_subject(EEC),
+        user_logic:rdn_sequence_to_dn_string(Rdn)
+    catch
+        _:Error -> throw(io_lib:format("Can not get dn: ~p", [Error]))
+    end.
 
-%% Opens requested file retring when error occurs
+
+%% open_helper/3
+%% ====================================================================
+%% @doc Opens requested file retring when error occurs
+-spec open_helper(File :: string(), {ok, IO :: pid()} | {error, Error :: term}, Retry :: integer()) -> Result when
+    Result :: pid() | {error | open_error}.
+%% ====================================================================
 open_helper(_, {ok, IO}, _) ->
     IO;
 open_helper(File, {error, _Error}, Retry) when Retry > 0 ->
     {_, _, M} = now(),
     timer:sleep(M rem 100),
-    ?DEBUG("Open helper error ~p (~p), retring in ~p", [_Error, Retry, M rem 100]),
+    ?ERROR("Open helper error ~p (~p), retring in ~p", [_Error, Retry, M rem 100]),
     open_helper(File, file:open(File, [write, raw]), Retry - 1);
 open_helper(_, _, _) ->
     {error, open_failed}.
 
-%% Maps hostname to node name
+
+%% map_hostname/1
+%% ====================================================================
+%% @doc Maps machine hostname to worker name
+-spec map_hostname(Hostname :: string()) -> Result when
+    Result :: atom() | no_return().
+%% ====================================================================
 map_hostname("149.156.10.162") -> 'worker@veil-d01.grid.cyf-kr.edu.pl';
 map_hostname("149.156.10.163") -> 'worker@veil-d02.grid.cyf-kr.edu.pl';
 map_hostname("149.156.10.164") -> 'worker@veil-d03.grid.cyf-kr.edu.pl';
-map_hostname("149.156.10.165") -> 'worker@veil-d04.grid.cyf-kr.edu.pl'.
+map_hostname("149.156.10.165") -> 'worker@veil-d04.grid.cyf-kr.edu.pl';
+map_hostname(Other) -> throw(io_lib:fwrite("Unknown hostname: ~p", [Other])).
 
-%% Create directory with parent directories
-make_dir([Root | Leafs]) ->
-    make_dir(Root, Leafs).
 
+%% make_dir/1
+%% ====================================================================
+%% @doc Creates directory with parent directories
+-spec make_dir(Dir :: string()) -> ok | {error, Reason :: term()}.
+%% ====================================================================
+make_dir(Dir) ->
+    [Root | Leafs] = filename:split(Dir),
+    case make_dir(Root, Leafs) of
+        ok -> ok;
+        {error, eexist} -> ok;
+        Other -> Other
+    end.
+
+
+%% make_dir/1
+%% ====================================================================
+%% @doc Creates directory with parent directories.
+%% Should not be used directly, use make_dir/1 instead.
+-spec make_dir(Root :: string(), Leafs :: [string()]) -> ok | {error, Reason :: term()}.
+%% ====================================================================
 make_dir(Root, []) ->
     file:make_dir(Root);
 make_dir(Root, [Leaf | Leafs]) ->
--- stress_test/limit/veilcluster_driver_limit.erl
+++ stress_test/limit/veilcluster_driver_limit.erl
@@ -1,6 +1,6 @@
 %% ===================================================================
 %% @author Michał Wrzeszcz
-%% @copyright (C): 2013 ACK CYFRONET AGH
+%% @copyright (C): 2014 ACK CYFRONET AGH
 %% This software is released under the MIT license
 %% cited in 'LICENSE.txt'.
 %% @end
@@ -15,34 +15,52 @@
 
 -include("basho_bench.hrl").
 
-new(Id) ->
-    ?DEBUG("limitlog: test started with id: ~p~n", [Id]),
-    Hosts = basho_bench_config:get(cluster_erlang_nodes),
-    try
-      ?DEBUG("limitlog: hosts: ~p~n", [Hosts]),
-      H1 = os:cmd("hostname"),
-      H2 = string:substr(H1, 1, length(H1) - 1),
-      ?DEBUG("limitlog: host name: ~p~n", [H2]),
-      Ans1 = net_kernel:start([list_to_atom("tester@" ++ H2), longnames]),
-      ?DEBUG("limitlog: net_kernel ans: ~p~n", [Ans1]),
-      Ans2 = erlang:set_cookie(node(), veil_cluster_node),
-      ?DEBUG("limitlog: set_cookie ans: ~p~n", [Ans2])
-    catch
-      E1:E2 -> ?DEBUG("limitlog: init error: ~p:~p~n", [E1, E2])
-    end,
-    {ok, Hosts}.
+%% ====================================================================
+%% Test driver callbacks
+%% ====================================================================
 
-run(_Action, KeyGen, _ValueGen, Hosts) ->
-    Host = lists:nth((KeyGen() rem length(Hosts)) + 1 , Hosts),
-    ?DEBUG("limitlog: run func started for host ~p~n", [Host]),
-    NewState = Hosts,
-    Ans = try
-      case net_adm:ping(Host) of
-        pong -> {ok, NewState};
-        pang -> {error, pang, NewState}
-      end
+%% new/1
+%% ====================================================================
+%% @doc Creates new worker with integer id
+-spec new(Id :: integer()) -> Result when
+    Result :: {ok, term()} | {error, Reason :: term()}.
+%% ====================================================================
+new(Id) ->
+    try
+        ?INFO("Initializing worker with id: ~p~n", [Id]),
+        Hosts = basho_bench_config:get(cluster_erlang_nodes),
+
+        H1 = os:cmd("hostname -f"),
+        H2 = string:substr(H1, 1, length(H1) - 1),
+        {ok, _} = net_kernel:start([list_to_atom("tester@" ++ H2), longnames]),
+        true = erlang:set_cookie(node(), veil_cluster_node),
+
+        ?INFO("Worker with id: ~p initialized successfully with arguments: ~p", [Id, Hosts]),
+        {ok, Hosts}
     catch
-      E1:E2 -> {error, E1, E2, NewState}
-    end,
-    ?DEBUG("limitlog: ping ans: ~p~n", [Ans]),
-    Ans.
+        E1:E2 ->
+            ?ERROR("Initialization error for worker with id: ~p: ~p", [Id, {E1, E2}]),
+            {error, {E1, E2}}
+    end.
+
+
+%% run/4
+%% ====================================================================
+%% @doc Runs an operation using one of workers
+-spec run(Operation :: atom(), KeyGen :: fun(), ValueGen :: fun(), State :: term()) -> Result when
+    Result :: {ok, NewState :: term()} | {error, Reason :: term(), NewState :: term()}.
+%% ====================================================================
+run(_Operation, KeyGen, _ValueGen, Hosts) ->
+    try
+        Host = lists:nth((KeyGen() rem length(Hosts)) + 1, Hosts),
+        NewState = Hosts,
+
+        case net_adm:ping(Host) of
+            pong -> {ok, NewState};
+            pang -> {error, pang, NewState}
+        end
+    catch
+        E1:E2 ->
+            ?ERROR("Run error: ~p", [{E1, E2}]),
+            {error, {E1, E2}, NewState}
+    end.
