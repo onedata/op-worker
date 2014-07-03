f79686446236ba73f2637bf1d117ecb526e769eb
Łukasz Opioła <lukasz.opiola@gmail.com>
2014-07-03 15:55:11 +0200

    VFS-608, merge release to develop

--- include/veil_modules/dao/dao_external.hrl
+++ include/veil_modules/dao/dao_external.hrl
@@ -29,7 +29,7 @@
 %% where 'record_name' is the name of the record. 'some_record' is an example.
 -define(dao_record_info(R),
     case R of
-        some_record         -> ?record_info_gen(some_record);   %example record from dao common.hrl
+        some_record         -> ?record_info_gen(some_record);
         cm_state            -> ?record_info_gen(cm_state);
         host_state          -> ?record_info_gen(host_state);
         node_state          -> ?record_info_gen(node_state);
@@ -47,6 +47,26 @@
     %next_record        -> ?record_info_gen(next_record);
         _ -> {error, unsupported_record}
     end).
+
+
+%% Record-wrapper for regular records that needs to be saved in DB. Adds UUID and Revision info to each record.
+%% `uuid` is document UUID, `rev_info` is documents' current revision number
+%% `record` is an record representing this document (its data), `force_update` is a flag
+%% that forces dao:save_record/1 to update this document even if rev_info isn't valid or up to date.
+-record(veil_document, {uuid = "", rev_info = 0, record = none, force_update = false}).
+
+%% Those records represent view result Each #view_resault contains list of #view_row.
+%% If the view has been queried with `include_docs` option, #view_row.doc will contain #veil_document, therefore
+%% #view_row.id == #view_row.doc#veil_document.uuid. Unfortunately wrapping demanded record in #veil_document is
+%% necessary because we may need revision info for that document.
+-record(view_row, {id = "", key = "", value = 0, doc = none}).
+-record(view_result, {total = 0, offset = 0, rows = []}).
+
+%% These records allows representing databases, design documents and their views.
+%% Used in DAO initial configuration in order to easily setup/update views in database.
+-record(db_info, {name = "", designs = []}).
+-record(design_info, {name = "", version = 0, views = []}).
+-record(view_info, {name = "", design = "", db_name = ""}).
 
 %% ====================================================================
 %% DB definitions
@@ -70,6 +90,7 @@
 -define(USER_BY_LOGIN_DESIGN_NAME, "user_by_login").
 -define(USER_BY_EMAIL_DESIGN_NAME, "user_by_email").
 -define(USER_BY_DN_DESIGN_NAME, "user_by_dn").
+-define(USER_BY_UNVERIFIED_DN_DESIGN_NAME, "user_by_unverified_dn").
 -define(USER_BY_UID_DESIGN_NAME, "user_by_uid").
 -define(SHARE_BY_FILE_DESIGN_NAME, "share_by_file").
 -define(SHARE_BY_USER_DESIGN_NAME, "share_by_user").
@@ -92,6 +113,7 @@
 -define(USER_BY_EMAIL_VIEW, #view_info{name = "user_by_email", design = ?USER_BY_EMAIL_DESIGN_NAME, db_name = ?USERS_DB_NAME}).
 -define(USER_BY_LOGIN_VIEW, #view_info{name = "user_by_login", design = ?USER_BY_LOGIN_DESIGN_NAME, db_name = ?USERS_DB_NAME}).
 -define(USER_BY_DN_VIEW, #view_info{name = "user_by_dn", design = ?USER_BY_DN_DESIGN_NAME, db_name = ?USERS_DB_NAME}).
+-define(USER_BY_UNVERIFIED_DN_VIEW, #view_info{name = "user_by_unverified_dn", design = ?USER_BY_UNVERIFIED_DN_DESIGN_NAME, db_name = ?USERS_DB_NAME}).
 -define(USER_BY_UID_VIEW, #view_info{name = "user_by_uid", design = ?USER_BY_UID_DESIGN_NAME, db_name = ?USERS_DB_NAME}).
 
 -define(SHARE_BY_FILE_VIEW, #view_info{name = "share_by_file", design = ?SHARE_BY_FILE_DESIGN_NAME, db_name = ?FILES_DB_NAME}).
@@ -106,23 +128,34 @@
 -define(EXPIRED_FUSE_SESSIONS_VIEW, #view_info{name = "expired_fuse_sessions", design = ?FUSE_SESSIONS_DESIGN_NAME, db_name = ?SYSTEM_DB_NAME}).
 -define(FUSE_SESSIONS_BY_USER_ID_VIEW, #view_info{name = "fuse_sessions_by_user_id", design = ?FUSE_SESSIONS_DESIGN_NAME, db_name = ?SYSTEM_DB_NAME}).
 
+%% Others
+-define(RECORD_INSTANCES_DOC_PREFIX, "record_instances_").
+-define(RECORD_FIELD_BINARY_PREFIX, "__bin__: ").
+-define(RECORD_FIELD_ATOM_PREFIX, "__atom__: ").
+-define(RECORD_FIELD_PID_PREFIX, "__pid__: ").
+-define(RECORD_TUPLE_FIELD_NAME_PREFIX, "tuple_field_").
+-define(RECORD_META_FIELD_NAME, "record__").
+
 %% List of all used databases :: [string()]
 -define(DB_LIST, [?SYSTEM_DB_NAME, ?FILES_DB_NAME, ?DESCRIPTORS_DB_NAME, ?USERS_DB_NAME]).
 %% List of all used views :: [#view_info]
 -define(VIEW_LIST, [?FILE_TREE_VIEW, ?WAITING_FILES_TREE_VIEW, ?FILE_SUBDIRS_VIEW, ?FD_BY_FILE_VIEW, ?FD_BY_EXPIRED_BEFORE_VIEW, ?ALL_STORAGE_VIEW,
     ?FILES_BY_UID_AND_FILENAME, ?FILE_META_BY_TIMES, ?FILES_BY_META_DOC,
-    ?USER_BY_EMAIL_VIEW, ?USER_BY_LOGIN_VIEW, ?USER_BY_DN_VIEW, ?USER_BY_UID_VIEW, ?STORAGE_BY_ID_VIEW,
+    ?USER_BY_EMAIL_VIEW, ?USER_BY_LOGIN_VIEW, ?USER_BY_DN_VIEW, ?USER_BY_UNVERIFIED_DN_VIEW, ?USER_BY_UID_VIEW, ?STORAGE_BY_ID_VIEW,
     ?SHARE_BY_FILE_VIEW, ?SHARE_BY_USER_VIEW, ?USER_FILES_NUMBER_VIEW, ?USER_FILES_SIZE_VIEW, ?GROUP_FILES_NUMBER_VIEW,
     ?FUSE_CONNECTIONS_VIEW, ?EXPIRED_FUSE_SESSIONS_VIEW, ?FUSE_SESSIONS_BY_USER_ID_VIEW]).
 %% Default database name
 -define(DEFAULT_DB, lists:nth(1, ?DB_LIST)).
+
+%% Session cleanup burst size
+-define(SESSION_CLEANUP_BURST_SIZE,1000).
 
 %% Do not try to read this macro (3 nested list comprehensions). All it does is:
 %% Create an list containing #db_info structures base on ?DB_LIST
 %% Inside every #db_info, list of #design_info is created based on views list (?VIEW_LIST)
 %% Inside every #design_info, list of #view_info is created based on views list (?VIEW_LIST)
 %% Such structural representation of views, makes it easier to initialize views in DBMS
-%% WARNING: Do not evaluate this macro anywhere but dao_worker:init/cleanup because it's
+%% WARNING: Do not evaluate this macro anywhere but dao:init/cleanup because it's
 %% potentially slow - O(db_count * view_count^2)
 -define(DATABASE_DESIGN_STRUCTURE, [#db_info{name = DbName,
     designs = [#design_info{name = DesignName,
@@ -130,5 +163,4 @@
             Design == DesignName, DbName2 == DbName]
     } || #view_info{db_name = DbName1, design = DesignName} <- ?VIEW_LIST, DbName1 == DbName]
 } || DbName <- ?DB_LIST]).
-
 -endif.
