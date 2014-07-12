80740a77747e79badda6523742184a86b6ecc5da
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2014-07-12 22:11:03 +0200

    VFS-719 Fix rpm build

--- submodules.sh
+++ submodules.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 git submodule update
+
 cd veilprotocol
 git fetch
 git checkout master
 git pull
 cd ..
+
 cd veilhelpers
 git fetch
 git checkout master
 git pull
 cd ..
+
 cd bigcouchdb
 git fetch
 git checkout master
