ecb0928015a150efb2939f031f7c537f95e6c68a
Konrad Zemek <konrad.zemek@gmail.com>
2014-07-15 12:33:32 +0200

    VFS-727 Add a MessageCreator class.
    Former-commit-id: dcb605c6d69dd901eb54fbd5332a32e79bfb3385

--- include/communication/messageCreator.h
+++ include/communication/messageCreator.h
@@ -1,21 +1,26 @@
 /**
- * @file mailbox.cc
+ * @file messageCreator.h
  * @author Konrad Zemek
  * @copyright (C) 2014 ACK CYFRONET AGH
  * @copyright This software is released under the MIT license cited in 'LICENSE.txt'
  */
 
-#include "communication/mailbox.h"
+#ifndef VEILHELPERS_MESSAGE_CREATOR_H
+#define VEILHELPERS_MESSAGE_CREATOR_H
+
 
 namespace veil
 {
 namespace communication
 {
 
-void Mailbox::onMessage(std::string payload)
+class MessageCreator
 {
-
-}
+public:
+};
 
 } // namespace communication
 } // namespace veil
+
+
+#endif // VEILHELPERS_MESSAGE_CREATOR_H
---
+++ src/communication/messageCreator.cc
@@ -0,0 +1,16 @@
+/**
+ * @file messageCreator.cc
+ * @author Konrad Zemek
+ * @copyright (C) 2014 ACK CYFRONET AGH
+ * @copyright This software is released under the MIT license cited in 'LICENSE.txt'
+ */
+
+#include "communication/messageCreator.h"
+
+namespace veil
+{
+namespace communication
+{
+
+} // namespace communication
+} // namespace veil
