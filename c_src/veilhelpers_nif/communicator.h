0adfaec3e110820ebf2873d2f009a30f32e95e18
Rafał Słota <roxeon@gmail.com>
2014-07-25 14:02:36 +0200

    VFS-754: update protocol

---
+++ c_src/veilhelpers_nif/communicator.h
@@ -0,0 +1,28 @@
+/*********************************************************************
+*  @author Rafal Slota
+*  @copyright (C): 2014 ACK CYFRONET AGH
+*  This software is released under the MIT license
+*  cited in 'LICENSE.txt'.
+*********************************************************************/
+
+#ifndef COMMUNICATOR_H
+#define COMMUNICATOR_H
+
+namespace veil
+{
+namespace cluster
+{
+
+class ProviderEndpointPool
+{
+public:
+
+
+protected:
+
+};
+
+} // namespace cluster
+} // namespace veil
+
+#endif // COMMUNICATOR_H
