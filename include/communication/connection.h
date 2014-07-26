4f7ca08c81e816c011e331a090fc5ace544ee33e
Konrad Zemek <konrad.zemek@gmail.com>
2014-07-26 16:21:36 +0200

    VFS-740 Document Connection class.
    Former-commit-id: 21da30c08e591bb28ef8657ee9a06970c89c86cf

--- include/communication/connection.h
+++ include/communication/connection.h
@@ -18,22 +18,62 @@ namespace veil
 namespace communication
 {
 
+/**
+ * The Connection class represents a single network-style connection.
+ */
 class Connection
 {
 public:
+    /**
+     * Constructor.
+     * @param onMessageCallback Callback to be called on received message.
+     * @param onFailCallback Callback to be called on connection open failure.
+     * @param onOpenCallback Callback to be called on connection open.
+     * @param onErrorCallback Callback to be called on open connection's error.
+     */
     Connection(std::function<void(const std::string&)> onMessageCallback,
                std::function<void(Connection&, std::exception_ptr)> onFailCallback,
                std::function<void(Connection&)> onOpenCallback,
                std::function<void(Connection&)> onErrorCallback);
 
+    /**
+     * Destructor.
+     * Closes the connections.
+     */
     virtual ~Connection();
 
+    /**
+     * Send a message through the connection.
+     * @param payload The message to send.
+     */
     virtual void send(const std::string &payload) = 0;
 
 protected:
+    /**
+     * The @p onMessageCallback callback set in @c Connection::Connection(),
+     * bound to *this.
+     * @param payload Data of the received message.
+     */
     std::function<void(const std::string&)> m_onMessageCallback;
+
+    /**
+     * The @p onFailCallback callback set in @c Connection::Connection(),
+     * bound to *this.
+     * @param exceptionPtr The exception to set as a possible reason for
+     * communication failure.
+     */
     std::function<void(std::exception_ptr)> m_onFailCallback;
+
+    /**
+     * The @p onOpenCallback callback set in @c Connection::Connection(),
+     * bound to *this.
+     */
     std::function<void()> m_onOpenCallback;
+
+    /**
+     * The @p onErrorCallback callback set in @c Connection::Connection(),
+     * bound to *this.
+     */
     std::function<void()> m_onErrorCallback;
 };
 
