01d501ce70f4255408acb539c82a35c8406be0de
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2014-08-27 17:54:50 +0200

    VFS-797 Open VeilCluster DB.

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -151,6 +151,7 @@ function start_cluster {
     db_nodes=`echo "$CLUSTER_DB_NODES" | tr ";" "\n"`
     for db_node in ${db_nodes}; do
         db_hosts="\\\"`strip_login "$db_node"`\\\",$db_hosts"
+        ssh $db_node "sed -i -e \"s/^bind_address = [0-9\.]*/bind_address = 0.0.0.0/\" /opt/veil/files/database_node/etc/default.ini" || error "Cannot set VeilCluster DB bind address on $db_node."
     done
     db_hosts=`echo "$db_hosts" | sed -e 's/.$//'`
 
