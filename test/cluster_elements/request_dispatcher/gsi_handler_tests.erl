019a5384c005ba849b2c5ad10c27ddf7becff28c
Tomasz Lichon <lichon@student.agh.edu.pl>
2014-08-28 16:20:43 +0200

    protocol update

--- test/cluster_elements/request_dispatcher/gsi_handler_tests.erl
+++
@@ -1,9 +0,0 @@
-%% Copyright
--module(gsi_handler_tests).
-
-%% TODO powinny powstać testy weryfikujące działanie gsi handlar'a
-%% powinny spradzać reakcję dla poprawnego i wadliwego certyfikatu
-%% najlepiej wykonać je z ticketem VFS-138
-
-%% API
--export([]).
