48f7e2734525859118a1b066fba73ce027fda705
Łukasz Opioła <lukasz.opiola@gmail.com>
2014-09-22 12:15:06 +0200

    VFS-807, update ctool ref

--- src/veil_modules/control_panel/cowboy_handlers/n2o_handler.erl
+++ src/veil_modules/control_panel/cowboy_handlers/n2o_handler.erl
@@ -14,7 +14,6 @@
 %% ===================================================================
 
 -module(n2o_handler).
--behaviour(cowboy_http_handler).
 
 -include_lib("veil_modules/control_panel/common.hrl").
 
@@ -22,34 +21,27 @@
 -export([init/3, handle/2, terminate/3]).
 
 % Bridge abstraction
--export([params/1, path/1, request_body/1, headers/1, header/3, response/2, reply/2]).
+-export([params/1, form/1, path/1, request_body/1, headers/1, header/3, response/2, reply/2]).
 -export([cookies/1, cookie/2, cookie/3, cookie/5, delete_cookie/2, peer/1]).
 
-% Handler state record
 -record(state, {headers, body}).
 
-
 %% ====================================================================
-%% cowboy_http_handler API
+%% Cowboy HTTP handler API
 %% ====================================================================
 
-init(_Transport, Req, _Opts) ->
-    {ok, Req, #state{}}.
-
-handle(Req, State) ->
-    {ok, NewReq} = wf_core:run(Req),
-    {ok, NewReq, State}.
-
-terminate(_Reason, _Req, _State) ->
-    ok.
+init(_Transport, Req, _Opts) -> {ok, Req, #state{}}.
+terminate(_Reason, _Req, _State) -> ok.
+handle(Req, State) ->  {ok, NewReq} = n2o_document:run(Req), {ok, NewReq, State}.
 
 
 %% ====================================================================
 %% Cowboy Bridge Abstraction
 %% ====================================================================
 
-params(Req) -> {Params, _NewReq} = cowboy_req:qs_vals(Req), Params.
-path(Req) -> {Path, _NewReq} = cowboy_req:path(Req), Path.
+params(Req) -> cowboy_req:qs_vals(Req).
+form(Req) -> {ok,Params,NewReq} = cowboy_req:body_qs(Req), {Params,NewReq}.
+path(Req) -> {Path,_NewReq} = cowboy_req:path(Req), Path.
 request_body(Req) -> veil_cowboy_bridge:apply(cowboy_req, body, [Req]).
 headers(Req) -> cowboy_req:headers(Req).
 header(Name, Value, Req) -> cowboy_req:set_resp_header(Name, Value, Req).
@@ -61,5 +53,6 @@ cookie(Cookie, Value, Req) -> cookie(Cookie, Value, <<"/">>, 0, Req).
 cookie(Name, Value, Path, TTL, Req) ->
     Options = [{path, Path}, {max_age, TTL}],
     cowboy_req:set_resp_cookie(Name, Value, Options, Req).
-delete_cookie(Cookie, Req) -> cookie(Cookie, <<"">>, <<"/">>, 0, Req).
-peer(Req) -> {{Ip, Port}, Req} = cowboy_req:peer(Req), {Ip, Port}.
+delete_cookie(Cookie,Req) -> cookie(Cookie,<<"">>,<<"/">>,0,Req).
+peer(Req) -> {{Ip,Port},Req} = cowboy_req:peer(Req), {Ip,Port}.
+
