369fdd56c3a5bfde3c678e48a0c3a918f4c56de9
Rafał Słota <roxeon@gmail.com>
2014-11-13 20:03:37 +0100

    VFS-889 docs for dbsync_records

--- src/oneprovider_modules/dbsync/dbsync_records.erl
+++ src/oneprovider_modules/dbsync/dbsync_records.erl
@@ -21,12 +21,25 @@
 -export([doc_to_db/1, get_space_ctx/2]).
 
 
+%% doc_to_db/1
+%% ====================================================================
+%% @doc Shall return database name for given db_document.
+-spec doc_to_db(#db_document{}) -> string() | binary().
+%% ====================================================================
 doc_to_db(#db_document{record = #file{}}) ->
     ?FILES_DB_NAME;
 doc_to_db(#db_document{record = #file_meta{}}) ->
     ?FILES_DB_NAME.
 
 
+%% get_space_ctx/2
+%% ====================================================================
+%% @doc Shall return list of UUIDs and associated #space_info{} record for given document.
+%%      This method should return as many UUID as possible in advance.
+%% @end
+-spec get_space_ctx(#db_document{}, Acc :: [binary()]) ->
+    {ok, {UUIDs :: [binary()], #space_info{}}} | {error, Reason :: any()}.
+%% ====================================================================
 get_space_ctx(#db_document{uuid = "", record = #file{}}, []) ->
     {error, no_space};
 get_space_ctx(#db_document{uuid = UUID, record = #file{extensions = Exts, parent = Parent}} = _Doc, UUIDs) ->
