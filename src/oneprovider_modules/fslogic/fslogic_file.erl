6c2fc5d4e8d4ccd6a9761583689085caf111a028
Tomasz Lichon <lichon@student.agh.edu.pl>
2014-12-10 16:12:12 +0100

    correct mode for remote files

--- src/oneprovider_modules/fslogic/fslogic_file.erl
+++ src/oneprovider_modules/fslogic/fslogic_file.erl
@@ -221,5 +221,6 @@ create_file_location_for_remote_file(FullFileName, FileUuid) ->
 %%     FileBlock = #file_block{file_location_id = LocationId, offset = 0, size = ?FILE_BLOCK_SIZE_INF},
 %%     {ok, _} = dao_lib:apply(dao_vfs, save_file_block, [FileBlock], fslogic_context:get_protocol_version()),
     {SH, StorageFileId} = fslogic_utils:get_sh_and_id(?CLUSTER_FUSE_ID, Storage, FileId, SpaceId, false),
-    ok = storage_files_manager:create(SH, StorageFileId),
+    {ok, #fileattributes{mode = Mode}} = logical_files_manager:getfileattr(FullFileName),
+    ok = storage_files_manager:create(SH, StorageFileId, Mode),
     {ok, {LocationId, StorageFileId}}.
