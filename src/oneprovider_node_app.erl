51e925cb49658cdf0abf1c9a5eb3ec0760b5f4cf
Tomasz Lichon <lichon@student.agh.edu.pl>
2015-01-18 23:35:53 +0100

    oneprovider app reformat + doc adjustment

--- src/oneprovider_node_app.erl
+++ src/oneprovider_node_app.erl
@@ -1,16 +1,17 @@
-%% ===================================================================
-%% @author Michal Wrzeszcz
-%% @copyright (C): 2013 ACK CYFRONET AGH
-%% This software is released under the MIT license
-%% cited in 'LICENSE.txt'.
-%% @end
-%% ===================================================================
-%% @doc: It is the main module of application. It lunches
+%%%--------------------------------------------------------------------
+%%% @author Michal Wrzeszcz
+%%% @copyright (C) 2014 ACK CYFRONET AGH
+%%% This software is released under the MIT license
+%%% cited in 'LICENSE.txt'.
+%%% @end
+%%%--------------------------------------------------------------------
+%%% @doc It is the main module of application. It lunches
 %% supervisor which then initializes appropriate components of node.
-%% @end
-%% ===================================================================
-
+%%% @end
+%%%--------------------------------------------------------------------
 -module(oneprovider_node_app).
+-author("Michal Wrzeszcz").
+
 -behaviour(application).
 
 -include("registered_names.hrl").
@@ -18,9 +19,9 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
+%%%===================================================================
+%%% Application callbacks
+%%%===================================================================
 
 %%--------------------------------------------------------------------
 %% @doc Starts application by supervisor initialization.
@@ -43,7 +44,3 @@ start(_StartType, _StartArgs) ->
 	Result ::  ok.
 stop(_State) ->
   ok.
-
-%% ===================================================================
-%% Internal functions
-%% ===================================================================
--- src/oneprovider_node_sup.erl
+++ src/oneprovider_node_sup.erl
@@ -1,16 +1,17 @@
-%% ===================================================================
-%% @author Michal Wrzeszcz
-%% @copyright (C): 2013 ACK CYFRONET AGH
-%% This software is released under the MIT license 
-%% cited in 'LICENSE.txt'.
-%% @end
-%% ===================================================================
-%% @doc: It is the main supervisor. It starts (as it child) node manager
-%% which initializes node.
-%% @end
-%% ===================================================================
-
+%%%--------------------------------------------------------------------
+%%% @author Michal Wrzeszcz
+%%% @copyright (C) 2013 ACK CYFRONET AGH
+%%% This software is released under the MIT license
+%%% cited in 'LICENSE.txt'.
+%%% @end
+%%%--------------------------------------------------------------------
+%%% @doc It is the main supervisor. It starts (as it child) node manager
+%%% which initializes node.
+%%% @end
+%%%--------------------------------------------------------------------
 -module(oneprovider_node_sup).
+-author("Michal Wrzeszcz").
+
 -include("registered_names.hrl").
 -include("supervision_macros.hrl").
 -behaviour(supervisor).
@@ -21,9 +22,9 @@
 %% Supervisor callbacks
 -export([init/1]).
 
-%% ===================================================================
-%% API functions
-%% ===================================================================
+%%%===================================================================
+%%% API
+%%%===================================================================
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -41,13 +42,20 @@
 start_link(NodeType) ->
     supervisor:start_link({local, ?SUPERVISOR_NAME}, ?MODULE, [NodeType]).
 
-%% ===================================================================
+%%%===================================================================
 %% Supervisor callbacks
-%% ===================================================================
+%%%===================================================================
 
-%% init/1
-%% ====================================================================
-%% @doc <a href="http://www.erlang.org/doc/man/supervisor.html#Module:init-1">supervisor:init/1</a>
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+%%
+%% @end
+%%--------------------------------------------------------------------
 -spec init(Args :: term()) -> Result when
     Result :: {ok, {SupervisionPolicy, [ChildSpec]}} | ignore,
     SupervisionPolicy :: {RestartStrategy, MaxR :: non_neg_integer(), MaxT :: pos_integer()},
