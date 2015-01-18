0481257ecbfea74c18bf5fbafaa2f78d10cb83bf
Tomasz Lichon <lichon@student.agh.edu.pl>
2015-01-18 22:35:31 +0100

    logger_plugin reformat + doc adjustment

--- src/workers/central_logger/logger_plugin.erl
+++ src/workers/central_logger/logger_plugin.erl
@@ -1,27 +1,29 @@
-%% ===================================================================
-%% @author Lukasz Opiola
-%% @copyright (C): 2014 ACK CYFRONET AGH
-%% This software is released under the MIT license
-%% cited in 'LICENSE.txt'.
-%% @end
-%% ===================================================================
-%% @doc: This module implements logger_plugin_behaviour in order
-%% to customize logging in oneprovider application.
-%% @end
-%% ===================================================================
+%%%-------------------------------------------------------------------
+%%% @author Michal Wrzeszcz
+%%% @copyright (C) 2013 ACK CYFRONET AGH
+%%% This software is released under the MIT license
+%%% cited in 'LICENSE.txt'.
+%%% @end
+%%%-------------------------------------------------------------------
+%%% @doc
+%%% This module implements logger_plugin_behaviour in order
+%%% to customize logging in oneprovider application.
+%%% @end
+%%%-------------------------------------------------------------------
 -module(logger_plugin).
+-author("Lukasz Opiola").
+
 -behaviour(logger_plugin_behaviour).
 
-%% logger_plugin_behaviour API
+%% API
 -export([gather_metadata/0]).
 
-
-%% gather_metadata/0
-%% ====================================================================
-%% @doc Should return a list of key, value tuples to be concatenated
+%%--------------------------------------------------------------------
+%% @doc
+%% Should return a list of key, value tuples to be concatenated
 %% to standard log metadata.
 %% @end
+%%--------------------------------------------------------------------
 -spec gather_metadata() -> list().
-%% ====================================================================
 gather_metadata() ->
     [].
