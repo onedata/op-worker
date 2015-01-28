44bfe0aa5b64958a2319d4ec80b3a72c3d034803
Konrad Zemek <konrad.zemek@gmail.com>
2015-01-28 14:01:28 +0100

    VFS-1000 Don't set up a custom cookie for oneprovider.

--- docker/provider_up.py
+++ docker/provider_up.py
@@ -15,22 +15,21 @@ def parse_config(path):
     data = f.read()
     return json.loads(data)
 
-def set_hostname(node, cookie):
+def set_hostname(node, uid):
   parts = list(node.partition('@'))
-  parts[2] = '{name}.{cookie}.dev.docker'.format(name=parts[0], cookie=cookie)
+  parts[2] = '{name}.{uid}.dev.docker'.format(name=parts[0], uid=uid)
   return ''.join(parts)
 
-def tweak_config(config, name):
+def tweak_config(config, name, uid):
   cfg = copy.deepcopy(config)
   cfg['nodes'] = { 'node': cfg['nodes'][name] }
 
   sys_config = cfg['nodes']['node']['sys.config']
-  sys_config['ccm_nodes'] = [set_hostname(n, cookie) for n in sys_config['ccm_nodes']]
-  sys_config['db_nodes']  = [set_hostname(n, cookie) for n in sys_config['db_nodes']]
+  sys_config['ccm_nodes'] = [set_hostname(n, uid) for n in sys_config['ccm_nodes']]
+  sys_config['db_nodes']  = [set_hostname(n, uid) for n in sys_config['db_nodes']]
 
   vm_args = cfg['nodes']['node']['vm.args']
-  vm_args['cookie'] = cookie
-  vm_args['name'] = set_hostname(vm_args['name'], cookie)
+  vm_args['name'] = set_hostname(vm_args['name'], uid)
 
   return cfg
 
@@ -74,11 +73,11 @@ parser.add_argument(
 
 args = parser.parse_args()
 client = docker.Client()
-cookie = str(int(time.time()))
+uid = str(int(time.time()))
 
 config = parse_config(args.config_path)
 config['config']['target_dir'] = '/root/bin'
-configs = [tweak_config(config, node) for node in config['nodes']]
+configs = [tweak_config(config, node, uid) for node in config['nodes']]
 
 pull_image(args.image)
 pull_image('crosbymichael/skydns')
@@ -87,17 +86,17 @@ pull_image('crosbymichael/skydock')
 skydns = client.create_container(
   image='crosbymichael/skydns',
   detach=True,
-  name='skydns_{cookie}'.format(cookie=cookie),
+  name='skydns_{uid}'.format(uid=uid),
   command='-nameserver 8.8.8.8:53 -domain docker')
 client.start(container=skydns)
 
 skydock = client.create_container(
   image='crosbymichael/skydock',
   detach=True,
-  name='skydock_{cookie}'.format(cookie=cookie),
+  name='skydock_{uid}'.format(uid=uid),
   volumes=['/createService.js', '/docker.sock'],
   command='-ttl 30 -environment dev -s /docker.sock -domain docker -name \
-           skydns_{cookie} -plugins /createService.js'.format(cookie=cookie))
+           skydns_{uid} -plugins /createService.js'.format(uid=uid))
 
 client.start(
   container=skydock,
@@ -108,12 +107,11 @@ skydns_config = client.inspect_container(skydns)
 dns = skydns_config['NetworkSettings']['IPAddress']
 
 output = collections.defaultdict(list)
-output['cookie'] = cookie
 
 for cfg in configs:
   node_type = cfg['nodes']['node']['sys.config']['node_type']
   node_name = cfg['nodes']['node']['vm.args']['name']
-  output[node_type].append(node_name)
+  output['op_{type}_nodes'.format(type=node_type)].append(node_name)
 
   (name, sep, hostname) = node_name.partition('@')
 
@@ -130,7 +128,7 @@ for cfg in configs:
     stdin_open=True,
     tty=True,
     working_dir='/root/build',
-    name='{name}_{cookie}'.format(name=name, cookie=cookie),
+    name='{name}_{uid}'.format(name=name, uid=uid),
     volumes=['/root/build'],
     command=['bash', '-c', command])
 
