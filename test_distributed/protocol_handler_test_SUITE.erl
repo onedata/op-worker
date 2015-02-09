deaf1199987bf80633ce8a5802ccac86e0109fe5
Tomasz Lichon <lichon@student.agh.edu.pl>
2015-02-09 18:13:27 +0200

    test fix

--- test_distributed/protocol_handler_test_SUITE.erl
+++ test_distributed/protocol_handler_test_SUITE.erl
@@ -31,41 +31,59 @@ all() -> [cert_connection_test, token_connection_test, protobuf_msg_test].
 %% ====================================================================
 
 cert_connection_test(Config) ->
+    % given
     ssl:start(),
     [Worker1, _] = ?config(op_worker_nodes, Config),
+
+    %then
     {ok, Sock} = connect_via_token(Worker1),
     ok = ssl:close(Sock),
     ?assertMatch({error, _}, ssl:connection_info(Sock)),
     ssl:stop().
 
 token_connection_test(Config) ->
+    % given
     ssl:start(),
     [Worker1, _] = ?config(op_worker_nodes, Config),
-    {ok, Sock} = ssl:connect(?GET_HOST(Worker1), 5555, [binary, {packet, 4}, {active, true}]),
-
     TokenAuthMessage = <<"{\"cert\":\"id\"}">>,
+
+    % when
+    {ok, Sock} = ssl:connect(?GET_HOST(Worker1), 5555, [binary, {packet, 4}, {active, true}]),
     ok = ssl:send(Sock, TokenAuthMessage),
+
+    % then
     ?assertMatch({ok, _}, ssl:connection_info(Sock)),
     ok = ssl:close(Sock),
     ?assertMatch({error, _}, ssl:connection_info(Sock)),
     ssl:stop().
 
 protobuf_msg_test(Config) ->
+    % given
     ssl:start(),
     [Worker1, _] = ?config(op_worker_nodes, Config),
-    {ok, Sock} = connect_via_token(Worker1),
-
     ok = rpc:call(Worker1, meck, new, [router, [passthrough, non_strict, unstick, no_link]]),
     ok = rpc:call(Worker1, meck, expect, [router, preroute_message,
-        fun(#client_message{credentials = #credentials{}, client_message = #handshake_request{}}) -> ok end]),
-    Msg = #'ClientMessage'{response_id = 0, client_message = {handshake_request, #'HandshakeRequest'{}}},
+        fun(
+            #client_message{
+                credentials = #credentials{},
+                client_message = #handshake_request{}
+            }
+        ) ->
+            ok
+        end]),
+    Msg = #'ClientMessage'{
+        response_id = 0,
+        client_message = {handshake_request, #'HandshakeRequest'{}}
+    },
     RawMsg = client_messages:encode_msg(Msg),
-    ok = ssl:send(Sock, RawMsg),
-    ?assertMatch({ok, _}, ssl:connection_info(Sock)),
-    true = rpc:call(Worker1, meck, validate, [router]),
-    ok = ssl:send(Sock, <<"non_protobuff">>),
-%%     ?assertMatch({error, _}, ssl:connection_info(Sock)),
 
+    % when
+    {ok, Sock} = connect_via_token(Worker1),
+    ok = ssl:send(Sock, RawMsg),
+
+    %then
+    ?assertEqual(true, rpc:call(Worker1, meck, validate, [router])),
+    ?assertMatch({ok, _}, ssl:connection_info(Sock)),
     ssl:stop().
 
 %%%===================================================================
@@ -73,8 +91,10 @@ protobuf_msg_test(Config) ->
 %%%===================================================================
 init_per_suite(Config) ->
     try
-        test_node_starter:prepare_test_environment(Config, ?TEST_FILE(Config, "env_desc.json"))
-    catch A:B -> ct:print("~p:~p~n~p", [A, B, erlang:get_stacktrace()]) end.
+        test_node_starter:prepare_test_environment(Config, ?TEST_FILE(Config, "env_desc.json"), ?MODULE)
+    catch
+        A:B -> ct:print("~p:~p~n~p", [A, B, erlang:get_stacktrace()])
+    end.
 
 end_per_suite(Config) ->
     test_node_starter:clean_environment(Config).
