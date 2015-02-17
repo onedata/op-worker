e95cfa0829a5ca4be4a534c3cbb2d1ab33cde95c
Łukasz Opioła <lukasz.opiola@gmail.com>
2015-02-17 14:02:19 +0100

    VFS-1046, update bamboos

--- docker/docker.py
+++ docker/docker.py
@@ -1,11 +1,9 @@
-"""A custom library for interacting with Docker through command-line tool."""
-
 import json
 import os
 import sys
 import subprocess
 
-def run(image, docker_host=None, detach=False, dns=[], hostname=None,
+def run(image, docker_host=None, detach=False, dns_list=[], envs={}, hostname=None,
         interactive=False, link={}, tty=False, rm=False, reflect=[],
         volumes=[], name=None, workdir=None, user=None, run_params=[],
         command=None):
@@ -20,8 +18,11 @@ def run(image, docker_host=None, detach=False, dns=[], hostname=None,
     if detach:
         cmd.append('-d')
 
-    for addr in dns:
+    for addr in dns_list:
         cmd.extend(['--dns', addr])
+
+    for key in envs:
+        cmd.extend(['-e', '{0}={1}'.format(key, envs[key])])
 
     if hostname:
         cmd.extend(['-h', hostname])
@@ -41,8 +42,8 @@ def run(image, docker_host=None, detach=False, dns=[], hostname=None,
     if rm:
         cmd.append('--rm')
 
-    for path in reflect:
-        vol = '{0}:{0}:rw'.format(os.path.abspath(path))
+    for path, read in reflect:
+        vol = '{0}:{0}:{1}'.format(os.path.abspath(path), read)
         cmd.extend(['-v', vol])
 
     for entry in volumes:
@@ -54,7 +55,7 @@ def run(image, docker_host=None, detach=False, dns=[], hostname=None,
             cmd.extend(['-v', entry])
 
     if workdir:
-        cmd.extend(['-w', workdir])
+        cmd.extend(['-w', os.path.abspath(workdir)])
 
     if user:
         cmd.extend(['-u', user])
