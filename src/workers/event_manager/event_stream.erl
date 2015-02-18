e3cbf7b6880cf58f2cb76b67a67bf3508511944c
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2015-02-18 18:26:31 +0100

    VFS-997 Apply recommended changes.

--- src/workers/event_manager/event_stream.erl
+++ src/workers/event_manager/event_stream.erl
@@ -45,7 +45,8 @@
     evt_disp :: pid(),
     evts = [] :: [event_manager:event()],
     spec :: event_stream(),
-    meta :: metadata()
+    meta :: metadata(),
+    emission_reference :: reference()
 }).
 
 %%%===================================================================
@@ -75,11 +76,10 @@ start_link(EvtDisp, SubId, EvtStmSpec) ->
 -spec init(Args :: term()) ->
     {ok, State :: #state{}} | {ok, State :: #state{}, timeout() | hibernate} |
     {stop, Reason :: term()} | ignore.
-init([EvtDisp, SubId, EvtStmSpec]) ->
+init([EvtDisp, SubId, #event_stream{metadata = Meta} = EvtStmSpec]) ->
     process_flag(trap_exit, true),
     gen_server:cast(self(), initialize),
-    {ok, #state{sub_id = SubId, evt_disp = EvtDisp, spec = EvtStmSpec,
-        meta = reset_metadata(EvtStmSpec)}}.
+    {ok, #state{sub_id = SubId, evt_disp = EvtDisp, spec = EvtStmSpec, meta = Meta}}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -109,29 +109,24 @@ handle_call(_Request, _From, State) ->
     {noreply, NewState :: #state{}} |
     {noreply, NewState :: #state{}, timeout() | hibernate} |
     {stop, Reason :: term(), NewState :: #state{}}.
-handle_cast(initialize, #state{sub_id = SubId, evt_disp = EvtDisp,
-    spec = #event_stream{emission_time = EmsTime}} = State) ->
-    case is_integer(EmsTime) of
-        true -> erlang:send_after(EmsTime, self(), periodic_emission);
-        _ -> ok
-    end,
+handle_cast(initialize, #state{sub_id = SubId, evt_disp = EvtDisp} = State) ->
     case gen_server:call(EvtDisp, {event_stream_initialized, SubId}) of
         {ok, #state{} = EvtStmState} ->
             ?info("Event stream reinitialized in state: ~p", [EvtStmState]),
-            {noreply, EvtStmState};
+            {noreply, reset_time_emission(EvtStmState)};
         _ ->
-            {noreply, State}
+            {noreply, reset_time_emission(State)}
     end;
 
 handle_cast({event, Evt}, #state{evts = Evts, meta = Meta, spec = EvtStmSpec} = State) ->
     NewEvts = apply_aggregation_rule(Evt, Evts, EvtStmSpec),
     NewMeta = apply_transition_rule(Evt, Meta, EvtStmSpec),
+    NewState = State#state{evts = NewEvts, meta = NewMeta},
     case apply_emission_rule(NewMeta, EvtStmSpec) of
         true ->
-            emit(NewEvts, EvtStmSpec),
-            {noreply, State#state{evts = [], meta = reset_metadata(EvtStmSpec)}};
+            {noreply, emit(NewState)};
         false ->
-            {noreply, State#state{evts = NewEvts, meta = NewMeta}}
+            {noreply, NewState}
     end;
 
 handle_cast(_Request, State) ->
@@ -148,16 +143,11 @@ handle_cast(_Request, State) ->
     {noreply, NewState :: #state{}} |
     {noreply, NewState :: #state{}, timeout() | hibernate} |
     {stop, Reason :: term(), NewState :: #state{}}.
-handle_info(periodic_emission, #state{evts = [],
-    spec = #event_stream{emission_time = EmsTime}} = State) ->
-    erlang:send_after(EmsTime, self(), periodic_emission),
-    {noreply, State};
+handle_info({periodic_emission, Ref}, #state{emission_reference = Ref} = State) ->
+    {noreply, emit(State)};
 
-handle_info(periodic_emission, #state{evts = Evts,
-    spec = #event_stream{emission_time = EmsTime} = EvtStmSpec} = State) ->
-    emit(Evts, EvtStmSpec),
-    erlang:send_after(EmsTime, self(), periodic_emission),
-    {noreply, State#state{evts = [], meta = reset_metadata(EvtStmSpec)}};
+handle_info({periodic_emission, _}, State) ->
+    {noreply, State};
 
 handle_info(_Info, State) ->
     ?log_bad_request(_Info),
@@ -196,24 +186,37 @@ code_change(_OldVsn, State, _Extra) ->
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
-%% Returns initial value of stream metadata.
+%% Executes stream event handlers on aggregated events stored in the stream.
+%% Resets periodic emission of events.
 %% @end
 %%--------------------------------------------------------------------
--spec reset_metadata(EvtStmSpec :: event_stream()) -> metadata().
-reset_metadata(#event_stream{metadata = Meta}) ->
-    Meta.
+-spec emit(State :: #state{}) -> NewState :: #state{}.
+emit(#state{evts = []} = State) ->
+    reset_time_emission(State);
+emit(#state{evts = Evts, spec = #event_stream{handlers = Handlers,
+    metadata = Meta}} = State) ->
+    lists:foreach(fun(Handler) ->
+        Handler(Evts)
+    end, Handlers),
+    reset_time_emission(State#state{evts = [], meta = Meta}).
 
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
-%% Executes stream event handlers on apply_aggregation_ruled events.
+%% Resets the time emission by sending 'periodic_emission' message to itself.
+%% Message is marked with new reference stored in event stream state, so that
+%% event stream can ignore messages with reference different from the one saved
+%% in the state.
 %% @end
 %%--------------------------------------------------------------------
--spec emit(Evts :: [event_manager:event()], EvtStmSpec :: event_stream()) -> ok.
-emit(Evts, #event_stream{handlers = Handlers}) ->
-    lists:foreach(fun(Handler) ->
-        Handler(Evts)
-    end, Handlers).
+-spec reset_time_emission(State :: #state{}) -> NewState :: #state{}.
+reset_time_emission(#state{spec = #event_stream{emission_time = EmTime}} = State)
+    when is_integer(EmTime) ->
+    Ref = make_ref(),
+    erlang:send_after(EmTime, self(), {periodic_emission, Ref}),
+    State#state{emission_reference = Ref};
+reset_time_emission(#state{} = State) ->
+    State.
 
 %%--------------------------------------------------------------------
 %% @private
