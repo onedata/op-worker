3b50fc45a2552415ce552da87945f9a9e7b822ca
Konrad Zemek <konrad.zemek@gmail.com>
2015-02-23 03:55:24 +0100

    VFS-1035 Introduce integration test framework for C++ code.
    Former-commit-id: 802003ab89ac22595c88ba64dd90f928ee207f06

---
+++ test/integration/connection_pool_test/connection_pool_test.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import time
+
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+project_dir = os.path.join(script_dir, '..', '..', '..')
+appmock_dir = os.path.join(project_dir, 'appmock')
+docker_dir = os.path.join(project_dir, 'bamboos', 'docker')
+sys.path = [appmock_dir, docker_dir] + sys.path
+
+from environment import appmock, common, docker
+import connection_pool
+import appmock_client
+
+
+class TestConnectionPool:
+    @classmethod
+    def setup_class(cls):
+        cls.result = appmock.up(image='onedata/builder', bindir=appmock_dir,
+                                dns='none', uid=common.generate_uid(),
+                                config_path=os.path.join(script_dir,
+                                                         'env.json'))
+        # TODO remove this sleep when appmock start is verified with nagios
+        time.sleep(3)
+
+    @classmethod
+    def teardown_class(cls):
+        docker.remove(cls.result['docker_ids'], force=True, volumes=True)
+
+    def test_communication(self):
+        [container] = self.result['docker_ids']
+        ip = docker.inspect(container)['NetworkSettings']['IPAddress'].encode(
+            'ascii')
+
+        CP = connection_pool.ConnectionPoolProxy(3, ip, 5555)
+        CP.send('test')
+        appmock_client.tcp_server_send(ip, 5555, 'message')
+
+        time.sleep(0.5)
+
+        assert 1 == appmock_client.tcp_server_message_count(ip, 5555, "test")
+        assert 'message' == CP.popMessage()
