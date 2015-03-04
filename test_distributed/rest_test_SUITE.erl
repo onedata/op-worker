0f460e51755c79034f08a150657baf2cccfc98c9
Tomasz Lichon <lichon@student.agh.edu.pl>
2015-03-04 17:52:27 +0200

    VFS-1051 accidental sending of CertInfo in oneproxy http_session fixed, rest_test_SUITE added

---
+++ test_distributed/rest_test_SUITE.erl
@@ -0,0 +1,74 @@
+%%%-------------------------------------------------------------------
+%%% @author Tomasz Lichon
+%%% @copyright (C) 2015 ACK CYFRONET AGH
+%%% This software is released under the MIT license
+%%% cited in 'LICENSE.txt'.
+%%% @end
+%%%-------------------------------------------------------------------
+%%% @doc
+%%% REST and CDMI tests
+%%% @end
+%%%-------------------------------------------------------------------
+-module(rest_test_SUITE).
+-author("Tomasz Lichon").
+
+-include("global_definitions.hrl").
+-include_lib("ctool/include/logging.hrl").
+-include_lib("ctool/include/test/test_utils.hrl").
+-include_lib("ctool/include/test/assertions.hrl").
+
+%% API
+-export([all/0, init_per_suite/1, end_per_suite/1, init_per_testcase/2,
+    end_per_testcase/2]).
+
+-export([rest_token_auth/1]).
+
+all() -> [rest_token_auth].
+
+-define(TOKEN, <<"TOKEN_VALUE">>).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+rest_token_auth(Config) ->
+    % given
+    [Worker | _] = ?config(op_worker_nodes, Config),
+    Endpoint = rest_endpoint(Worker),
+    ct:print("~p", [ibrowse:send_req(Endpoint ++ "unknown", [{"X-Auth-Token", ?TOKEN}], get)]).
+
+%%%===================================================================
+%%% SetUp and TearDown functions
+%%%===================================================================
+init_per_suite(Config) ->
+    test_node_starter:prepare_test_environment(Config,
+        ?TEST_FILE(Config, "env_desc.json"), ?MODULE).
+
+end_per_suite(Config) ->
+%%     test_node_starter:clean_environment(Config).
+ok.
+
+init_per_testcase(_, Config) ->
+    ssl:start(),
+    ibrowse:start(),
+    Config.
+
+end_per_testcase(_, _) ->
+    ibrowse:stop(),
+    ssl:stop().
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+rest_endpoint(Node) ->
+    Port =
+        case get(port) of
+            undefined ->
+                {ok, P} = rpc:call(Node, application, get_env, [?APP_NAME, http_worker_rest_port]),
+                PStr = integer_to_list(P),
+                put(port, PStr),
+                PStr;
+            P -> P
+        end,
+    string:join(["https://", utils:get_host(Node), ":", Port, "/rest/latest/"], "").
