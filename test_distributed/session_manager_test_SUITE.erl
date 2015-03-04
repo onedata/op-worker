3e3cbed059d90c384595fdeda9f36ae3f56ecf41
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2015-03-04 15:18:10 +0100

    VFS-997 Add annotations header.

--- test_distributed/session_manager_test_SUITE.erl
+++ test_distributed/session_manager_test_SUITE.erl
@@ -16,6 +16,7 @@
 -include_lib("ctool/include/logging.hrl").
 -include_lib("ctool/include/test/test_utils.hrl").
 -include_lib("ctool/include/test/assertions.hrl").
+-include_lib("annotations/include/annotations.hrl").
 
 %% export for ct
 -export([all/0, init_per_suite/1, end_per_suite/1, init_per_testcase/2,
