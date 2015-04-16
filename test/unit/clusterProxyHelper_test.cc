f237f92443eba06fb98c574f6d84f5f6a7883422
Rafał Słota <roxeon@gmail.com>
2015-04-16 15:58:21 +0200

    fix some clusterproxy tests
    Former-commit-id: 1da6ae5c08fd3161487bbff07c015fdcd8222312

--- test/unit/clusterProxyHelper_test.cc
+++ test/unit/clusterProxyHelper_test.cc
@@ -87,98 +87,91 @@ ACTION_P(SaveMsg, msg)
     msg->CopyFrom(arg1);
 }
 
-//TEST_F(ClusterProxyHelperTest, read)
-//{
-//    ::google::protobuf::LogSilencer silencer; //silence protobuf error logs for this testcase, to test malformatted msg processing
-//    FileData resp;
-//    Answer answer;
-//    std::string sbuf;
+TEST_F(ClusterProxyHelperTest, read)
+{
+    ::google::protobuf::LogSilencer silencer; //silence protobuf error logs for this testcase, to test malformatted msg processing
+    FileData resp;
+    Answer answer;
+    char sbuf[10];
+    auto buf1 = boost::asio::buffer(sbuf, 10);
 
-//    EXPECT_CALL(*mockCommunicator, communicateMock(_, _, _, _)).WillOnce(Return(answer));
-//    EXPECT_EQ(-EIO, proxy->doRead("file_id", sbuf, 10, 2, &ffi));
+    EXPECT_CALL(*mockCommunicator, communicateMock(_, _, _, _)).WillOnce(Return(answer));
+    EXPECT_EQ(-EIO, proxy->doRead("file_id", buf1, 2, &ffi));
 
-//    answer.set_answer_status(VOK);
+    answer.set_answer_status(VOK);
 
-//    resp.set_answer_status(VOK);
-//    char str[] = {0, 1, 45, 34, 0, 0, 0, 34, 56};
-//    std::string strRaw(str, 9);
-//    resp.set_data(strRaw);
-//    answer.set_worker_answer(resp.SerializeAsString());
-//    EXPECT_CALL(*mockCommunicator, communicateMock(_, _, _, _)).WillOnce(Return(answer));
-//    EXPECT_EQ(9, proxy->doRead("file_id", sbuf, 10, 2, &ffi));
-//    for(int i = 0; i < 9; ++i )
-//        EXPECT_EQ(str[i], sbuf[i]);
+    resp.set_answer_status(VOK);
+    char str[] = {0, 1, 45, 34, 0, 0, 0, 34, 56};
+    std::string strRaw(str, 9);
+    resp.set_data(strRaw);
+    answer.set_worker_answer(resp.SerializeAsString());
+    EXPECT_CALL(*mockCommunicator, communicateMock(_, _, _, _)).WillOnce(Return(answer));
+    EXPECT_EQ(9, proxy->doRead("file_id", buf1, 2, &ffi));
+    for(int i = 0; i < 9; ++i )
+        EXPECT_EQ(str[i], sbuf[i]);
 
-//    RemoteFileMangement sentMsg;
+    RemoteFileMangement sentMsg;
 
-//    resp.set_answer_status(VENOENT);
-//    answer.set_worker_answer(resp.SerializeAsString());
-//    EXPECT_CALL(*mockCommunicator, communicateMock(_, _, _, _)).WillOnce(DoAll(SaveMsg(&sentMsg), Return(answer)));
-//    EXPECT_EQ(-ENOENT, proxy->doRead("file_id", sbuf, 10, 2, &ffi));
+    resp.set_answer_status(VENOENT);
+    answer.set_worker_answer(resp.SerializeAsString());
+    EXPECT_CALL(*mockCommunicator, communicateMock(_, _, _, _)).WillOnce(DoAll(SaveMsg(&sentMsg), Return(answer)));
+    EXPECT_EQ(-ENOENT, proxy->doRead("file_id", buf1, 2, &ffi));
 
-//    RemoteFileMangement rfm;
+    RemoteFileMangement rfm;
 
-//    ReadFile subMsg;
-//    subMsg.set_file_id("file_id");
-//    subMsg.set_size(10);
-//    subMsg.set_offset(2);
+    ReadFile subMsg;
+    subMsg.set_file_id("file_id");
+    subMsg.set_size(10);
+    subMsg.set_offset(2);
 
-//    rfm.set_input(subMsg.SerializeAsString());
-//    rfm.set_message_type(tolower(subMsg.GetDescriptor()->name()));
-//    rfm.set_space_id("testSpace");
+    rfm.set_input(subMsg.SerializeAsString());
+    rfm.set_message_type(tolower(subMsg.GetDescriptor()->name()));
+    rfm.set_space_id("testSpace");
 
-//    EXPECT_EQ(rfm.SerializeAsString(), sentMsg.SerializeAsString());
-//}
-
-
-//TEST_F(ClusterProxyHelperTest, write)
-//{
-//    ::google::protobuf::LogSilencer silencer; //silence protobuf error logs for this testcase, to test malformatted msg processing
-//    WriteInfo resp;
-//    Answer answer;
-//    char str[] = {0, 1, 45, 34, 0, 0, 0, 34, 56, 2};
-//    std::string strRaw(str, 10);
-//    std::string sbuf;
-//    sbuf = strRaw;
-
-//    EXPECT_CALL(*mockCommunicator, communicateMock(_, _, _, _)).WillOnce(Return(answer));
-//    EXPECT_EQ(-EIO, proxy->doWrite("file_id", sbuf, 10, 2, &ffi));
-
-//    answer.set_answer_status(VOK);
-
-//    resp.set_answer_status(VOK);
-//    resp.set_bytes_written(9);
-//    answer.set_worker_answer(resp.SerializeAsString());
-//    EXPECT_CALL(*mockCommunicator, communicateMock(_, _, _, _)).WillOnce(Return(answer));
-//    EXPECT_EQ(9, proxy->doWrite("file_id", sbuf, 10, 2, &ffi));
-
-//    RemoteFileMangement sentMsg;
-
-//    resp.set_answer_status(VENOENT);
-//    answer.set_worker_answer(resp.SerializeAsString());
-//    EXPECT_CALL(*mockCommunicator, communicateMock(_, _, _, _)).WillOnce(DoAll(SaveMsg(&sentMsg), Return(answer)));
-//    EXPECT_EQ(-ENOENT, proxy->doWrite("file_id", sbuf, 10, 2, &ffi));
-
-//    RemoteFileMangement rfm;
-
-//    WriteFile subMsg;
-//    subMsg.set_file_id("file_id");
-//    subMsg.set_data(strRaw);
-//    subMsg.set_offset(2);
-
-//    rfm.set_input(subMsg.SerializeAsString());
-//    rfm.set_message_type(tolower(subMsg.GetDescriptor()->name()));
-//    rfm.set_space_id("testSpace");
-
-//    EXPECT_EQ(rfm.SerializeAsString(), sentMsg.SerializeAsString());
-//}
+    EXPECT_EQ(rfm.SerializeAsString(), sentMsg.SerializeAsString());
+}
 
 
-//TEST_F(ClusterProxyHelperTest, statfs)
-//{
-//    struct statvfs stbuf;
-//    EXPECT_EQ(ENOTSUP, proxy->sh_statfs("file_id", &stbuf));
-//}
+TEST_F(ClusterProxyHelperTest, write)
+{
+    ::google::protobuf::LogSilencer silencer; //silence protobuf error logs for this testcase, to test malformatted msg processing
+    WriteInfo resp;
+    Answer answer;
+    char str[] = {0, 1, 45, 34, 0, 0, 0, 34, 56, 2};
+    std::string strRaw(str, 10);
+    auto buf1 = boost::asio::buffer(str, 10);
+
+    EXPECT_CALL(*mockCommunicator, communicateMock(_, _, _, _)).WillOnce(Return(answer));
+    EXPECT_EQ(-EIO, proxy->doWrite("file_id", buf1, 2, &ffi));
+
+    answer.set_answer_status(VOK);
+
+    resp.set_answer_status(VOK);
+    resp.set_bytes_written(9);
+    answer.set_worker_answer(resp.SerializeAsString());
+    EXPECT_CALL(*mockCommunicator, communicateMock(_, _, _, _)).WillOnce(Return(answer));
+    EXPECT_EQ(9, proxy->doWrite("file_id", buf1, 2, &ffi));
+
+    RemoteFileMangement sentMsg;
+
+    resp.set_answer_status(VENOENT);
+    answer.set_worker_answer(resp.SerializeAsString());
+    EXPECT_CALL(*mockCommunicator, communicateMock(_, _, _, _)).WillOnce(DoAll(SaveMsg(&sentMsg), Return(answer)));
+    EXPECT_EQ(-ENOENT, proxy->doWrite("file_id", buf1, 2, &ffi));
+
+    RemoteFileMangement rfm;
+
+    WriteFile subMsg;
+    subMsg.set_file_id("file_id");
+    subMsg.set_data(strRaw);
+    subMsg.set_offset(2);
+
+    rfm.set_input(subMsg.SerializeAsString());
+    rfm.set_message_type(tolower(subMsg.GetDescriptor()->name()));
+    rfm.set_space_id("testSpace");
+
+    EXPECT_EQ(rfm.SerializeAsString(), sentMsg.SerializeAsString());
+}
 
 
 TEST_F(ClusterProxyHelperTest, release)
