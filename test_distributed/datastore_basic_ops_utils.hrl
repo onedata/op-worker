73bf3e7d8dc0097feef2597d8e567360061504c1
Michał Wrzeszcz <wrzeszcz@agh.edu.pl>
2015-05-03 11:42:47 +0200

    VFS-1097, testing

--- test_distributed/datastore_basic_ops_utils.hrl
+++ test_distributed/datastore_basic_ops_utils.hrl
@@ -18,8 +18,8 @@
     -performance([
         {repeats, ?repeats},
         {parameters, [
-            [{name, threads_num}, {value, 50}, {description, "Number of threads that operates at single node."}],
-            [{name, docs_per_thead}, {value, 50}, {description, "Number of documents used by single threads."}],
+            [{name, threads_num}, {value, 20}, {description, "Number of threads that operates at single node."}],
+            [{name, docs_per_thead}, {value, 5}, {description, "Number of documents used by single threads."}],
             [{name, ops_per_doc}, {value, 5}, {description, "Number of oprerations on each document."}],
             [{name, conflicted_threads}, {value, 10}, {description, "Number of threads that work with the same documents set."}]
         ]},
