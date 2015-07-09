1766e322a05239b9a379fd9b65ae8d1c8a846225
Łukasz Opioła <lukasz.opiola@gmail.com>
2015-07-10 00:12:45 +0200

    VFS-1172, change c_lib dir to to priv

--- src/utils/csr_creator.erl
+++ src/utils/csr_creator.erl
@@ -25,10 +25,22 @@
 %% ====================================================================
 %% @doc Initializes NIF library.
 %% @end
--spec init() -> ok | no_return().
-%% ====================================================================
+-spec init() -> ok | {error, Reason :: atom()}.
 init() ->
-    ok = erlang:load_nif("c_lib/csr_creator_drv", 0).
+    LibName = "csr_creator_drv",
+    LibPath =
+        case code:priv_dir(op_worker) of
+            {error, bad_name} ->
+                case filelib:is_dir(filename:join(["..", priv])) of
+                    true ->
+                        filename:join(["..", priv, LibName]);
+                    _ ->
+                        filename:join([priv, LibName])
+                end;
+            Dir ->
+                filename:join(Dir, LibName)
+        end,
+    erlang:load_nif(LibPath, 0).
 
 
 %% create_csr/3
