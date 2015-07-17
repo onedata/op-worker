cb110452075e872bf67e6c8c2f8d0685dd1f2d69
Rafal Slota <roxeon@gmail.com>
2015-07-17 20:23:36 +0200

    VFS-1193 add couchbase driver support

--- docker/ct_run.py
+++ docker/ct_run.py
@@ -41,6 +41,7 @@ parser.add_argument(
 parser.add_argument(
     '--suite', '-s',
     action='append',
+    default=['datastore_basic_ops_test_1x1_SUITE'],
     help='name of the test suite',
     dest='suites')
 
