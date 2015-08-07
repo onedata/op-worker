0e17944fb747e277812141c790c2576cba7d1e6a
Rafal Slota <roxeon@gmail.com>
2015-08-07 09:52:50 +0200

    Merge commit '1106d68a9a5e6a01dbc28902ed850fb6baa9393d' into feature/VFS-1193-couchbase-for-production

--- docker/dns_up.py
+++ docker/dns_up.py
@@ -13,8 +13,9 @@ Run the script with -h flag to learn about script's running options.
 from __future__ import print_function
 import argparse
 import json
+import sys
 
-from environment import common, dns
+from environment import common, dns, docker
 
 
 parser = argparse.ArgumentParser(
@@ -28,6 +29,28 @@ parser.add_argument(
     help='uid that will be concatenated to docker name',
     dest='uid')
 
+parser.add_argument(
+    '--root-servers',
+    action='store',
+    default=[],
+    help='list of root servers for non-authoritative domain delegation, in format domain1/ip,domain2/ip...',
+    dest='root_servers')
+
+parser.add_argument(
+    '-r', '--restart',
+    action='store',
+    default='none',
+    help='use this flag with a docker name, then a dns server with this name will be restarted (uid arg will be ignored)',
+    dest='restart')
+
 args = parser.parse_args()
-output = dns.up(args.uid)
+if args.restart is not 'none':
+    try:
+        docker.inspect(args.restart)
+    except:
+        sys.exit(1)
+
+if args.root_servers:
+    args.root_servers = args.root_servers.split(',')
+output = dns.up(args.uid, args.root_servers, args.restart)
 print(json.dumps(output))
