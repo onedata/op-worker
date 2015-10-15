2bfbbfdcb3bc78fa15a239110839842a18bc0ba4
Konrad Zemek <konrad.zemek@gmail.com>
2015-10-15 14:29:24 +0200

    Merge commit '53600cf9070a265d32ddb20a03b348efe37cb1c3' into bugfix/VFS-1255-onedata-packages-is-green-on-bamboo

--- bamboos/docker/provider_ccm_up.py
+++ bamboos/docker/provider_ccm_up.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
-# coding=utf-8
 
-"""Authors: Tomasz Lichoń, Konrad Zemek
+"""Author: Tomasz Lichon
 Copyright (C) 2015 ACK CYFRONET AGH
 This software is released under the MIT license cited in 'LICENSE.txt'
 
-A script to bring up a set of Global Registry nodes along with databases.
-They can create separate clusters.
+A script to bring up a set of oneprovider ccm nodes. They can create separate
+clusters.
 Run the script with -h flag to learn about script's running options.
 """
 
 from __future__ import print_function
 import json
 
-from environment import common, globalregistry
+from environment import common, provider_ccm
 
 
-parser = common.standard_arg_parser('Bring up globalregistry nodes.')
+parser = common.standard_arg_parser('Bring up op_ccm nodes.')
 parser.add_argument(
     '-l', '--logdir',
     action='store',
@@ -25,6 +24,7 @@ parser.add_argument(
     dest='logdir')
 
 args = parser.parse_args()
-output = globalregistry.up(args.image, args.bin, args.dns,
-                           args.uid, args.config_path, args.logdir)
+output = provider_ccm.up(args.image, args.bin, args.dns, args.uid,
+                     args.config_path, args.logdir)
+
 print(json.dumps(output))
