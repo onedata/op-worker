c0b8052c2c205648fdd223b827ed808414a73ad3
Jakub Kudzia <jakub.kudzia@outlook.com>
2015-11-12 10:41:31 +0100

    VFS-1341- tests for cdmi GET request, only application/binary content-type

--- test_distributed/cdmi_test_SUITE.erl
+++ test_distributed/cdmi_test_SUITE.erl
@@ -17,6 +17,7 @@
 -include_lib("ctool/include/logging.hrl").
 -include_lib("ctool/include/test/test_utils.hrl").
 -include_lib("ctool/include/test/assertions.hrl").
+-include_lib("ctool/include/posix/file_attr.hrl").
 -include_lib("ctool/include/posix/errors.hrl").
 -include_lib("annotations/include/annotations.hrl").
 
@@ -24,11 +25,11 @@
 -export([all/0, init_per_suite/1, end_per_suite/1, init_per_testcase/2,
     end_per_testcase/2]).
 
--export([choose_adequate_handler/1, use_supported_cdmi_version/1,
+-export([get_file_test/1, choose_adequate_handler/1, use_supported_cdmi_version/1,
     use_unsupported_cdmi_version/1, create_dir_test/1]).
 
 -performance({test_cases, []}).
-all() -> [choose_adequate_handler, use_supported_cdmi_version,
+all() -> [get_file_test, choose_adequate_handler, use_supported_cdmi_version,
     use_unsupported_cdmi_version, create_dir_test].
 
 -define(MACAROON, "macaroon").
@@ -37,9 +38,52 @@ all() -> [choose_adequate_handler, use_supported_cdmi_version,
 -define(USER_1_TOKEN_HEADER, {"X-Auth-Token", "1"}).
 -define(CDMI_VERSION_HEADER, {"X-CDMI-Specification-Version", "1.1.1"}).
 
+-define(FILE_PERMISSIONS, 8#664).
+
+-define(OFFSET, 0).
+
+
 %%%===================================================================
 %%% Test functions
 %%%===================================================================
+
+% Tests cdmi object GET request. Request can be done without cdmi header (in that case
+% file conent is returned as response body), or with cdmi header (the response
+% contains json string of type: application/cdmi-object, and we can specify what
+% parameters we need by listing then as ';' separated list after '?' in URL ),
+%  )
+get_file_test(Config) ->
+    FileName = "/toRead.txt",
+    FileContent = <<"Some content...">>,
+    Size = string:len(binary_to_list(FileContent)),
+    [Worker | _] = ?config(op_worker_nodes, Config),
+
+    create_file(Config, FileName),
+    ?assert(object_exists(Config, FileName)),
+    write_to_file(Config, FileName,FileContent),
+    ?assertEqual(FileContent,get_file_content(Config, FileName, Size)),
+
+
+    %%------- noncdmi read --------
+    {Code4, Headers4, Response4} = do_request(Worker, FileName, get, [], []),
+    ?assertEqual("200",Code4),
+
+    ?assertEqual("application/octet-stream", proplists:get_value("content-type",Headers4)),
+    ?assertEqual(binary_to_list(FileContent), Response4),
+    %%------------------------------
+
+    %% selective value read non-cdmi
+    RequestHeaders7 = [{"Range","1-3,5-5,-3"}],
+    {Code7, _Headers7, Response7} = do_request(Worker, FileName, get, RequestHeaders7, []),
+    ?assertEqual("206",Code7),
+    ?assertEqual("omec...", Response7), % 1-3,5-5,12-14  from FileContent = <<"Some content...">>
+    %%------------------------------
+
+    %% selective value read non-cdmi error
+    RequestHeaders8 = [{"Range","1-3,6-4,-3"}],
+    {Code8, _Headers8, _Response8} = do_request(Worker, FileName, get, RequestHeaders8, []),
+    ?assertEqual("400",Code8).
+    %%------------------------------
 
 choose_adequate_handler(Config) ->
     % given
@@ -185,4 +229,43 @@ object_exists(Config, Path) ->
             true;
         {error, ?ENOENT} ->
             false
+    end.
+
+create_file(Config, Path) ->
+    [Worker | _] = ?config(op_worker_nodes, Config),
+    SessionId = ?config({session_id, 1}, Config),
+
+    case lfm_proxy:create(Worker, SessionId, {path, utils:ensure_unicode_binary(Path)}, ?FILE_PERMISSIONS) of
+        {ok, UUID} -> UUID;
+        {error, Code} -> {error, Code}
+    end.
+
+%% FileKey type is file_id_or_path() :: {uuid, file_uuid()} | { path, file_path()}
+open_file(Config, FileKey, OpenMode) ->
+    [Worker | _] = ?config(op_worker_nodes, Config),
+    SessionId = ?config({session_id, 1}, Config),
+
+    case lfm_proxy:open(Worker, SessionId, FileKey, OpenMode) of
+        {error, Error} -> {error, Error};
+        FileHandle -> FileHandle
+    end.
+
+write_to_file(Config, Path, Data) ->
+    %%TODO maybe Offset sholud be function argument ?
+    [Worker | _] = ?config(op_worker_nodes, Config),
+
+    FileHandle = open_file(Config, {path, Path}, write),
+    case lfm_proxy:write(Worker, FileHandle, ?OFFSET, Data) of
+        {error, Error} -> {error, Error};
+        Result -> Result
+    end.
+
+get_file_content(Config, Path, Size) ->
+    %%TODO maybe Offset sholud be function argument ?
+    [Worker | _] = ?config(op_worker_nodes, Config),
+
+    FileHandle = open_file(Config, {path, Path}, write),
+    case lfm_proxy:read(Worker, FileHandle, ?OFFSET, Size) of
+        {error, Error} -> {error, Error};
+        Result -> Result
     end.
