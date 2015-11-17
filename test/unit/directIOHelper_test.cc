8d934f7eaafdd0f71837322fa094758b0a0d74dd
Konrad Zemek <konrad.zemek@gmail.com>
2015-11-17 18:04:34 +0100

    VFS-1371 Add ProxyIO helper.

--- test/unit/directIOHelper_test.cc
+++ test/unit/directIOHelper_test.cc
@@ -1,7 +1,7 @@
 /**
- * @file clusterProxyHelper_test.cc
+ * @file directIOHelper_test.cc
  * @author Rafal Slota
- * @copyright (C) 2013 ACK CYFRONET AGH
+ * @copyright (C) 2015 ACK CYFRONET AGH
  * @copyright This software is released under the MIT license cited in
  * 'LICENSE.txt'
  */
