ba452d8caf643e81d7e8719ef73c1319a26f3b4e
Rafal Slota <roxeon@gmail.com>
2015-11-23 17:45:11 +0100

    add some docs

--- src/modules/fslogic/storage_file_manager.erl
+++ src/modules/fslogic/storage_file_manager.erl
@@ -39,6 +39,13 @@
 %%%===================================================================
 
 
+%%--------------------------------------------------------------------
+%% @doc
+%% Opens the file. To used opened descriptor, pass returned handle to other functions.
+%% File may and should be closed with release/1, but file will be closed automatically
+%% when handle goes out of scope (term will be released by Erlang's GC).
+%% @end
+%%--------------------------------------------------------------------
 -spec new_handle(SessionId :: session:id(), SpaceUUID :: file_meta:uuid(), Storage :: datastore:document(), FileId :: helpers:file()) ->
     handle().
 new_handle(SessionId, SpaceUUID, Storage, FileId) ->
