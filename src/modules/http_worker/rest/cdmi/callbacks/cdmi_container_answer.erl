d944b79c98f19885ab6a946f56db079fd2b157f6
Tomasz Lichon <xorver@gmail.com>
2015-11-30 16:02:54 +0100

    VFS-1404 Remove call to undefined function.

--- src/modules/http_worker/rest/cdmi/callbacks/cdmi_container_answer.erl
+++ src/modules/http_worker/rest/cdmi/callbacks/cdmi_container_answer.erl
@@ -40,7 +40,7 @@ prepare([<<"objectName">> | Tail], #{path := Path} = State) ->
 prepare([<<"parentURI">> | Tail], #{path := <<"/">>} = State) ->
     [{<<"parentURI">>, <<>>} | prepare(Tail, State)];
 prepare([<<"parentURI">> | Tail], #{path := Path} = State) ->
-    ParentURI = utils:ensure_path_ends_with_slash(
+    ParentURI = gui_str:ensure_ends_with_slash(
         filename:dirname(binary:part(Path, {0, byte_size(Path) - 1}))),
     [{<<"parentURI">>, ParentURI} | prepare(Tail, State)];
 %% prepare([<<"parentID">> | Tail], #{path := <<"/">>} = State) -> todo introduce objectid
