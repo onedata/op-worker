0dfd03c443bb31f7fdcce65f735710cc67bd6234
Michał Żmuda <zmu.michal@gmail.com>
2015-12-02 22:47:28 +0100

    VFS-1382 global definitions trimmed

--- include/global_definitions.hrl
+++ include/global_definitions.hrl
@@ -26,34 +26,13 @@
 %% Name of the base worker application.
 -define(WORKER_APP_NAME, cluster_worker).
 
-%% Name supervisor of the base worker application.
--define(WORKER_SUP_NAME, cluster_worker_sup).
-
 %% Local name (name and node is used to identify it) of supervisor that
 %% coordinates application at each node (one supervisor per node).
 -define(APPLICATION_SUPERVISOR_NAME, op_worker_sup).
 
-%% Local name (name and node is used to identify it) of supervisor that
-%% coordinates workers at each node
--define(MAIN_WORKER_SUPERVISOR_NAME, main_worker_sup).
-
 %% Local name (name and node is used to identify it) of gen_server that
 %% works as a dispatcher.
 -define(DISPATCHER_NAME, request_dispatcher).
-
-%% Local name (name and node is used to identify it) of supervisor that
-%% coordinates the processes started by concrete worker_host (given by arg)
--define(WORKER_HOST_SUPERVISOR_NAME(Module), list_to_atom(atom_to_list(Module) ++ "_sup")).
-
-%%%===================================================================
-%%% Global types
-%%%===================================================================
-
-% Macro that should be used to log an error during healthcheck
--define(HEALTHCHECK_ERROR_LOG_MSG(_Msg),
-    HEALTHCHECK_ERROR_LOG(_Msg, [])).
--define(HEALTHCHECK_ERROR_LOG_MSG(_Msg, _Args),
-    lists:flatten(io_lib:format("Healthcheck error in ~p on node ~p: " ++ _Msg, [?MODULE, node()] ++ _Args))).
 
 %%%===================================================================
 %%% Global identities
