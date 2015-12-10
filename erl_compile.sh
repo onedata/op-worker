2f3865e5bdb5b43b1d921922b1929d1c98cda7c8
Tomasz Lichon <xorver@gmail.com>
2015-12-10 14:28:47 +0100

    VFS-1400 Compile only files from src in erl_compile.sh script.

--- erl_compile.sh
+++ erl_compile.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-set -e -v
+set -e
 
 # for each given module, find its location, compile and copy to releases
 for module in $@; do
-    find . -type f -name ${module}.erl -exec erlc `ls deps | awk '{ print "-I deps/" $1}'` -I include -I deps {} \;
+    find src -type f -name ${module}.erl -exec erlc `ls deps | awk '{ print "-I deps/" $1}'` -I include -I deps {} \;
     for rel_dir in `ls rel/op_worker/lib/ | grep op_worker`; do
         cp *.beam rel/op_worker/lib/${rel_dir}/ebin/
     done
