d3025e3524fa6ed0648cd8cd9acd05b08b619b7f
Michał Żmuda <zmu.michal@gmail.com>
2015-12-21 12:32:02 +0100

    VFS-1457 fix ccm app name

--- gen_dev/src/release_configurator.erl
+++ gen_dev/src/release_configurator.erl
@@ -14,7 +14,7 @@
 -author("Tomasz Lichon").
 
 % oneprovider specific config
--define(ONEPROVIDER_CCM_APP_NAME, op_ccm).
+-define(ONEPROVIDER_CCM_APP_NAME, cluster_manager).
 -define(DIST_APP_FAILOVER_TIMEOUT, timer:seconds(5)).
 -define(SYNC_NODES_TIMEOUT, timer:minutes(1)).
 
