e2ef4934de75afa3e2734ce2ad237cba25b08eaa
Michał Żmuda <zmu.michal@gmail.com>
2015-12-23 12:38:28 +0100

    Squashed 'bamboos/' changes from 38d8036..ff23669
    ff23669 VFS-1457 PR issues: refactoring starting cluster workers
    d3025e3 VFS-1457 fix ccm app name
    099c5e4 VFS-1457 initial cluster-manager renaming
    f48ad9f Merge pull request #69 in VFS/bamboos from feature/VFS-1382-cluster-worker-scripts to develop
    7f1b0f3 Merge commit 'd8557594485e0fce689e96490441d72cc2059313' into feature/VFS-1382-cluster-framework
    2dc4cff VFS-1382 addressing review issues
    
    git-subtree-dir: bamboos
    git-subtree-split: ff2366918cd35996a4193113b3b46a323c73dfcf

--- docker/cluster_up.py
+++ docker/cluster_up.py
@@ -12,10 +12,10 @@ Run the script with -h flag to learn about script's running options.
 from __future__ import print_function
 import json
 import os
-from environment import common, cluster_worker, provider_ccm, dns
+from environment import common, cluster_worker, cluster_manager, dns
 
 parser = common.standard_arg_parser(
-    'Bring up bare cluster nodes (workers and ccms).')
+    'Bring up bare cluster nodes (workers and cms).')
 parser.add_argument(
     '-l', '--logdir',
     action='store',
@@ -29,17 +29,17 @@ parser.add_argument(
     help='the path to cluster-worker repository (precompiled)',
     dest='bin_op_worker')
 parser.add_argument(
-    '-bccm', '--bin-ccm',
+    '-bcm', '--bin-cm',
     action='store',
-    default=os.getcwd() + '/op_ccm',
-    help='the path to op_ccm repository (precompiled)',
-    dest='bin_op_ccm')
+    default=os.getcwd() + '/cluster_manager',
+    help='the path to cluster_manager repository (precompiled)',
+    dest='bin_cluster_manager')
 
 # Prepare config
 args = parser.parse_args()
 config = common.parse_json_file(args.config_path)
 output = {
-    'op_ccm_nodes': [],
+    'cluster_manager_nodes': [],
     'cluster_worker_nodes': [],
 }
 uid = common.generate_uid()
@@ -48,9 +48,9 @@ uid = common.generate_uid()
 [dns_server], dns_output = dns.maybe_start('auto', uid)
 common.merge(output, dns_output)
 
-# Start ccms
-ccm_output = provider_ccm.up(args.image, args.bin_op_ccm, dns_server, uid, args.config_path, args.logdir)
-common.merge(output, ccm_output)
+# Start cms
+cm_output = cluster_manager.up(args.image, args.bin_cluster_manager, dns_server, uid, args.config_path, args.logdir)
+common.merge(output, cm_output)
 
 # Start workers
 worker_output = cluster_worker.up(args.image, args.bin_op_worker, dns_server, uid, args.config_path, args.logdir)
