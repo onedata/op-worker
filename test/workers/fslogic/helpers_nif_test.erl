2b0bf0417535d008dd4c647a46b139156999be72
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2016-01-28 15:14:11 +0100

    Add environment setup in helpers NIF unit tests.

--- test/workers/fslogic/helpers_nif_test.erl
+++ test/workers/fslogic/helpers_nif_test.erl
@@ -22,15 +22,13 @@
 %%%===================================================================
 
 new_obj_test() ->
-    application:set_env(?APP_NAME, ceph_helper_threads_number, 1),
-    application:set_env(?APP_NAME, direct_io_helper_threads_number, 1),
+    prepare_environment(),
     ok = helpers_nif:init(),
     ?assertMatch({ok, _}, helpers_nif:new_helper_obj(?DIRECTIO_HELPER_NAME, #{<<"root_path">> => <<"/tmp">>})),
     ok.
 
 new_ctx_test() ->
-    application:set_env(?APP_NAME, ceph_helper_threads_number, 1),
-    application:set_env(?APP_NAME, direct_io_helper_threads_number, 1),
+    prepare_environment(),
     ok = helpers_nif:init(),
     {ok, Helper} = helpers_nif:new_helper_obj(?DIRECTIO_HELPER_NAME, #{<<"root_path">> => <<"/tmp">>}),
     ?assertMatch({ok, _}, helpers_nif:new_helper_ctx(Helper)),
@@ -39,8 +37,7 @@ new_ctx_test() ->
 ctx_test_() ->
     {setup,
         fun() ->
-            application:set_env(?APP_NAME, ceph_helper_threads_number, 1),
-            application:set_env(?APP_NAME, direct_io_helper_threads_number, 1),
+            prepare_environment(),
             ok = helpers_nif:init(),
             {ok, Helper} = helpers_nif:new_helper_obj(?DIRECTIO_HELPER_NAME, #{<<"root_path">> => <<"/tmp">>}),
             {ok, CTX} = helpers_nif:new_helper_ctx(Helper),
@@ -73,19 +70,33 @@ ctx_test_() ->
     }.
 
 username_to_uid_test() ->
-    application:set_env(?APP_NAME, ceph_helper_threads_number, 1),
-    application:set_env(?APP_NAME, direct_io_helper_threads_number, 1),
+    prepare_environment(),
     ok = helpers_nif:init(),
     ?assertMatch({ok, 0}, helpers_nif:username_to_uid(<<"root">>)),
     ?assertMatch({error, einval}, helpers_nif:username_to_uid(<<"sadmlknfqlwknd">>)),
     ok.
 
 groupname_to_gid_test() ->
-    application:set_env(?APP_NAME, ceph_helper_threads_number, 1),
-    application:set_env(?APP_NAME, direct_io_helper_threads_number, 1),
+    prepare_environment(),
     ok = helpers_nif:init(),
     ?assertMatch({ok, 0}, helpers_nif:groupname_to_gid(<<"root">>)),
     ?assertMatch({error, einval}, helpers_nif:groupname_to_gid(<<"sadmlknfqlwknd">>)),
     ok.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Sets required environment variables.
+%% @end
+%%--------------------------------------------------------------------
+-spec prepare_environment() -> ok.
+prepare_environment() ->
+    application:set_env(?APP_NAME, ceph_helper_threads_number, 1),
+    application:set_env(?APP_NAME, direct_io_helper_threads_number, 1),
+    application:set_env(?APP_NAME, s3_helper_threads_number, 1).
 
 -endif.
