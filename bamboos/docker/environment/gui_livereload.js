0ccfe76198dfa7aed53e3995b3cd0d01572654a6
Łukasz Opioła <lukasz.opiola@gmail.com>
2016-02-04 14:27:59 +0100

    VFS-1508 improve some specs and comments

--- bamboos/docker/environment/gui_livereload.js
+++ bamboos/docker/environment/gui_livereload.js
@@ -1,14 +1,20 @@
+// This file starts livereload service (secure websockets server). Browser
+// clients connect to the server and if a file changes, a page reload request
+// is pushed to the browser so it refreshes automatically.
+
 try {
     require('livereload');
 }
 catch (e) {
-    console.error('Dependency livereload not found. Try doing `npm link livereload`.');
+    console.error(
+        'Dependency livereload not found. Try doing `npm link livereload`.');
     process.exit(1);
 }
 const livereload = require('livereload');
 const fs = require('fs');
 const path = require('path');
 
+// Use provider certs as certs for livereload websocket server.
 const options = {
     key: fs.readFileSync('/root/bin/node/etc/certs/onedataServerWeb.pem'),
     cert: fs.readFileSync('/root/bin/node/etc/certs/onedataServerWeb.pem')
@@ -20,6 +26,7 @@ if (watchedDir === undefined) {
     watchedDir = __dirname
 }
 watchedDir = path.resolve(watchedDir);
+// Check if resolved path is a directory
 var isDir = false;
 try {
     var stats = fs.lstatSync(watchedDir);
@@ -36,6 +43,7 @@ if (isDir) {
     server.watch(watchedDir);
     console.log("Successfully started livereload in directory " + watchedDir);
 } else {
-    console.error('Failed to start livereload. Directory `' + watchedDir + '` does not exist.');
+    console.error('Failed to start livereload. Directory `' +
+        watchedDir + '` does not exist.');
     process.exit(1);
 }
