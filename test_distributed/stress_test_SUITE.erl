2870483cf26c0f5198d206a66eb450cf8875f7ac
Jakub Kudzia <jakub.kudzia@outlook.com>
2016-02-04 23:21:25 +0100

    VFS-1520-code improvements

--- test_distributed/stress_test_SUITE.erl
+++ test_distributed/stress_test_SUITE.erl
@@ -44,7 +44,8 @@ all() ->
 %%% Test functions
 %%%===================================================================
 
-stress_test(Config) ->   ?STRESS(Config,[
+stress_test(Config) ->
+    ?STRESS(Config,[
             {description, "Main stress test function. Links together all cases to be done multiple times as one continous test."},
             {success_rate, 99.9},
             {config, [{name, stress}, {description, "Basic config for stress test"}]}
