62c5a18a0f9a8ce13c1813eb533c212d5b446922
Łukasz Opioła <lukasz.opiola@gmail.com>
2016-02-11 10:56:45 +0100

    VFS-1508 fix nvm sourcing during building

--- test_gui.sh
+++ test_gui.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Path to nvm binary
-NVM_BINARY=/usr/lib/nvm/nvm.sh
-
-# Source NVM
-. ${NVM_BINARY}
 # Make sure NVM loads node binaries
 nvm use default node
 # Run the tests
