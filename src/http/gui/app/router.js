8bfaa6fcf7306edafa86b9f7e9fab3cc35fe13b1
Jakub Liput <jakub.liput@gmail.com>
2016-02-11 14:13:34 +0100

    VFS-1549: Snapshot of work: routes preparation

--- src/http/gui/app/router.js
+++ src/http/gui/app/router.js
@@ -27,7 +27,19 @@ Router.map(function() {
       this.route('groups');
     });
   });
-  this.route('data');
+
+  // data/ - list of Spaces - user can select the Space
+  this.route('data', function() {
+    // data/:space_id - show contents of a root dir of selected Space
+    this.route('space', {path: ':space_id'}, function() {
+      // data/:space_id/:file_id - select a file in a browser
+      this.route('file', {path: ':file_id'});
+    });
+    this.route('space', function() {
+      this.route('file');
+    });
+  });
+
   this.route('links');
   this.route('collection');
   this.route('trash');
