1ded073a1fa68eb2723987b6dd80278c09fb5034
Jakub Liput <jakub.liput@gmail.com>
2016-03-11 18:30:06 +0100

    VFS-1549: Enter into directory from file browser (list)

--- src/http/gui/app/components/data-files-list.js
+++ src/http/gui/app/components/data-files-list.js
@@ -11,7 +11,20 @@ export default Ember.Component.extend({
     return this.get('dir.children').map((file) => {
       let fileSystemTree = this.get('fileSystemTree');
       // TODO: get space id will be removed - only for space resolve demonstration
-      return `id: "${file.get('id')}" "${file.get('name')}" (${file.get('type')}) <- space "${fileSystemTree.getSpaceIdForFile(file)}"`;
+      return {
+        file: file,
+        label: `id: "${file.get('id')}" "${file.get('name')}" (${file.get('type')}) <- space "${fileSystemTree.getSpaceIdForFile(file)}"`
+      };
     });
-  }.property('dir.children.@each.name', 'dir.children.@each.type')
+  }.property('dir.children.@each.name', 'dir.children.@each.type'),
+
+  actions: {
+    openFile(dirFile) {
+      if (dirFile.get('isDir')) {
+        this.sendAction('openDirInBrowser', dirFile.get('id'));
+      } else {
+        console.error(`Clicked file is no a dir, so nothing will happen: ${dirFile.get('id')}`);
+      }
+    }
+  }
 });
