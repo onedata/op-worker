64ca5247e331f56f6a2ad34e7ae2b8d2d8a07678
Łukasz Opioła <lukasz.opiola@gmail.com>
2016-03-14 16:58:41 +0100

    VFS-1734 mock create file failure to test error handliung

--- src/http/gui_backend/file_data_backend.erl
+++ src/http/gui_backend/file_data_backend.erl
@@ -88,31 +88,36 @@ create_record(<<"file">>, Data) ->
     ?log_debug({create_record, <<"file">>, Data}),
     SessionId = g_session:get_session_id(),
     Name = proplists:get_value(<<"name">>, Data),
-    Type = proplists:get_value(<<"type">>, Data),
-    ParentUUID = proplists:get_value(<<"parent">>, Data, null),
-    {ok, ParentPath} = logical_file_manager:get_file_path(
-        SessionId, ParentUUID),
-    Path = filename:join([ParentPath, Name]),
-    ?log_debug(Path),
-    FileId = case Type of
-        <<"file">> ->
-            {ok, FId} = logical_file_manager:create(
-                SessionId, Path, 8#777),
-            FId;
-        <<"dir">> ->
-            {ok, DirId} = logical_file_manager:mkdir(
-                SessionId, Path, 8#777),
-            DirId
-    end,
-    Res = [
-        {<<"id">>, FileId},
-        {<<"name">>, Name},
-        {<<"type">>, Type},
-        {<<"parent">>, ParentUUID},
-        {<<"children">>, []}
-    ],
-    ?log_debug({create_record, Res}),
-    {ok, Res}.
+    case binary:match(Name, <<"nie">>) of
+        {_, _} ->
+            {error, bleblelbebll};
+        nomatch ->
+            Type = proplists:get_value(<<"type">>, Data),
+            ParentUUID = proplists:get_value(<<"parent">>, Data, null),
+            {ok, ParentPath} = logical_file_manager:get_file_path(
+                SessionId, ParentUUID),
+            Path = filename:join([ParentPath, Name]),
+            ?log_debug(Path),
+            FileId = case Type of
+                <<"file">> ->
+                    {ok, FId} = logical_file_manager:create(
+                        SessionId, Path, 8#777),
+                    FId;
+                <<"dir">> ->
+                    {ok, DirId} = logical_file_manager:mkdir(
+                        SessionId, Path, 8#777),
+                    DirId
+            end,
+            Res = [
+                {<<"id">>, FileId},
+                {<<"name">>, Name},
+                {<<"type">>, Type},
+                {<<"parent">>, ParentUUID},
+                {<<"children">>, []}
+            ],
+            ?log_debug({create_record, Res}),
+            {ok, Res}
+    end.
 
 %% Called when ember asks to update a record
 update_record(<<"file">>, _Id, _Data) ->
@@ -121,7 +126,6 @@ update_record(<<"file">>, _Id, _Data) ->
 %% Called when ember asks to delete a record
 delete_record(<<"file">>, _Id) ->
     {error, not_iplemented}.
-
 
 
 % ------------------------------------------------------------
