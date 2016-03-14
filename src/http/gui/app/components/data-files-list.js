bd335581ecf2b104de2bc382b2cc83959cdefd92
Jakub Liput <jakub.liput@gmail.com>
2016-03-14 16:54:58 +0100

    VFS-1549: Stub of error handling service

--- src/http/gui/app/components/data-files-list.js
+++ src/http/gui/app/components/data-files-list.js
@@ -4,6 +4,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   store: Ember.inject.service('store'),
   fileSystemTree: Ember.inject.service('file-system-tree'),
+  errorNotifier: Ember.inject.service('error-notifier'),
 
   // TODO: doc
   dir: null,
@@ -37,7 +38,9 @@ export default Ember.Component.extend({
         parent: this.get('dir'),
         type: type
       });
-      record.save();
+      record.save().then(() => {}, (failMessage) => {
+        this.get('errorNotifier').handle(failMessage);
+      });
     }
   },
 
