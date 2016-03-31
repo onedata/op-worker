6e8675a894a7bae7ed0495dabf6ea1c786d562fc
Rafał Słota <roxeon@gmail.com>
2016-03-31 20:32:01 +0200

    VFS-1521 Fix dialyzer

--- src/modules/events/event_manager.erl
+++ src/modules/events/event_manager.erl
@@ -276,7 +276,7 @@ handle_or_reroute(RequestMessage, {file, Entry}, SessId, HandleLocallyFun) ->
             {ok, #document{key = SpaceUUID}} = fslogic_spaces:get_space(Entry, UserId),
             SpaceId = fslogic_uuid:space_dir_uuid_to_spaceid(SpaceUUID),
             RestClient = fslogic_utils:session_to_rest_client(SessId),
-            {ok, #space_info{providers = ProviderIds}} = space_info:get_or_fetch(RestClient, SpaceId),
+            {ok, #document{value = #space_info{providers = ProviderIds}}} = space_info:get_or_fetch(RestClient, SpaceId),
             case {ProviderIds, lists:member(oneprovider:get_provider_id(), ProviderIds)} of
                 {_, true} ->
                     HandleLocallyFun();
