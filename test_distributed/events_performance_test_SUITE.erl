6c4fe1540b96687b880f5a11ccd90bb0162dadbb
Rafał Słota <roxeon@gmail.com>
2016-04-01 00:17:41 +0200

    VFS-1521 Fix teardown in events_performance_test_SUITE

--- test_distributed/events_performance_test_SUITE.erl
+++ test_distributed/events_performance_test_SUITE.erl
@@ -534,11 +534,11 @@ end_per_testcase(subscribe_should_work_for_multiple_sessions, Config) ->
     test_utils:mock_validate_and_unload(Workers, communicator);
 
 end_per_testcase(_, Config) ->
-    [Worker | _] = ?config(op_worker_nodes, Config),
+    [Worker | _] = Workers = ?config(op_worker_nodes, Config),
     SessId = ?config(session_id, Config),
     session_teardown(Worker, SessId),
     initializer:clean_test_users_and_spaces_no_validate(Config),
-    test_utils:mock_validate_and_unload(Worker, communicator).
+    test_utils:mock_validate_and_unload(Workers, communicator).
 
 %%%===================================================================
 %%% Internal functions
