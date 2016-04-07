6009f5fa2ef811cca832fa4bb15a0d3b08d3da1e
Tomasz Lichon <xorver@gmail.com>
2016-04-07 15:38:24 +0200

    Squashed 'helpers/' changes from be7c821..c45cc09
    c45cc09 Put fsync success callback inside else statement, to prevent returning two callbacks.
    856d92b Merge branch 'develop' into feature/VFS-1747-fsync-storage-after-file-replication
    a184bae Merge pull request #176 in VFS/helpers from bugfix/fix-uninitialized-s3-security-token to develop
    a883aa1 Initialize S3 bucket context.
    7f928c4 Merge pull request #175 in VFS/helpers from feature/VFS-1728-timeouts-update to develop
    08d6d5b Merge pull request #174 in VFS/helpers from bugfix/VFS-1757-fix-oneprovider-onezone-application to develop
    3058326 VFS-1728-increase default timeout
    6f7d975 Merge commit 'fdf076c04b582e14ca5bbad11e3a20a6f65d366d' into bugfix/VFS-1757-fix-oneprovider-onezone-application
    fdf076c Squashed 'bamboos/' changes from a30c6f2..2729a2b
    
    git-subtree-dir: helpers
    git-subtree-split: c45cc0903b8f1fac3199549308c73ba79e122f76

--- bamboos/docker/env_up.py
+++ bamboos/docker/env_up.py
@@ -10,12 +10,11 @@ Run the script with -h flag to learn about script's running options.
 """
 
 from __future__ import print_function
+
 import argparse
 import json
-import os
 
 from environment import env
-
 
 parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -50,11 +49,11 @@ parser.add_argument(
     dest='bin_cluster_manager')
 
 parser.add_argument(
-    '-bg', '--bin-gr',
+    '-boz', '--bin-oz',
     action='store',
-    default=env.default('bin_gr'),
-    help='the path to globalregistry repository (precompiled)',
-    dest='bin_gr')
+    default=env.default('bin_oz'),
+    help='the path to zone repository (precompiled)',
+    dest='bin_oz')
 
 parser.add_argument(
     '-ba', '--bin-appmock',
@@ -85,8 +84,10 @@ parser.add_argument(
 args = parser.parse_args()
 
 output = env.up(args.config_path, image=args.image, bin_am=args.bin_am,
-       bin_gr=args.bin_gr, bin_cluster_manager=args.bin_cluster_manager,
-       bin_op_worker=args.bin_op_worker, bin_cluster_worker=args.bin_cluster_worker,
-       bin_oc=args.bin_oc, logdir=args.logdir)
+                bin_oz=args.bin_oz,
+                bin_cluster_manager=args.bin_cluster_manager,
+                bin_op_worker=args.bin_op_worker,
+                bin_cluster_worker=args.bin_cluster_worker,
+                bin_oc=args.bin_oc, logdir=args.logdir)
 
 print(json.dumps(output))
