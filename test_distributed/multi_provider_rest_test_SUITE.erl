846850a1d9d4b87fe85ca400edf693c942408d00
Tomasz Lichon <xorver@gmail.com>
2016-05-09 13:53:57 +0200

    VFS-2003 Prepare rest test suites.

---
+++ test_distributed/multi_provider_rest_test_SUITE.erl
@@ -0,0 +1,81 @@
+%%%-------------------------------------------------------------------
+%%% @author Tomasz Lichon
+%%% @copyright (C) 2016 ACK CYFRONET AGH
+%%% This software is released under the MIT license
+%%% cited in 'LICENSE.txt'.
+%%% @end
+%%%-------------------------------------------------------------------
+%%% @doc
+%%% Multi provider rest tests
+%%% @end
+%%%-------------------------------------------------------------------
+-module(multi_provider_rest_test_SUITE).
+-author("Tomasz Lichon").
+
+-include("global_definitions.hrl").
+-include("http/rest/cdmi/cdmi_errors.hrl").
+-include("http/rest/cdmi/cdmi_capabilities.hrl").
+-include("http/rest/http_status.hrl").
+-include("proto/common/credentials.hrl").
+-include_lib("ctool/include/logging.hrl").
+-include_lib("ctool/include/test/test_utils.hrl").
+-include_lib("ctool/include/test/assertions.hrl").
+-include_lib("ctool/include/test/performance.hrl").
+-include_lib("ctool/include/posix/file_attr.hrl").
+-include_lib("ctool/include/posix/errors.hrl").
+-include_lib("ctool/include/posix/acl.hrl").
+
+%% API
+-export([all/0, init_per_suite/1, end_per_suite/1, init_per_testcase/2,
+    end_per_testcase/2]).
+
+-export([
+    file_distribution/1
+]).
+
+all() ->
+    ?ALL([
+        file_distribution
+    ]).
+
+-define(TIMEOUT, timer:seconds(5)).
+
+
+%%%===================================================================
+%%% Test functions
+%%%===================================================================
+
+file_distribution(Config) ->
+    [_WorkerP1, _WorkerP2] = ?config(op_worker_nodes, Config).
+
+%%%===================================================================
+%%% SetUp and TearDown functions
+%%%===================================================================
+
+init_per_suite(Config) ->
+    ConfigWithNodes = ?TEST_INIT(Config, ?TEST_FILE(Config, "env_desc.json"), [initializer]),
+    initializer:setup_storage(ConfigWithNodes).
+
+end_per_suite(Config) ->
+    initializer:teardown_storage(Config),
+    test_node_starter:clean_environment(Config).
+
+init_per_testcase(_, Config) ->
+    application:start(ssl2),
+    hackney:start(),
+    ConfigWithSessionInfo = initializer:create_test_users_and_spaces(?TEST_FILE(Config, "env_desc.json"), Config),
+    initializer:enable_grpca_based_communication(Config),
+    lfm_proxy:init(ConfigWithSessionInfo).
+
+end_per_testcase(_, Config) ->
+    lfm_proxy:teardown(Config),
+     %% TODO change for initializer:clean_test_users_and_spaces after resolving VFS-1811
+    initializer:clean_test_users_and_spaces_no_validate(Config),
+    initializer:disable_grpca_based_communication(Config),
+    hackney:stop(),
+    application:stop(ssl2).
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
