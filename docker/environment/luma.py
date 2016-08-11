60293e2d4920a2685e15c4a93abf5586a19f1994
Rafał Słota <roxeon@gmail.com>
2016-08-11 16:39:49 +0200

    Squashed 'bamboos/' changes from 6cfbde1..0f0a931
    0f0a931 Merge pull request #144 in VFS/bamboos from feature/VFS-2293-replace-bamboo-artifacts to develop
    671b0d0 VFS-2293 - applying patch to change owner of logdir in worker
    235ee0c VFS-2293 - use locks when pushing artifact, code refactor
    bfeaa54 Merge pull request #145 in VFS/bamboos from feature/VFS-2336-update-luma-api-to-swagger-version to develop
    3b059ff VFS-2293 - use locks when pulling artifact
    1278752 VFS-2293 - code improvements according to PR
    c2a1849 VFS-2336 Update LUMA config
    6b1d5b6 VFS-2293 - use 'with' keyword when opening scp connection
    18adb6d VFS-2293-bugfixes
    de3333d VFS-2293 - scripts for pushing and pulling artifacts
    6be5406 Merge pull request #143 in VFS/bamboos from bugfix/assigning-permissions-to-storage-dir to develop
    06dfbf5 change logical operators to bitwise operators in os.chmod argument
    5c24fdd delete doubled function
    cc5fda3 Merge pull request #138 in VFS/bamboos from feature/VFS-2002-gui-selenium-tests to develop
    3336e5d Merge branch 'develop' of ssh://git.plgrid.pl:7999/vfs/bamboos into feature/VFS-2002-gui-selenium-tests
    39a8877 Merge pull request #141 in VFS/bamboos from feature/VFS-2177-implement-luma-swift-functionality to develop
    e4cd526 Merge pull request #142 in VFS/bamboos from bugfix/VFS-2218-check-why-swift-storage-docker-fails to develop
    d72901c Merge pull request #137 in VFS/bamboos from feature/VFS-1994-api-for-tests-of-user-and-space to develop
    133aef1 VFS-2177 Add swift to luma config
    569742f VFS-1994 - add docstring to docker.cp function
    b8bc0ca VFS-1994 - delete unused imports
    ed94a6f Merge remote-tracking branch 'origin/develop' into feature/VFS-1994-api-for-tests-of-user-and-space
    d03559d VFS-1994 - add onepanel rest port and protocol to sys.config
    57927a2 VFS-2002 Updated help in test_run.py (GUI)
    6811e08 VFS-1994 - add onepanel_rest_url to oz sys_config
    9b2a160 VFS-1994-fix typog
    447c43a Merge remote-tracking branch 'origin/feature/VFS-1847-onepanel-awesome-spring-refresh' into feature/VFS-1994-api-for-tests-of-user-and-space
    6fc3381 Merge remote-tracking branch 'origin/develop' into feature/VFS-1994-api-for-tests-of-user-and-space
    4568886 VFS-2002 A function to copy local etc/hosts entries to docker
    a41e441 VFS-1918 - bugfixes
    b3991e0 VFS-2054 add flags to escript to allow running escript only for mapping storages, without registering in oz and without setting up test entities
    148c75d Merge remote-tracking branch 'origin/develop' into feature/VFS-1994-api-for-tests-of-user-and-space
    d5a0f90 Merge remote-tracking branch 'origin/develop' into feature/VFS-1994-api-for-tests-of-user-and-space
    2504c89 Merge remote-tracking branch 'origin/feature/VFS-1918-refactor-onedata-tests' into feature/VFS-1994-api-for-tests-of-user-and-space
    54298ff VFS-1944 fix bug
    8aa952a VFS-1944 add docker cp
    ad3ec61 VFS-1847 Update panel.py script.
    
    git-subtree-dir: bamboos
    git-subtree-split: 0f0a931c36de88729dfa5876d9cdf8021fbfbcdf

--- docker/environment/luma.py
+++ docker/environment/luma.py
@@ -16,6 +16,7 @@ default_config = {
         "DEBUG": "True",
         "DATABASE": "'luma_database.db'",
         "HOST": "'0.0.0.0'",
+        "API_KEY": "'example_api_key'"
     },
     "generators_config": {
         "ceph": {
@@ -31,20 +32,28 @@ default_config = {
         "posix": {
             "lowest_uid": 1000,
             "highest_uid": 65536
+        },
+        "swift": {
+            "user_name": "swift",
+            "password": "swift"
         }
     },
     "generators_mapping": [
         {
-            "storage_id": "Ceph",
-            "generator_id": "ceph"
+            "storageType": "Ceph",
+            "generatorId": "ceph"
         },
         {
-            "storage_type": "DirectIO",
-            "generator_id": "posix"
+            "storageType": "Posix",
+            "generatorId": "posix"
         },
         {
-            "storage_type": "AmazonS3",
-            "generator_id": "s3"
+            "storageType": "S3",
+            "generatorId": "s3"
+        },
+        {
+            "storageType": "Swift",
+            "generatorId": "swift"
         }
     ],
     "storages_mapping": [],
