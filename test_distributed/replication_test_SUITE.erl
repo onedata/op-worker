4d76b8e3ee00cc2d35645dea78c0e86acbe9b1fb
Rafał Słota <roxeon@gmail.com>
2016-09-19 16:16:17 +0200

    Fix reference to undefined function

--- test_distributed/replication_test_SUITE.erl
+++ test_distributed/replication_test_SUITE.erl
@@ -138,7 +138,7 @@ local_file_location_should_have_correct_uid_for_local_user(Config) ->
     },
     {ok, FileUuid} = ?assertMatch({ok, _}, rpc:call(W1, file_meta, create, [{uuid, SpaceDirUuid}, FileMeta])),
     {ok, FileToCompareGUID} = lfm_proxy:create(W1, SessionId, <<SpaceName/binary, "/file_to_compare">>, 8#777),
-    FileToCompareUUID = fslogic_uuid:file_guid_to_uuid(FileToCompareGUID),
+    FileToCompareUUID = fslogic_uuid:guid_to_uuid(FileToCompareGUID),
 
     [$/ | FileToCompareFID] = binary_to_list(?rpc(fslogic_utils, gen_storage_file_id, [{uuid, FileToCompareUUID}])),
     [$/ | FileFID] = binary_to_list(?rpc(fslogic_utils, gen_storage_file_id, [{uuid, FileUuid}])),
@@ -185,7 +185,7 @@ local_file_location_should_be_chowned_when_missing_user_appears(Config) ->
     {ok, FileUuid} = ?assertMatch({ok, _}, rpc:call(W1, file_meta, create, [{uuid, SpaceDirUuid}, FileMeta])),
     {ok, FileUuid2} = ?assertMatch({ok, _}, rpc:call(W1, file_meta, create, [{uuid, SpaceDirUuid}, FileMeta2])),
     {ok, FileToCompareGUID} = lfm_proxy:create(W1, SessionId, <<SpaceName/binary, "/file_to_compare">>, 8#777),
-    FileToCompareUUID = fslogic_uuid:file_guid_to_uuid(FileToCompareGUID),
+    FileToCompareUUID = fslogic_uuid:guid_to_uuid(FileToCompareGUID),
 
     [$/ | FileToCompareFID] = binary_to_list(rpc:call(W1, fslogic_utils, gen_storage_file_id, [{uuid, FileToCompareUUID}])),
     [$/ | File1FID] = binary_to_list(rpc:call(W1, fslogic_utils, gen_storage_file_id, [{uuid, FileUuid}])),
