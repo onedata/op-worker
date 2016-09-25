01a92a8cf8fc532084577773e4b323f013b0d770
Łukasz Opioła <lukasz.opiola@gmail.com>
2016-09-25 16:30:22 +0200

    VFS-2567 Use new approach to shared files displaying in public view

--- src/http/gui_backend/public_share_data_backend.erl
+++ src/http/gui_backend/public_share_data_backend.erl
@@ -29,7 +29,6 @@
 -export([init/0, terminate/0]).
 -export([find/2, find_all/1, find_query/2]).
 -export([create_record/2, update_record/3, delete_record/2]).
--export([file_record/2]).
 
 %%%===================================================================
 %%% API functions
@@ -76,24 +75,142 @@ find(<<"share-public">>, ShareId) ->
         {<<"containerDir">>, <<"containerDir.", ShareId/binary>>},
         {<<"publicUrl">>, PublicURL}
     ]};
-find(<<"file-public">>, FileId) ->
-    try
-        file_record(?GUEST_SESS_ID, FileId)
-    catch T:M ->
-        ?warning("Cannot get meta-data for file (~p). ~p:~p", [
-            FileId, T, M
-        ]),
-        {ok, [{<<"id">>, FileId}, {<<"type">>, <<"broken">>}]}
+find(<<"file-public">>, <<"containerDir.", ShareId/binary>>) ->
+    UserAuth = op_gui_utils:get_user_auth(),
+    {ok, #document{
+        value = #share_info{
+            name = Name,
+            root_file_id = RootFileId
+        }}} = share_logic:get(UserAuth, ShareId),
+    FileId = fslogic_uuid:share_guid_to_guid(RootFileId),
+    Res = [
+        {<<"id">>, <<"containerDir.", ShareId/binary>>},
+        {<<"name">>, Name},
+        {<<"type">>, <<"dir">>},
+        {<<"permissions">>, 0},
+        {<<"modificationTime">>, 0},
+        {<<"size">>, 0},
+        {<<"parent">>, null},
+        {<<"children">>, [op_gui_utils:ids_to_association(ShareId, FileId)]},
+        {<<"fileAcl">>, null},
+        {<<"share">>, null},
+        {<<"provider">>, null},
+        {<<"fileProperty">>, null}
+    ],
+    {ok, Res};
+
+find(<<"file-public">>,AssocId) ->
+    SessionId = g_session:get_session_id(),
+    {ShareId, FileId} = op_gui_utils:association_to_ids(AssocId),
+    case logical_file_manager:stat(SessionId, {guid, FileId}) of
+        {error, ?ENOENT} ->
+            gui_error:report_error(<<"No such file or directory.">>);
+        {ok, FileAttr} ->
+            #file_attr{
+                name = Name,
+                type = TypeAttr,
+                size = SizeAttr,
+                mtime = ModificationTime,
+                mode = PermissionsAttr,
+                shares = Shares,
+                provider_id = ProviderId
+            } = FileAttr,
+
+            ParentUUID = case Shares of
+                [ShareId] ->
+                    % Check if this is the root dir
+                    <<"containerDir.", ShareId/binary>>;
+                [] ->
+                    op_gui_utils:ids_to_association(
+                        ShareId, get_parent(SessionId, FileId)
+                    )
+            end,
+
+            {Type, Size} = case TypeAttr of
+                ?DIRECTORY_TYPE -> {<<"dir">>, null};
+                _ -> {<<"file">>, SizeAttr}
+            end,
+            Permissions = integer_to_binary((PermissionsAttr rem 1000), 8),
+            Children = case Type of
+                <<"file">> ->
+                    [];
+                <<"dir">> ->
+                    case logical_file_manager:ls(
+                        SessionId, {guid, FileId}, 0, 1000) of
+                        {ok, List} ->
+                            List;
+                        _ ->
+                            []
+                    end
+            end,
+            ChildrenIds = [
+                op_gui_utils:ids_to_association(ShareId, ChId) ||
+                {ChId, _} <- Children
+            ],
+            {ok, HasCustomMetadata} = logical_file_manager:has_custom_metadata(
+                SessionId, {guid, FileId}
+            ),
+            Metadata = case HasCustomMetadata of
+                false -> null;
+                true -> FileId
+            end,
+            Res = [
+                {<<"id">>, AssocId},
+                {<<"name">>, Name},
+                {<<"type">>, Type},
+                {<<"permissions">>, Permissions},
+                {<<"modificationTime">>, ModificationTime},
+                {<<"size">>, Size},
+                {<<"parent">>, ParentUUID},
+                {<<"children">>, ChildrenIds},
+                {<<"fileAcl">>, FileId},
+                {<<"share">>, null},
+                {<<"provider">>, ProviderId},
+                {<<"fileProperty">>, Metadata}
+            ],
+            {ok, Res}
     end;
-find(<<"file-property-public">>, FileId) ->
-    try
-        metadata_record(?GUEST_SESS_ID, FileId)
-    catch T:M ->
-        ?warning("Cannot get metadata for file (~p). ~p:~p", [
-            FileId, T, M
-        ]),
-        gui_error:internal_server_error()
-    end.
+
+
+find(<<"file-property-public">>, AssocId) ->
+    SessionId = g_session:get_session_id(),
+    {_, FileId} = op_gui_utils:association_to_ids(AssocId),
+    {ok, XattrKeys} = logical_file_manager:list_xattr(
+        SessionId, {guid, FileId}, false, false
+    ),
+    Basic = lists:map(
+        fun(Key) ->
+            {ok, #xattr{value = Value}} = logical_file_manager:get_xattr(
+                SessionId, {guid, FileId}, Key, false
+            ),
+            {Key, Value}
+        end, XattrKeys),
+    BasicVal = case Basic of
+        [] -> null;
+        _ -> Basic
+    end,
+    GetJSONResult = logical_file_manager:get_metadata(
+        SessionId, {guid, FileId}, json, [], false
+    ),
+    JSONVal = case GetJSONResult of
+        {error, ?ENOATTR} -> null;
+        {ok, Map} when map_size(Map) =:= 0 -> <<"{}">>;
+        {ok, JSON} -> json_utils:decode(json_utils:encode_map(JSON))
+    end,
+    GetRDFResult = logical_file_manager:get_metadata(
+        SessionId, {guid, FileId}, rdf, [], false
+    ),
+    RDFVal = case GetRDFResult of
+        {error, ?ENOATTR} -> null;
+        {ok, RDF} -> RDF
+    end,
+    {ok, [
+        {<<"id">>, AssocId},
+        {<<"file">>, AssocId},
+        {<<"basic">>, BasicVal},
+        {<<"json">>, JSONVal},
+        {<<"rdf">>, RDFVal}
+    ]}.
 
 
 %%--------------------------------------------------------------------
@@ -159,120 +276,11 @@ delete_record(_, _Id) ->
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
-%% Constructs a file record from given FileId.
+%% Returns the UUID of parent of given file. This is needed because
+%% spaces dir has two different UUIDs, should be removed when this is fixed.
 %% @end
 %%--------------------------------------------------------------------
--spec file_record(SessionId :: binary(), FileId :: binary()) ->
-    {ok, proplists:proplist()}.
-file_record(SessionId, FileId) ->
-    case logical_file_manager:stat(SessionId, {guid, FileId}) of
-        {error, ?ENOENT} ->
-            gui_error:report_error(<<"No such file or directory.">>);
-        {ok, FileAttr} ->
-            #file_attr{
-                name = Name,
-                type = TypeAttr,
-                size = SizeAttr,
-                mtime = ModificationTime,
-                mode = PermissionsAttr,
-                provider_id = ProviderId
-            } = FileAttr,
-
-            % Resolve parent guid of this file
-            {ok, ParentGUID} = logical_file_manager:get_parent(
-                SessionId, {guid, FileId}
-            ),
-            % If the parent guid is the same as file id, it means that it is
-            % the root file of the share -> tell ember there is no parent.
-            Parent = case ParentGUID of
-                FileId -> null;
-                GUID -> GUID
-            end,
-            {Type, Size} = case TypeAttr of
-                ?DIRECTORY_TYPE -> {<<"dir">>, null};
-                _ -> {<<"file">>, SizeAttr}
-            end,
-            Permissions = integer_to_binary((PermissionsAttr rem 1000), 8),
-            Children = case Type of
-                <<"file">> ->
-                    [];
-                <<"dir">> ->
-                    case logical_file_manager:ls(
-                        SessionId, {guid, FileId}, 0, 1000) of
-                        {ok, List} ->
-                            List;
-                        _ ->
-                            []
-                    end
-            end,
-            ChildrenIds = [ChId || {ChId, _} <- Children],
-            {ok, HasCustomMetadata} = logical_file_manager:has_custom_metadata(
-                SessionId, {guid, FileId}
-            ),
-            Metadata = case HasCustomMetadata of
-                false -> null;
-                true -> FileId
-            end,
-            Res = [
-                {<<"id">>, FileId},
-                {<<"name">>, Name},
-                {<<"type">>, Type},
-                {<<"permissions">>, Permissions},
-                {<<"modificationTime">>, ModificationTime},
-                {<<"size">>, Size},
-                {<<"parent">>, Parent},
-                {<<"children">>, ChildrenIds},
-                {<<"fileAcl">>, FileId},
-                {<<"share">>, null},
-                {<<"provider">>, ProviderId},
-                {<<"fileProperty">>, Metadata}
-            ],
-            {ok, Res}
-    end.
-
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Constructs a metadata record for given FileId.
-%% @end
-%%--------------------------------------------------------------------
--spec metadata_record(SessionId :: binary(), FileId :: binary()) ->
-    {ok, proplists:proplist()} | gui_error:error_result().
-metadata_record(SessionId, FileId) ->
-    {ok, XattrKeys} = logical_file_manager:list_xattr(
-        SessionId, {guid, FileId}, false, false
-    ),
-    Basic = lists:map(
-        fun(Key) ->
-            {ok, #xattr{value = Value}} = logical_file_manager:get_xattr(
-                SessionId, {guid, FileId}, Key, false
-            ),
-            {Key, Value}
-        end, XattrKeys),
-    BasicVal = case Basic of
-        [] -> null;
-        _ -> Basic
-    end,
-    GetJSONResult = logical_file_manager:get_metadata(
-        SessionId, {guid, FileId}, json, [], false
-    ),
-    JSONVal = case GetJSONResult of
-        {error, ?ENOATTR} -> null;
-        {ok, Map} when map_size(Map) =:= 0 -> <<"{}">>;
-        {ok, JSON} -> json_utils:decode(json_utils:encode_map(JSON))
-    end,
-    GetRDFResult = logical_file_manager:get_metadata(
-        SessionId, {guid, FileId}, rdf, [], false
-    ),
-    RDFVal = case GetRDFResult of
-        {error, ?ENOATTR} -> null;
-        {ok, RDF} -> RDF
-    end,
-    {ok, [
-        {<<"id">>, FileId},
-        {<<"filePublic">>, FileId},
-        {<<"basic">>, BasicVal},
-        {<<"json">>, JSONVal},
-        {<<"rdf">>, RDFVal}
-    ]}.
+-spec get_parent(SessionId :: binary(), FileGUID :: binary()) -> binary().
+get_parent(SessionId, FileGUID) ->
+    {ok, ParentGUID} = logical_file_manager:get_parent(SessionId, {guid, FileGUID}),
+    ParentGUID.
