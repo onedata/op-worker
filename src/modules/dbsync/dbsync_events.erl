f51d85f3ed46565c69b81e610c2aed2036f45c21
Tomasz Lichon <t.j.lichon@gmail.com>
2016-10-09 23:27:48 +0200

    VFS-2665 Improve consistency checking in dbsync_events module.

--- src/modules/dbsync/dbsync_events.erl
+++ src/modules/dbsync/dbsync_events.erl
@@ -56,16 +56,17 @@ change_replicated_internal(_SpaceId, #change{model = file_meta, doc =  #document
 change_replicated_internal(SpaceId, Change = #change{model = file_meta, doc = FileDoc =
     #document{key = FileUUID, value = #file_meta{type = ?REGULAR_FILE_TYPE}}}) ->
     ?info("change_replicated_internal: changed file_meta ~p", [FileUUID]),
-    ok = file_consistency:wait(FileUUID, SpaceId, [file_meta, times, link_to_parent, parent_links], [SpaceId, Change]),
+    ok = file_consistency:wait(FileUUID, SpaceId, [times, link_to_parent, parent_links], [SpaceId, Change]),
     ok = fslogic_file_location:create_storage_file_if_not_exists(SpaceId, FileDoc),
     ok = fslogic_event:emit_file_attr_update({uuid, FileUUID}, []),
-    ok = file_consistency:add_components_and_notify(FileUUID, [local_file_location]),
+    ok = file_consistency:add_components_and_notify(FileUUID, [file_meta, local_file_location]),
     ok = file_consistency:check_and_add_components(FileUUID, SpaceId, [parent_links]);
-change_replicated_internal(SpaceId, #change{model = file_meta, doc = #document{key = FileUUID, value = #file_meta{}}}) ->
+change_replicated_internal(SpaceId, Change = #change{model = file_meta, doc = #document{key = FileUUID, value = #file_meta{}}}) ->
     ?info("change_replicated_internal: changed file_meta ~p", [FileUUID]),
-    ok = file_consistency:check_and_add_components(FileUUID, SpaceId, [times]),
+    ok = file_consistency:wait(FileUUID, SpaceId, [times], [SpaceId, Change]),
     ok = fslogic_event:emit_file_attr_update({uuid, FileUUID}, []),
-    ok = file_consistency:add_components_and_notify(FileUUID, [file_meta]);
+    ok = file_consistency:add_components_and_notify(FileUUID, [file_meta]),
+    ok = file_consistency:check_and_add_components(FileUUID, SpaceId, [parent_links]);
 change_replicated_internal(SpaceId, Change = #change{model = file_location, doc = Doc = #document{value = #file_location{uuid = FileUUID}}}) ->
     ?info("change_replicated_internal: changed file_location ~p", [FileUUID]),
     ok = file_consistency:wait(FileUUID, SpaceId, [file_meta, times, local_file_location], [SpaceId, Change]),
