336256d1cc1d068c0a6a168da2579c44c3f56309
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2017-07-19 11:37:12 +0200

    Squashed 'bamboos/' changes from f35b0fc..30916ef
    30916ef VFS-3448 Reconfigure couchbase RAM quota
    08208bf Merge pull request #181 in VFS/bamboos from feature/VFS-3347-auto-update-of-onepanel-test-dockers to develop
    63d8f2c VFS-3347 Set docker config dir in ct_run.py
    4053452 VFS-3347 Enable set of docker build report name
    58b97ce Merge pull request #179 in VFS/bamboos from feature/VFS-3250-implement-glusterfs-storage-helper to develop
    c77323f Merge pull request #180 in VFS/bamboos from feature/VFS-3236-integrate-dbsync to develop
    027322b Merge branch 'develop' into feature/VFS-3250-implement-glusterfs-storage-helper
    8a006dc Added GlusterFS support
    86a115a Merge pull request #178 in VFS/bamboos from feature/VFS-3319-integrate-new-datastore-with-old to develop
    c3f6616 VFS-3236 Update couchbase server to version 4.5.1
    
    git-subtree-dir: bamboos
    git-subtree-split: 30916ef5ee66c7e3be74c8908ba893cd1478ee00

--- docker/environment/provider_worker.py
+++ docker/environment/provider_worker.py
@@ -104,7 +104,7 @@ class ProviderWorkerConfigurator:
         return 1024
 
     def couchbase_buckets(self):
-        return {"default": 300, "sync": 400}
+        return {"onedata": 1024}
 
     def app_name(self):
         return "op_worker"
@@ -124,7 +124,8 @@ def create_storages(storages, op_nodes, op_config, bindir, storages_dockers):
     script_names = {'posix': 'create_posix_storage.escript',
                     's3': 'create_s3_storage.escript',
                     'ceph': 'create_ceph_storage.escript',
-                    'swift': 'create_swift_storage.escript'}
+                    'swift': 'create_swift_storage.escript',
+                    'glusterfs': 'create_glusterfs_storage.escript'}
     pwd = common.get_script_dir()
     for script_name in script_names.values():
         command = ['cp', os.path.join(pwd, script_name),
@@ -166,7 +167,6 @@ def create_storages(storages, op_nodes, op_config, bindir, storages_dockers):
                        config['access_key'], config['secret_key'], 'true']
             assert 0 is docker.exec_(container, command, tty=True,
                                      stdout=sys.stdout, stderr=sys.stderr)
-
         elif storage['type'] == 'swift':
             config = storages_dockers['swift'][storage['name']]
             command = ['escript', script_paths['swift'], cookie,
@@ -177,7 +177,14 @@ def create_storages(storages, op_nodes, op_config, bindir, storages_dockers):
                        config['user_name'], config['password'], 'true']
             assert 0 is docker.exec_(container, command, tty=True,
                                      stdout=sys.stdout, stderr=sys.stderr)
-
+        elif storage['type'] == 'glusterfs':
+            config = storages_dockers['glusterfs'][storage['name']]
+            command = ['escript', script_paths['glusterfs'], cookie,
+                       first_node, storage['name'], storage['volume'],
+                       config['host_name'], str(config['port']), config['transport'],
+                       config['mountpoint'], 'cluster.write-freq-threshold=100;', 'true']
+            assert 0 is docker.exec_(container, command, tty=True,
+                                     stdout=sys.stdout, stderr=sys.stderr)
         else:
             raise RuntimeError(
                 'Unknown storage type: {}'.format(storage['type']))
