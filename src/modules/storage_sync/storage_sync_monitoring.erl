a427c4aa4712cd9420910eeb3c133af4b8daeb60
Jakub Kudzia <jakub.kudzia@outlook.com>
2017-10-02 16:53:04 +0200

    VFS-3709 fix dialyzer

--- src/modules/storage_sync/storage_sync_monitoring.erl
+++ src/modules/storage_sync/storage_sync_monitoring.erl
@@ -503,7 +503,7 @@ start_reporter(?ETS_REPORTER_NAME) ->
 %% TODO improve handling failures of exometer VFS-3173
 %% @end
 %%-------------------------------------------------------------------
--spec resubscribe([atom()], od_space:id()) -> ok.
+-spec resubscribe(atom() | [atom()], od_space:id()) -> ok.
 resubscribe([], _SpaceId) ->
     ok;
 resubscribe(?LAGER_REPORTER_NAME, SpaceId) ->
