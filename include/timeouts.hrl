5551cf410b14dd563000c77555ace914637e4a70
Łukasz Opioła <lukasz.opiola@gmail.com>
2017-11-29 13:39:06 +0100

    VFS-3864 Lower default request timeout to 30 seconds

--- include/timeouts.hrl
+++ include/timeouts.hrl
@@ -10,12 +10,9 @@
 %%% @end
 -author("Jakub Kudzia").
 
-%%lfm_files.erl
--define(FSYNC_TIMEOUT, timer:minutes(1)).
-
 %% gateway_connection
 -define(CONNECTION_TIMEOUT, timer:minutes(1)).
 -define(REQUEST_COMPLETION_TIMEOUT, timer:minutes(5)).
 
 -define(SEND_RETRY_DELAY, timer:seconds(10)).
--define(DEFAULT_REQUEST_TIMEOUT, timer:minutes(5)).
+-define(DEFAULT_REQUEST_TIMEOUT, timer:seconds(30)).
