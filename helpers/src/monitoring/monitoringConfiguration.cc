00544c7ce6bf3413460ebb9ddb00e343295a755e
Konrad Zemek <konrad.zemek@gmail.com>
2018-01-14 02:29:56 +0100

    Switch to rebar-dependency helpers.

--- helpers/src/monitoring/monitoringConfiguration.cc
+++
@@ -1,9 +0,0 @@
-/**
- * @file monitoringConfiguration.cc
- * @author Bartek Kryza
- * @copyright (C) 2017 ACK CYFRONET AGH
- * @copyright This software is released under the MIT license cited in
- * 'LICENSE.txt'
- */
-
-#include "monitoring/monitoringConfiguration.h"
