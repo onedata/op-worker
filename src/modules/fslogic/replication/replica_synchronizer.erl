437a5f524769733198c3b023b6fb59b02069ffca
Michal Wrzeszcz <wrzeszcz@agh.edu.pl>
2018-07-25 13:14:21 +0200

    VFS-4611 Resolve conflicts after merge

--- src/modules/fslogic/replication/replica_synchronizer.erl
+++ src/modules/fslogic/replication/replica_synchronizer.erl
@@ -239,7 +239,7 @@ apply_or_run_locally(Uuid, InCacheFun, ApplyOnCacheFun, FallbackFun) ->
 -spec apply_if_alive_no_check(file_ctx:ctx(), term()) ->
     term().
 apply_if_alive_no_check(Uuid, FunOrMsg) ->
-    Node = consistent_hasing:get_node(Uuid),
+    Node = consistent_hashing:get_node(Uuid),
     rpc:call(Node, ?MODULE, apply_if_alive_internal, [Uuid, FunOrMsg]).
 
 %%--------------------------------------------------------------------
@@ -252,7 +252,7 @@ apply_if_alive_no_check(Uuid, FunOrMsg) ->
     term().
 apply_no_check(FileCtx, FunOrMsg) ->
     Uuid = file_ctx:get_uuid_const(FileCtx),
-    Node = consistent_hasing:get_node(Uuid),
+    Node = consistent_hashing:get_node(Uuid),
     rpc:call(Node, ?MODULE, apply_internal, [FileCtx, FunOrMsg]).
 
 %%--------------------------------------------------------------------
@@ -264,7 +264,7 @@ apply_no_check(FileCtx, FunOrMsg) ->
 -spec apply_or_run_locally_no_check(file_meta:uuid(), fun(() -> term()), fun(() -> term())) ->
     term().
 apply_or_run_locally_no_check(Uuid, Fun, FallbackFun) ->
-    Node = consistent_hasing:get_node(Uuid),
+    Node = consistent_hashing:get_node(Uuid),
     rpc:call(Node, ?MODULE, apply_or_run_locally_internal, [Uuid, Fun, FallbackFun]).
 
 %%%===================================================================
