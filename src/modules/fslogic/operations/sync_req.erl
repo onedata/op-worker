69da08d4800637c5a268f5f5bfedede4bec5b901
Michal Wrzeszcz <wrzeszcz@agh.edu.pl>
2018-09-04 21:29:40 +0200

    VFS-4813 Fix dialyzer and update rtransfer

--- src/modules/fslogic/operations/sync_req.erl
+++ src/modules/fslogic/operations/sync_req.erl
@@ -72,6 +72,8 @@ synchronize_block(UserCtx, FileCtx, Block, Prefetch, TransferId, Priority) ->
         Prefetch, TransferId, Priority) of
         {ok, Ans} ->
             #fuse_response{status = #status{code = ?OK}, fuse_response = Ans};
+        {error, cancelled} ->
+            throw(replication_cancelled);
         {error, _} = Error ->
             throw(Error)
     end.
@@ -290,15 +292,11 @@ replicate_dir(UserCtx, FileCtx, Block, Offset, TransferId) ->
 -spec replicate_regular_file(user_ctx:ctx(), file_ctx:ctx(), block(),
     transfer_id()) -> provider_response().
 replicate_regular_file(UserCtx, FileCtx, Block, TransferId) ->
-    case synchronize_block(UserCtx, FileCtx, Block, false, TransferId,
-        ?DEFAULT_REPLICATION_PRIORITY
-    ) of
-        #fuse_response{status = Status} ->
-            transfer:increment_files_processed_counter(TransferId),
-            #provider_response{status = Status};
-        {error, cancelled} ->
-            throw(replication_cancelled)
-    end.
+    #fuse_response{status = Status} =
+        synchronize_block(UserCtx, FileCtx, Block, false, TransferId,
+            ?DEFAULT_REPLICATION_PRIORITY),
+    transfer:increment_files_processed_counter(TransferId),
+    #provider_response{status = Status}.
 
 %%-------------------------------------------------------------------
 %% @doc
