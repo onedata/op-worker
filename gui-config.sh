46de6331bdcf5d285296aa96d58ce5e204560486
Jakub Liput <jakub.liput@gmail.com>
2019-04-02 10:23:50 +0200

    VFS-4751 Updating GUI to release candidate of unified GUI

--- gui-config.sh
+++ gui-config.sh
@@ -22,6 +22,6 @@ TARGET_DIR='_build/default/lib'
 ARCHIVE_NAME='gui_static.tar.gz'
 # Image which will be used by default to get the static files. If it cannot
 # be resolved, the script will fall back to secondary.
-PRIMARY_IMAGE='docker.onedata.org/op-gui-default:ID-b58129598a' # @fixme
+PRIMARY_IMAGE='docker.onedata.org/op-gui-default:ID-d43c1ff617'
 # Image which will be used if primary image is not resolved.
 SECONDARY_IMAGE='no-secondary-image' # @fixme
