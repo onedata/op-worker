54fc9fa773d474b90c11f21680993213fb1cec58
Michał Stanisz <mch.stanisz@gmail.com>
2020-01-08 14:31:59 +0100

    VFS-5853 Fixed CT tests

--- src/test_utils/initializer.erl
+++ src/test_utils/initializer.erl
@@ -1444,9 +1444,9 @@ storage_mock_setup(Workers) ->
             name = <<>>,
             qos_parameters = #{}
         }}} end),
-    ok = test_utils:mock_expect(Workers, storage_logic, fetch_qos_parameters_of_local_storage, fun(_) -> #{} end),
-    ok = test_utils:mock_expect(Workers, storage_logic, fetch_qos_parameters_of_remote_storage, fun(_,_) -> #{} end),
-    ok = test_utils:mock_expect(Workers, storage_logic, fetch_name,
+    ok = test_utils:mock_expect(Workers, storage_logic, get_qos_parameters_of_local_storage, fun(_) -> #{} end),
+    ok = test_utils:mock_expect(Workers, storage_logic, get_qos_parameters_of_remote_storage, fun(_,_) -> #{} end),
+    ok = test_utils:mock_expect(Workers, storage_logic, get_name,
         % storage name is equal to its id
         fun(#document{key = Id}) -> Id;
            (Id) -> Id
