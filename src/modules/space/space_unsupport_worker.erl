0a0ec6678016a7021189226f67aa4092bea4c41c
Michał Stanisz <mch.stanisz@gmail.com>
2020-02-27 18:18:10 +0100

    VFS-6147 Database cleanup

--- src/modules/space/space_unsupport_worker.erl
+++ src/modules/space/space_unsupport_worker.erl
@@ -14,6 +14,8 @@
 -author("Michal Stanisz").
 
 -behaviour(gen_server).
+% fixme not needed (and maybe whole behaviour and callbacks not needed)
+-behaviour(qos_caller_behaviour).
 
 -include("modules/datastore/qos.hrl").
 -include("modules/fslogic/fslogic_common.hrl").
@@ -28,6 +30,9 @@
 -export([debug/0]).
 -export([xd/2]).
 
+% fixme qos behaviour
+-export([qos_traverse_finished/2]).
+
 -define(SERVER, ?MODULE).
 
 -define(START(SpaceId, StorageId), {start, SpaceId, StorageId}).
@@ -36,7 +41,8 @@
 
 % fixme
 -record(state, {
-    current_stage :: space_unsupport:stage()
+    current_stage :: space_unsupport:stage(),
+    id :: qos_entry:id() | traverse:id()
     % fixme is canceling
 }).
 
@@ -52,6 +58,9 @@ next_stage(SpaceId, StorageId) ->
 
 debug() ->
     gen_server2:cast(?SERVER, debug).
+
+qos_traverse_finished(_QosEntryId, _) ->
+    ok.
 
 %%%===================================================================
 %%% Spawning and gen_server implementation
@@ -74,14 +83,15 @@ handle_cast(debug, State) ->
     {noreply, State};
     
 handle_cast(?START(SpaceId, StorageId), State) ->
+    % fixme ignore if exists
     gen_server2:cast(?SERVER, ?EXECUTE_STAGE(SpaceId, StorageId)),   
     {noreply, State#{{SpaceId, StorageId} => #state{current_stage = init}}};
 
 handle_cast(?EXECUTE_STAGE(SpaceId, StorageId), State) ->
-    #state{current_stage = CurrentStage} = maps:get({SpaceId, StorageId}, State),
+    #state{current_stage = CurrentStage} = InternalState = maps:get({SpaceId, StorageId}, State),
     ?notice("execute stage: ~p", [CurrentStage]),
-    execute_stage(CurrentStage, SpaceId, StorageId),
-    {noreply, State};
+    NewInternalState = execute_stage(InternalState, SpaceId, StorageId),
+    {noreply, State#{{SpaceId, StorageId} => NewInternalState}};
 
 handle_cast(?NEXT_STAGE(SpaceId, StorageId), State) ->
     #state{current_stage = CurrentStage} = maps:get({SpaceId, StorageId}, State),
@@ -93,6 +103,7 @@ handle_cast(?NEXT_STAGE(SpaceId, StorageId), State) ->
             gen_server2:cast(?SERVER, ?EXECUTE_STAGE(SpaceId, StorageId)),
             {noreply, State#{{SpaceId, StorageId} => #state{current_stage = NextStage}}}
     end.
+    
     
 handle_info(_Info, State) ->
     % fixme log
@@ -111,38 +122,37 @@ get_next_stage(clear_storage) -> wait_for_dbsync;
 get_next_stage(wait_for_dbsync) -> clean_database;
 get_next_stage(clean_database) -> finished.
 
-execute_stage(init, SpaceId, StorageId) ->
+execute_stage(#state{current_stage = init} = State, SpaceId, StorageId) ->
     % fixme inform zone space unsupported
     % fixme start blockades
     % fixme stop all incoming transfers
     % fixme kill handles
-    % fixme mozna zrownoleglić (+ qos)
-    next_stage(SpaceId, StorageId);
-execute_stage(qos, SpaceId, StorageId) ->
+    % fixme mozna zrownoleglić (+ qos) -> uzyc traversu 
+    next_stage(SpaceId, StorageId),
+    State;
+execute_stage(#state{current_stage = qos} = State, SpaceId, StorageId) ->
     SpaceGuid = fslogic_uuid:spaceid_to_space_dir_guid(SpaceId),
     % fixme if preferred provider replace AllStorages with provider Id (not really if other providers still allowed)
     Expression = <<?ALL_STORAGES/binary, " - storage_id = ", StorageId/binary>>,
-    % fixme internal qos (pid to inform? (what if dead - callback))
-    {ok, QosEntryId} = lfm:add_qos_entry(?ROOT_SESS_ID, {guid, SpaceGuid}, Expression, 1),
+    {ok, QosEntryId} = lfm:add_qos_entry(?ROOT_SESS_ID, {guid, SpaceGuid}, Expression, 1, ?MODULE),
     ?notice("QosEntry: ~p", [QosEntryId]),
-    % fixme use qos status sub
     spawn(fun() -> wait_for_qos(QosEntryId, SpaceId, StorageId) end),
-    ok;
-execute_stage(clear_storage, SpaceId, StorageId) ->
-    % fixme remove file_location ()
+    State#state{id = QosEntryId};
+execute_stage(#state{current_stage = clear_storage} = State, SpaceId, StorageId) ->
     % fixme do not clean storage if import is on
     {ok, TaskId} = unsupport_traverse:start(SpaceId, StorageId),
     ?notice("TaskId: ~p", [TaskId]),
     % fixme delete space root dir
-    ok;
-execute_stage(wait_for_dbsync, SpaceId, StorageId) ->
+    State#state{id = TaskId};
+execute_stage(#state{current_stage = wait_for_dbsync} = State, SpaceId, StorageId) ->
     % fixme
     % fixme wait for all docs to be saved on disc
-    timer:sleep(timer:seconds(15)),
+    timer:sleep(timer:seconds(60)),
     % fixme stop dbsync
-    next_stage(SpaceId, StorageId);
-execute_stage(clean_database, SpaceId, StorageId) ->
-    % fixme do it first (inform zone, that space is being unsupported)
+    next_stage(SpaceId, StorageId),
+    State;
+execute_stage(#state{current_stage = clean_database} = State, SpaceId, StorageId) ->
+    % fixme do it in init (inform zone, that space is being unsupported)
     storage_logic:revoke_space_support(StorageId, SpaceId),
     xd(SpaceId, StorageId),
     % fixme find which local documents are to be deleted and to it here (storage:space_unsupported?), space_quota
@@ -150,44 +160,43 @@ execute_stage(clean_database, SpaceId, StorageId) ->
     % fixme do not call storage (move it here?)
     storage:on_space_unsupported(SpaceId, StorageId),
     space_quota:delete(SpaceId),
-    unsupport_traverse:delete_ended(SpaceId, StorageId).
+    unsupport_traverse:delete_ended(SpaceId, StorageId),
+    State.
 
 
 wait_for_qos(QosEntryId, SpaceId, StorageId) ->
     case lfm:check_qos_fulfilled(?ROOT_SESS_ID, QosEntryId) of
         {ok, true} ->
-            lfm:remove_qos_entry(?ROOT_SESS_ID, QosEntryId),
+            lfm:remove_qos_entry(?ROOT_SESS_ID, QosEntryId, true),
             next_stage(SpaceId, StorageId);
         {ok, false} ->
-            timer:sleep(timer:seconds(1)),
-            wait_for_qos(QosEntryId, SpaceId, StorageId);
-        {error, _} = Error ->
-            ?error("wait for qos error: ~p", [Error]),
             timer:sleep(timer:seconds(1)),
             wait_for_qos(QosEntryId, SpaceId, StorageId)
     end.
 
 % fixme
--define(EXPIRY, 3).
+-define(EXPIRY, 30).
 
 xd(SpaceId, StorageId) ->
     Until = dbsync_state:get_seq(SpaceId, oneprovider:get_id()),
     Callback = 
-        fun ({ok, end_of_stream}) -> next_stage(SpaceId, StorageId);
+        fun ({ok, end_of_stream}) -> 
+                ?notice("end of stream"),
+                next_stage(SpaceId, StorageId);
             ({ok, Batch}) ->
-                lists:foreach(fun(#document{value = Value} = Doc) ->
-                    case Value of
+                lists:foreach(fun(#document{key = Ads, value = Value} = Doc) ->
+                    ?critical("xd: ~p", [Ads]),
+                    {ok, _} = case Value of
                         #links_forest{model = Model, key = Key} ->
-                            ?warning("links_forest"),
                             links_save(Model, Key, Doc);
                         #links_node{model = Model, key = Key} ->
-                            ?warning("links_node"),
                             links_save(Model, Key, Doc);
                         _ ->
-                            Model = element(1, Value),
-                            ?critical("xd: ~p", [Value]),
-                            Ctx = datastore_model_default:get_ctx(Model),
-                            datastore_model:save(Ctx#{hooks_disabled => true, expiry => ?EXPIRY}, Doc)
+                            datastore_model:save(#{
+                                model => element(1, Value),
+                                hooks_disabled => true, 
+                                expiry => ?EXPIRY
+                            }, Doc)
                     end
                 end, Batch);
             (Dupa) ->
@@ -198,17 +207,12 @@ xd(SpaceId, StorageId) ->
         [{since, 0}, {until, Until}], [self()]
     ).
 
-% fixme almost the same as in dbsync_changes
 links_save(Model, RoutingKey, Doc = #document{key = Key}) ->
-    Ctx = datastore_model_default:get_ctx(Model),
+    Ctx = Model:get_ctx(),
     Ctx2 = Ctx#{
-        local_links_tree_id => oneprovider:get_id(),
+        expiry => ?EXPIRY,
         routing_key => RoutingKey
     },
-    Ctx3 = datastore_multiplier:extend_name(RoutingKey, Ctx2),
-    case datastore_router:route(save, [Ctx3#{expiry => ?EXPIRY}, Key, Doc]) of
-        {ok, Doc2} ->
-            Doc2;
-        {error, ignored} ->
-            undefined
-    end.
+    Ctx3 = datastore_model_default:set_defaults(Ctx2),
+    Ctx4 = datastore_multiplier:extend_name(RoutingKey, Ctx3),
+    datastore_router:route(save, [Ctx4, Key, Doc]).
