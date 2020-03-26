4a35b61bf88b5c7718463e19cc3051a311881167
Bartosz Walkowicz <wallko1@hotmail.com>
2020-03-26 14:12:10 +0100

    VFS-6140 Add stubs for file_data_api and file_metadata_api test suites

---
+++ test_distributed/api/file_metadata_api_test_SUITE.erl
@@ -0,0 +1,30 @@
+%%%-------------------------------------------------------------------
+%%% @author Bartosz Walkowicz
+%%% @copyright (C) 2020 ACK CYFRONET AGH
+%%% This software is released under the MIT license
+%%% cited in 'LICENSE.txt'.
+%%% @end
+%%%-------------------------------------------------------------------
+%%% @doc
+%%% This file contains tests concerning file metadata basic API (REST + gs).
+%%% @end
+%%%-------------------------------------------------------------------
+-module(file_metadata_api_test_SUITE).
+-author("Bartosz Walkowicz").
+
+%% API
+-export([all/0]).
+-export([dummy_test/1]).
+
+all() -> [
+    dummy_test
+].
+
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+
+dummy_test(_Config) ->
+    ok.
