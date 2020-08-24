153c0845799430a86f69f32bdb193fc53b60c437
Michal Wrzeszcz <michalwrzeszcz@gmail.com>
2020-08-24 20:40:55 +0200

    VFS-6682 Update transfers test

--- test_distributed/transfers_restart_test_SUITE.erl
+++ test_distributed/transfers_restart_test_SUITE.erl
@@ -64,7 +64,7 @@ rtransfer_restart_test(Config) ->
         ct:pal("Rtransfer restarted")
     end,
 
-    restart_test_base(Config, RestartFun).
+    restart_test_base(Config, RestartFun, false).
 
 % TODO - delete fun
 rtransfer_restart_test2(Config) ->
@@ -82,14 +82,14 @@ node_restart_test(Config) ->
         ct:pal("Node restarted")
     end,
 
-    restart_test_base(Config, RestartFun).
+    restart_test_base(Config, RestartFun, true).
 
 % TODO - delete fun
 node_restart_test2(Config) ->
     NewConfig = provider_onenv_test_utils:initialize(Config),
     node_restart_test(NewConfig).
 
-restart_test_base(Config, RestartFun) ->
+restart_test_base(Config, RestartFun, NodeRestart) ->
     [P1, P2] = test_config:get_providers(Config),
     [WorkerP1] = test_config:get_provider_nodes(Config, P1),
     [WorkerP2] = test_config:get_provider_nodes(Config, P2),
@@ -99,7 +99,7 @@ restart_test_base(Config, RestartFun) ->
     SessId = fun(P) -> test_config:get_user_session_id_on_provider(Config, User1, P) end,
     SessIdP1 = SessId(P1),
     SessIdP2 = SessId(P2),
-    Attempts = 30,
+    Attempts = 60,
     FileSize = byte_size(?FILE_DATA),
     Priority = 32,
     UserCtxP2 = rpc:call(WorkerP2, user_ctx, new, [SessIdP2]),
@@ -108,20 +108,27 @@ restart_test_base(Config, RestartFun) ->
     % TODO - delete when framework handles node stop properly
     onenv_test_utils:disable_panel_healthcheck(Config),
 
-    Files = lists:map(fun(_) ->
-        File = generator:gen_name(),
-        {ok, FileGuid} = ?assertMatch({ok, _}, rpc:call(WorkerP1, lfm, create, [SessIdP1, SpaceGuid, File, 8#755], 5000)),
-        {ok, Handle} = ?assertMatch({ok, _}, rpc:call(WorkerP1, lfm, open, [SessIdP1, {guid, FileGuid}, rdwr], 5000)),
-        {ok, NewHandle, _} = ?assertMatch({ok, _, _}, rpc:call(WorkerP1, lfm, write,  [Handle, 0, ?FILE_DATA], 5000)),
-        ?assertEqual(ok, rpc:call(WorkerP1, lfm, fsync, [NewHandle], 35000)),
-        ?assertEqual(ok, rpc:call(WorkerP1, lfm, release, [NewHandle], 5000)),
-        FileGuid
+    Files1 = lists:map(fun(_) ->
+        create_file(WorkerP1, SessIdP1, SpaceGuid)
     end, lists:seq(1, 1000)),
+    Files2 = lists:map(fun(_) ->
+        create_file(WorkerP1, SessIdP1, SpaceGuid)
+    end, lists:seq(1, 1000)),
+    AllFiles = Files1 ++ Files2,
 
     lists:foreach(fun(File) ->
         ?assertMatch({ok, #file_attr{type = ?REGULAR_FILE_TYPE, size = FileSize}},
             rpc:call(WorkerP2, lfm, stat, [SessIdP2, {guid, File}]), Attempts)
-    end, Files),
+    end, AllFiles),
+
+    lists_utils:pforeach(fun(File) ->
+        FileCtx = file_ctx:new_by_guid(File),
+        lists:foreach(fun(Offset) ->
+            ?assertMatch(#fuse_response{status = #status{code = ?OK}},
+                rpc:call(WorkerP2, sync_req, request_block_synchronization,
+                    [UserCtxP2, FileCtx, #file_block{offset = Offset, size = 1}, false, undefined, Priority]))
+        end, lists:seq(0, FileSize))
+    end, Files1),
 
     PMapAns = lists_utils:pmap(fun(File) ->
         FileCtx = file_ctx:new_by_guid(File),
@@ -131,7 +138,7 @@ restart_test_base(Config, RestartFun) ->
                     [UserCtxP2, FileCtx, #file_block{offset = Offset, size = 1}, false, undefined, Priority]))
         end, lists:seq(0,9)),
         rpc:call(WorkerP2, lfm, schedule_file_replication, [SessIdP2, {guid, File}, P2, undefined])
-    end, Files),
+    end, Files2),
 
     TransferIDs = lists:map(fun(Ans) ->
         {ok, TransferID} = ?assertMatch({ok, _}, Ans),
@@ -143,6 +150,18 @@ restart_test_base(Config, RestartFun) ->
     lists:foreach(fun(TransferId) ->
         multi_provider_file_ops_test_base:await_replication_end(WorkerP2, TransferId, Attempts, get_effective)
     end, TransferIDs),
+
+    FilesToCheckDistribution = case NodeRestart of
+        true -> Files2;
+        false -> AllFiles
+    end,
+    lists:foreach(fun(File) ->
+        % Use root session as user session is not valid after restart
+        ?assertMatch({ok, [
+            #{<<"blocks">> := [[0, FileSize]], <<"totalBlocksSize">> := FileSize},
+            #{<<"blocks">> := [[0, FileSize]], <<"totalBlocksSize">> := FileSize}
+        ]}, rpc:call(WorkerP2, lfm, get_file_distribution, [?ROOT_SESS_ID, {guid, File}]), Attempts)
+    end, FilesToCheckDistribution),
 
     ok.
 
@@ -171,12 +190,24 @@ init_per_testcase(_Case, Config) ->
     Workers = test_config:get_all_op_worker_nodes(Config),
     test_utils:set_env(Workers, ?APP_NAME, minimal_sync_request, 1),
     test_utils:set_env(Workers, ?APP_NAME, synchronizer_block_suiting, false),
-    lfm_proxy:init(Config, false).
-
-
-end_per_testcase(_Case, Config) ->
-    lfm_proxy:teardown(Config),
     Config.
+
+
+end_per_testcase(_Case, _Config) ->
+    ok.
 
 end_per_suite(_Config) ->
     ok.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+create_file(Worker, SessId, SpaceGuid) ->
+    {ok, FileGuid} = ?assertMatch({ok, _}, rpc:call(Worker, lfm, create,
+        [SessId, SpaceGuid, generator:gen_name(), 8#755], 5000)),
+    {ok, Handle} = ?assertMatch({ok, _}, rpc:call(Worker, lfm, open, [SessId, {guid, FileGuid}, rdwr], 5000)),
+    {ok, NewHandle, _} = ?assertMatch({ok, _, _}, rpc:call(Worker, lfm, write,  [Handle, 0, ?FILE_DATA], 5000)),
+    ?assertEqual(ok, rpc:call(Worker, lfm, fsync, [NewHandle], 35000)),
+    ?assertEqual(ok, rpc:call(Worker, lfm, release, [NewHandle], 5000)),
+    FileGuid.
