81cc188b23dd13b84e43db5154962da53a186856
Jakub Kudzia <jakub.kudzia@outlook.com>
2020-10-15 17:10:02 +0200

    VFS-6862 add verification of auto-cleaning in single_node_failure_test_SUITE, fix restart of auto-cleaning after provider restart

--- test_distributed/single_node_failure_test_SUITE.erl
+++ test_distributed/single_node_failure_test_SUITE.erl
@@ -28,7 +28,7 @@ all() -> [
     failure_test
 ].
 
--define(FILE_DATA, <<"1234567890abcd">>).
+-define(FILE_TEST_CONTENT, <<"1234567890abcd">>).
 -define(ATTEMPTS, 30).
 
 %%%===================================================================
@@ -103,82 +103,15 @@ create_initial_data_structure(Config) ->
     }.
 
 create_test_data(Config, InitialData) ->
-    [P1, P2] = test_config:get_providers(Config),
-    [WorkerP1] = test_config:get_provider_nodes(Config, P1),
-    [WorkerP2] = test_config:get_provider_nodes(Config, P2),
-    [User1] = test_config:get_provider_users(Config, P1),
-    SessId = fun(P) -> test_config:get_user_session_id_on_provider(Config, User1, P) end,
-    [SpaceId | _] = test_config:get_provider_spaces(Config, P1),
-    SpaceGuid = fslogic_uuid:spaceid_to_space_dir_guid(SpaceId),
-    P1DirGuid = kv_utils:get([test_dirs, P1], InitialData),
-    P2DirGuid = kv_utils:get([test_dirs, P2], InitialData),
-    ImportingProvider = kv_utils:get(failing_provider, InitialData),
-    [ImportingOpNode | _] = test_config:get_provider_nodes(Config, ImportingProvider),
+    TestData2 = prepare_files(Config, InitialData, #{}),
+    TestData3 = prepare_import(Config, InitialData, TestData2),
+    prepare_auto_cleaning(Config, InitialData, TestData3).
 
-
-    % check whether initial scan has been finished
-    storage_import_test_base:assertInitialScanFinished(ImportingOpNode, SpaceId, ?ATTEMPTS),
-    storage_import_test_base:assertNoScanInProgress(ImportingOpNode, SpaceId, ?ATTEMPTS),
-    FinishedScans = storage_import_test_base:get_finished_scans_num(ImportingOpNode, SpaceId),
-
-    % wait for new files to occur on storage
-    % the storage supporting the space is a nulldevice storage with simulated filesystem that grows with the time
-    timer:sleep(timer:seconds(1)),
-    % mock importing process to block
-    block_import(ImportingOpNode),
-    % forcefully start import scan
-    storage_import_test_base:start_scan(ImportingOpNode, SpaceId),
-
-    #{
-        files_and_dirs => #{
-            P1 => #{
-                root => create_files_and_dirs(WorkerP1, SessId(P1), SpaceGuid),
-                local => create_files_and_dirs(WorkerP1, SessId(P1), P1DirGuid),
-                remote => create_files_and_dirs(WorkerP1, SessId(P1), P2DirGuid)
-            },
-            P2 => #{
-                root => create_files_and_dirs(WorkerP2, SessId(P2), SpaceGuid),
-                local => create_files_and_dirs(WorkerP2, SessId(P2), P2DirGuid),
-                remote => create_files_and_dirs(WorkerP2, SessId(P2), P1DirGuid)
-            }
-        },
-        finished_scans => FinishedScans
-    }.
 
 verify(Config, InitialData, TestData, StopAppBeforeKill) ->
-    Providers = [P1, P2] = test_config:get_providers(Config),
-    [User1] = test_config:get_provider_users(Config, P1),
-    SessId = fun(P) -> test_config:get_user_session_id_on_provider(Config, User1, P) end,
-    [SpaceId | _] = test_config:get_provider_spaces(Config, P1),
-    SpaceGuid = fslogic_uuid:spaceid_to_space_dir_guid(SpaceId),
-    FailingProvider = kv_utils:get(failing_provider, InitialData),
-    [NotFailingProvider] = Providers -- [FailingProvider],
-    [FailingNode] = test_config:get_provider_nodes(Config, FailingProvider),
-    [NotFailingNode] = test_config:get_provider_nodes(Config, NotFailingProvider),
-    verify_all_files_and_dirs_created_by_provider(NotFailingNode, SessId(NotFailingProvider), TestData, NotFailingProvider),
-
-    case StopAppBeforeKill of
-        true ->
-            % App was stopped before node killing - all data should be present
-            verify_all_files_and_dirs_created_by_provider(FailingNode, SessId(FailingProvider), TestData, NotFailingProvider),
-            verify_all_files_and_dirs_created_by_provider(FailingNode, SessId(FailingProvider), TestData, FailingProvider),
-            verify_all_files_and_dirs_created_by_provider(NotFailingNode, SessId(NotFailingProvider), TestData, FailingProvider);
-        false ->
-            % App wasn't stopped before node killing - some data can be lost
-            % but operations on dirs should be possible,
-            P1DirGuid = kv_utils:get([test_dirs, P1], InitialData),
-            P2DirGuid = kv_utils:get([test_dirs, P2], InitialData),
-            test_new_files_and_dirs_creation(Config, SpaceGuid),
-            test_new_files_and_dirs_creation(Config, P1DirGuid),
-            test_new_files_and_dirs_creation(Config, P2DirGuid)
-    end,
-
-    % verify import
-    % check whether scan is correctly restarted after node restart
-    % get last finished scan
-    Scans0 = kv_utils:get(finished_scans, TestData),
-    % wait till next scan is finished
-    ?assertEqual(Scans0 + 1, storage_import_test_base:get_finished_scans_num(FailingNode, SpaceId), ?ATTEMPTS).
+    verify_files(Config, InitialData, TestData, StopAppBeforeKill),
+    verify_import(Config, InitialData, TestData),
+    verify_auto_cleaning(Config, InitialData, TestData).
 
 
 verify_all_files_and_dirs_created_by_provider(Worker, SessId, TestData, ProviderId) ->
@@ -223,9 +156,156 @@ end_per_testcase(_Case, Config) ->
 end_per_suite(_Config) ->
     ok.
 
+%%%===================================================================
+%%% Preparation functions
+%%%===================================================================
+
+prepare_files(Config, InitialData, TestData) ->
+    [P1, P2] = test_config:get_providers(Config),
+    [WorkerP1] = test_config:get_provider_nodes(Config, P1),
+    [WorkerP2] = test_config:get_provider_nodes(Config, P2),
+    [User1] = test_config:get_provider_users(Config, P1),
+    SessId = fun(P) -> test_config:get_user_session_id_on_provider(Config, User1, P) end,
+    [SpaceId | _] = test_config:get_provider_spaces(Config, P1),
+    SpaceGuid = fslogic_uuid:spaceid_to_space_dir_guid(SpaceId),
+    P1DirGuid = kv_utils:get([test_dirs, P1], InitialData),
+    P2DirGuid = kv_utils:get([test_dirs, P2], InitialData),
+
+    TestData#{
+        files_and_dirs => #{
+            P1 => #{
+                root => create_files_and_dirs(WorkerP1, SessId(P1), SpaceGuid),
+                local => create_files_and_dirs(WorkerP1, SessId(P1), P1DirGuid),
+                remote => create_files_and_dirs(WorkerP1, SessId(P1), P2DirGuid)
+            },
+            P2 => #{
+                root => create_files_and_dirs(WorkerP2, SessId(P2), SpaceGuid),
+                local => create_files_and_dirs(WorkerP2, SessId(P2), P2DirGuid),
+                remote => create_files_and_dirs(WorkerP2, SessId(P2), P1DirGuid)
+            }
+        }
+    }.
+
+prepare_import(Config, InitialData, TestData) ->
+    ImportingProvider = kv_utils:get(failing_provider, InitialData),
+    [ImportingOpNode | _] = test_config:get_provider_nodes(Config, ImportingProvider),
+    [SpaceId | _] = test_config:get_provider_spaces(Config, ImportingProvider),
+
+    % check whether initial scan has been finished
+    storage_import_test_base:assertInitialScanFinished(ImportingOpNode, SpaceId, ?ATTEMPTS),
+    storage_import_test_base:assertNoScanInProgress(ImportingOpNode, SpaceId, ?ATTEMPTS),
+    FinishedScans = storage_import_test_base:get_finished_scans_num(ImportingOpNode, SpaceId),
+
+    % wait for new files to occur on storage
+    % the storage supporting the space is a nulldevice storage with simulated filesystem that grows with the time
+    timer:sleep(timer:seconds(1)),
+    % mock importing process to block
+    block_import(ImportingOpNode),
+    % forcefully start import scan
+    storage_import_test_base:start_scan(ImportingOpNode, SpaceId),
+
+    TestData#{finished_scans => FinishedScans}.
+
+
+prepare_auto_cleaning(Config, InitialData, TestData) ->
+    FailingProvider = kv_utils:get(failing_provider, InitialData),
+    [FailingNode | _] = test_config:get_provider_nodes(Config, FailingProvider),
+    [SpaceId | _] = test_config:get_provider_spaces(Config, FailingProvider),
+    [User1] = test_config:get_provider_users(Config, FailingProvider),
+    SessId = fun(P) -> test_config:get_user_session_id_on_provider(Config, User1, P) end,
+    SpaceGuid = fslogic_uuid:spaceid_to_space_dir_guid(SpaceId),
+
+    ok = rpc:call(FailingNode, file_popularity_api, enable, [SpaceId]),
+    block_auto_cleaning(FailingNode),
+    ok = rpc:call(FailingNode, autocleaning_api, configure, [SpaceId, #{enabled => true, target => 0}]),
+
+    {ok, {_, Handle}} = lfm_proxy:create_and_open(FailingNode, SessId(FailingProvider), SpaceGuid,
+        generator:gen_name(), 8#664),
+    {ok, _} = lfm_proxy:write(FailingNode, Handle, 0, ?FILE_TEST_CONTENT),
+    lfm_proxy:close(FailingNode, Handle),
+    ?assertEqual(true, rpc:call(FailingNode, space_quota, current_size, [SpaceId]) > 0 , ?ATTEMPTS),
+
+    {ok, ARId} = rpc:call(FailingNode, autocleaning_api, force_run, [SpaceId]),
+
+    TestData#{autocleaning_run_id => ARId}.
+
+
+%%%===================================================================
+%%% Verification functions
+%%%===================================================================
+
+verify_files(Config, InitialData, TestData, StopAppBeforeKill) ->
+    Providers = [P1, P2] = test_config:get_providers(Config),
+    [User1] = test_config:get_provider_users(Config, P1),
+    SessId = fun(P) -> test_config:get_user_session_id_on_provider(Config, User1, P) end,
+    [SpaceId | _] = test_config:get_provider_spaces(Config, P1),
+    SpaceGuid = fslogic_uuid:spaceid_to_space_dir_guid(SpaceId),
+    FailingProvider = kv_utils:get(failing_provider, InitialData),
+    [NotFailingProvider] = Providers -- [FailingProvider],
+    [FailingNode] = test_config:get_provider_nodes(Config, FailingProvider),
+    [NotFailingNode] = test_config:get_provider_nodes(Config, NotFailingProvider),
+    verify_all_files_and_dirs_created_by_provider(NotFailingNode, SessId(NotFailingProvider), TestData, NotFailingProvider),
+
+    case StopAppBeforeKill of
+        true ->
+            % App was stopped before node killing - all data should be present
+            verify_all_files_and_dirs_created_by_provider(FailingNode, SessId(FailingProvider), TestData, NotFailingProvider),
+            verify_all_files_and_dirs_created_by_provider(FailingNode, SessId(FailingProvider), TestData, FailingProvider),
+            verify_all_files_and_dirs_created_by_provider(NotFailingNode, SessId(NotFailingProvider), TestData, FailingProvider);
+        false ->
+            % App wasn't stopped before node killing - some data can be lost
+            % but operations on dirs should be possible,
+            P1DirGuid = kv_utils:get([test_dirs, P1], InitialData),
+            P2DirGuid = kv_utils:get([test_dirs, P2], InitialData),
+            test_new_files_and_dirs_creation(Config, SpaceGuid),
+            test_new_files_and_dirs_creation(Config, P1DirGuid),
+            test_new_files_and_dirs_creation(Config, P2DirGuid)
+    end.
+
+verify_import(Config, InitialData, TestData) ->
+    ImportingProvider = kv_utils:get(failing_provider, InitialData),
+    [ImportingOpNode | _] = test_config:get_provider_nodes(Config, ImportingProvider),
+    [SpaceId | _] = test_config:get_provider_spaces(Config, ImportingProvider),
+
+    % check whether scan is correctly restarted after node restart
+    % get last finished scan
+    Scans0 = kv_utils:get(finished_scans, TestData),
+    % wait till next scan is finished
+    ?assertEqual(Scans0 + 1, storage_import_test_base:get_finished_scans_num(ImportingOpNode, SpaceId), ?ATTEMPTS).
+
+
+verify_auto_cleaning(Config, InitialData, TestData) ->
+    FailingProvider = kv_utils:get(failing_provider, InitialData),
+    [FailingNode | _] = test_config:get_provider_nodes(Config, FailingProvider),
+    [SpaceId | _] = test_config:get_provider_spaces(Config, FailingProvider),
+
+    % auto-cleaning runs will be failed as cleanup was not performed because there was nothing to clean
+
+    ARId1 = kv_utils:get(autocleaning_run_id, TestData),
+    %check whether run started before provider was stopped/killed was restarted and finished
+    ?assertMatch({ok, #{status := <<"failed">>}}, rpc:call(FailingNode, autocleaning_api, get_run_report, [ARId1]), ?ATTEMPTS),
+
+    %check whether next run will be started and finished
+    {ok, ARId2} = rpc:call(FailingNode, autocleaning_api, force_run, [SpaceId]),
+    ?assertMatch({ok, #{status := <<"failed">>}}, rpc:call(FailingNode, autocleaning_api, get_run_report, [ARId2]), ?ATTEMPTS).
+
+
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
 block_import(OpwNode) ->
     test_node_starter:load_modules([OpwNode], [?MODULE]),
     ok = test_utils:mock_new(OpwNode, storage_import_engine),
     ok = test_utils:mock_expect(OpwNode, storage_import_engine, import_file_unsafe, fun(_StorageFileCtx, _Info) ->
+        timer:sleep(timer:minutes(10))
+    end).
+
+
+block_auto_cleaning(OpwNode) ->
+    test_node_starter:load_modules([OpwNode], [?MODULE]),
+    ok = test_utils:mock_new(OpwNode, autocleaning_view_traverse),
+    ok = test_utils:mock_expect(OpwNode, autocleaning_view_traverse, process_row, fun(_Row, _Info, _RowNumber) ->
         timer:sleep(timer:minutes(10))
     end).
