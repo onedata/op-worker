479fa5416d0743ab7d84907d7aab52ba89fec312
Bartosz Walkowicz <wallko1@hotmail.com>
2020-10-27 07:41:45 +0100

    VFS-6891 Properly sort rest routes before passing them to cowboy

--- src/http/rest/rest_routes.erl
+++ src/http/rest/rest_routes.erl
@@ -32,12 +32,12 @@
 -spec routes() -> [{binary(), module(), map()}].
 routes() ->
     AllRoutes = lists:flatten([
-        file_registration_rest_routes:routes(),
         basic_file_operations_rest_routes:routes(),
         custom_file_metadata_rest_routes:routes(),
         deprecated_file_api_rest_routes:routes(),
         file_distribution_rest_routes:routes(),
         file_path_resolution_rest_routes:routes(),
+        file_registration_rest_routes:routes(),
         monitoring_rest_routes:routes(),
         oneprovider_rest_routes:routes(),
         qos_rest_routes:routes(),
@@ -48,20 +48,44 @@ routes() ->
         view_rest_routes:routes()
     ]),
 
+    SortedRoutes = sort_routes(AllRoutes),
+
     % Aggregate routes that share the same path
     AggregatedRoutes = lists:foldr(fun
         ({Path, Handler, #rest_req{method = Method} = RestReq}, [{Path, _, RoutesForPath} | Acc]) ->
             [{Path, Handler, RoutesForPath#{Method => RestReq}} | Acc];
         ({Path, Handler, #rest_req{method = Method} = RestReq}, Acc) ->
             [{Path, Handler, #{Method => RestReq}} | Acc]
-    end, [], AllRoutes),
+    end, [], SortedRoutes),
 
     % Convert all routes to cowboy-compliant routes
     % - prepend REST prefix to every route
-    % - rest handler module must be added as second element to the tuples
     % - RoutesForPath will serve as Opts to rest handler init.
     {ok, PrefixStr} = application:get_env(?APP_NAME, op_rest_api_prefix),
     Prefix = str_utils:to_binary(PrefixStr),
     lists:map(fun({Path, Handler, RoutesForPath}) ->
         {<<Prefix/binary, Path/binary>>, Handler, RoutesForPath}
     end, AggregatedRoutes).
+
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Sorts rest routes alphanumerically with accounting for fact that any concrete
+%% path must precede path with match (e.g. `data/register` must precede `data/:id`).
+%% Otherwise it would be impossible to make requests for such routes.
+%% @end
+%%--------------------------------------------------------------------
+-spec sort_routes({binary(), module(), #rest_req{}}) ->
+    [{binary(), module(), #rest_req{}}].
+sort_routes(AllRoutes) ->
+    % Replace ':' (ASCII 58) with `}` (ASCII 125) as this makes routes properly sortable
+    SortableYetInvalidRoutes = lists:map(fun({Path, Handler, RestReq}) ->
+        {binary:replace(Path, <<":">>, <<"}">>, [global]), Handler, RestReq}
+    end, AllRoutes),
+
+    SortedInvalidRoutes = lists:sort(SortableYetInvalidRoutes),
+
+    lists:map(fun({Path, Handler, RestReq}) ->
+        {binary:replace(Path, <<"}">>, <<":">>, [global]), Handler, RestReq}
+    end, SortedInvalidRoutes).
