c224e37828fc2771ec4045b98ae4a52e0780ed36
Bartosz Walkowicz <wallko1@hotmail.com>
2021-03-29 18:15:33 +0200

    VFS-7429 Add dummy dataset api suites

---
+++ test_distributed/suites/api/dataset/api_dataset_crud_test_SUITE.erl
@@ -0,0 +1,59 @@
+%%%-------------------------------------------------------------------
+%%% @author Bartosz Walkowicz
+%%% @copyright (C) 2021 ACK CYFRONET AGH
+%%% This software is released under the MIT license
+%%% cited in 'LICENSE.txt'.
+%%% @end
+%%%-------------------------------------------------------------------
+%%% @doc
+%%% This file contains tests concerning dataset crud API (REST + gs).
+%%% @end
+%%%-------------------------------------------------------------------
+-module(api_dataset_crud_test_SUITE).
+-author("Bartosz Walkowicz").
+
+-include("api_test_runner.hrl").
+
+-export([
+    all/0,
+    init_per_suite/1, end_per_suite/1,
+    init_per_testcase/2, end_per_testcase/2
+]).
+
+-export([
+    dummy_test/1
+]).
+
+
+all() -> [
+    dummy_test
+].
+
+
+dummy_test(Config) ->
+    ok.
+
+
+%%%===================================================================
+%%% SetUp and TearDown functions
+%%%===================================================================
+
+
+init_per_suite(Config) ->
+    oct_background:init_per_suite(Config, #onenv_test_config{
+        onenv_scenario = "api_tests",
+        envs = [{op_worker, op_worker, [{fuse_session_grace_period_seconds, 24 * 60 * 60}]}]
+    }).
+
+
+end_per_suite(_Config) ->
+    oct_background:end_per_suite().
+
+
+init_per_testcase(_Case, Config) ->
+    ct:timetrap({minutes, 10}),
+    Config.
+
+
+end_per_testcase(_Case, _Config) ->
+    ok.
