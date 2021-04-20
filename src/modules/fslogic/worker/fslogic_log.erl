360d48c355c21f399a7158b177fb6c2b7d33974f
Łukasz Opioła <ymopiola@cyfronet.pl>
2021-04-20 23:04:34 +0200

    VFS-7579 Improve file access audit log; please dialyzer and ensure proper handling of unknown file context

--- src/modules/fslogic/worker/fslogic_log.erl
+++ src/modules/fslogic/worker/fslogic_log.erl
@@ -20,8 +20,8 @@
 -export([report_file_access_operation/3]).
 -export([mask_data_in_message/1]).
 
--type logged_record() :: fslogic_worker:request() | fuse_request_type() | file_request_type() 
-    | provider_request_type() | proxyio_request_type().
+-type logged_record() :: fslogic_worker:request() | fuse_request_type() | file_request_type()
+| provider_request_type() | proxyio_request_type().
 
 -define(MAX_BINARY_DATA_SIZE, 32).
 
@@ -29,26 +29,14 @@
 %%% API
 %%%===================================================================
 
--spec report_file_access_operation(fslogic_worker:request(), file_ctx:ctx(), od_user:id()) -> ok.
-report_file_access_operation(Request, FileCtx, UserId) ->
+-spec report_file_access_operation(fslogic_worker:request(), od_user:id(), file_ctx:ctx() | undefined) ->
+    ok.
+report_file_access_operation(Request, UserId, FileCtx) ->
     case op_worker:get_env(file_access_audit_log_enabled, false) of
-        false -> ok;
         true ->
-            FormattedRequest = format_request(Request),
-            Uuid = file_ctx:get_uuid_const(FileCtx),
-            FormattedShareId = 
-                str_utils:to_list(utils:undefined_to_null(file_ctx:get_share_id_const(FileCtx))),
-            FilePath = try
-                {Path, _} = file_ctx:get_canonical_path(FileCtx),
-                Path
-            catch _:_ ->
-                % could not resolve file path, log anyway
-                <<"">>
-            end,
-            ok = lager:log(file_access_audit_lager_event, info, self(),
-                "request: ~s; user: ~s; file_uuid: ~s; share_id: ~s; path: ~ts",
-                    [FormattedRequest, UserId, Uuid, FormattedShareId, FilePath]
-            )
+            append_to_audit_log(Request, UserId, FileCtx);
+        false ->
+            ok
     end.
 
 
@@ -86,6 +74,42 @@ mask_data_in_message(Message) ->
 %%% Internal functions
 %%%===================================================================
 
+%% @private
+-spec append_to_audit_log(fslogic_worker:request(), od_user:id(), file_ctx:ctx() | undefined) ->
+    ok.
+append_to_audit_log(Request, UserId, undefined) ->
+    append_to_audit_log(Request, UserId, null, null, null);
+append_to_audit_log(Request, UserId, FileCtx) ->
+    Uuid = file_ctx:get_uuid_const(FileCtx),
+    ShareId = utils:undefined_to_null(file_ctx:get_share_id_const(FileCtx)),
+    FilePath = try
+        {Path, _} = file_ctx:get_canonical_path(FileCtx),
+        Path
+    catch _:_ ->
+        % could not resolve file path, log anyway
+        null
+    end,
+    append_to_audit_log(Request, UserId, Uuid, ShareId, FilePath).
+
+
+-spec append_to_audit_log(
+    fslogic_worker:request(),
+    od_user:id(),
+    file_meta:uuid() | null,
+    od_share:id() | null,
+    file_meta:path() | null
+) ->
+    ok.
+%% @private
+append_to_audit_log(Request, UserId, Uuid, ShareId, FilePath) ->
+    FormattedRequest = format_request(Request),
+    ok = lager:log(file_access_audit_lager_event, info, self(),
+        "request: ~s; user: ~s; file_uuid: ~s; share_id: ~s; path: ~ts",
+        [FormattedRequest, UserId, Uuid, ShareId, FilePath]
+    ).
+
+
+%% @private
 -spec format_request(logged_record()) -> string().
 format_request(#file_request{file_request = SubRecord} = Record) ->
     format_inner_record(Record, SubRecord);
@@ -95,16 +119,18 @@ format_request(#provider_request{provider_request = SubRecord} = Record) ->
     format_inner_record(Record, SubRecord);
 format_request(#proxyio_request{proxyio_request = SubRecord} = Record) ->
     format_inner_record(Record, SubRecord);
-format_request(Record) -> 
+format_request(Record) ->
     str_utils:format("~tp", [utils:record_type(Record)]).
 
 
+%% @private
 -spec format_inner_record(logged_record(), logged_record()) -> string().
 format_inner_record(Record, SubRecord) ->
     str_utils:format("~tp::~s", [utils:record_type(Record), format_request(SubRecord)]).
 
 
 %%--------------------------------------------------------------------
+%% @private
 %% @doc
 %% Shortens binary data into format "<SIZE> bytes", for logging purposes.
 %% @end
