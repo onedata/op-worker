bf99c3a4aff4b9ec9cdb9792583901f69ccf70ee
duleba.pio <duleba.pio@gmail.com>
2021-05-20 22:11:37 +0200

    VFS-7667 move url prasing logic to ctool

--- src/modules/storage/helpers/helper_params.erl
+++ src/modules/storage/helpers/helper_params.erl
@@ -19,7 +19,6 @@
 -include("modules/datastore/datastore_models.hrl").
 -include("proto/common/credentials.hrl").
 -include_lib("ctool/include/aai/aai.hrl").
--include_lib("ctool/include/errors.hrl").
 -include_lib("ctool/include/logging.hrl").
 
 %% API
@@ -29,9 +28,6 @@
 %% Onepanel RPC API
 -export([prepare_helper_args/2, prepare_user_ctx_params/2]).
 
-%% Export for eunit tests.
--export([parse_url/1]).
-
 -type name() :: helper:name().
 -type args() :: helper:args().
 -type user_ctx() :: helper:user_ctx().
@@ -39,9 +35,6 @@
 -type field() :: binary().
 -type optional_field() :: {optional, field()}.
 -type field_spec() :: field() | optional_field().
-
--define(DEFAULT_HTTP_PORT, 80).
--define(DEFAULT_HTTPS_PORT, 443).
 
 %%%===================================================================
 %%% API
@@ -69,14 +62,14 @@ prepare_helper_args(HelperName, Params) ->
 -spec derive_scheme_from_url(args()) -> args().
 derive_scheme_from_url(#{<<"hostname">> := Hostname, <<"scheme">> := Scheme} = Params) ->
     HostnameWithScheme = str_utils:join_binary([Scheme, <<"://">>, Hostname]),
-    {ok, UrlScheme, Host} = parse_url(HostnameWithScheme),
+    {ok, UrlScheme, Host} = get_scheme_and_hostname(HostnameWithScheme),
     Scheme = case UrlScheme of
         https -> <<"https">>;
         _ -> <<"http">>
     end,
     Params#{<<"scheme">> => Scheme, <<"hostname">> => Host};
 derive_scheme_from_url(#{<<"hostname">> := HostnameWithScheme} = Params) ->
-    {ok, UrlScheme, Host} = parse_url(HostnameWithScheme),
+    {ok, UrlScheme, Host} = get_scheme_and_hostname(HostnameWithScheme),
     Scheme = case UrlScheme of
         https -> <<"https">>;
         _ -> <<"http">>
@@ -326,45 +319,6 @@ remove_field(ToRemove, Fields) ->
     end end, Fields).
 
 
-%% @private
--spec parse_url(URL :: binary()) ->
-    {ok, Scheme :: http | https, HostAndPort :: binary()}.
-parse_url(URL) ->
-    try
-        parse_url_insecure(URL)
-    catch
-        _:_ -> throw(?ERROR_MALFORMED_DATA)
-    end.
-
-
-%% @private
--spec parse_url_insecure(URL :: binary()) ->
-    {ok, Scheme :: http | https, HostAndPort :: binary()} | no_return().
-parse_url_insecure(URL) ->
-    URLTrimmed = list_to_binary(string:trim(binary_to_list(URL))),
-    #{host := ParsedHost, path := ParsedPath} = url_utils:parse(URLTrimmed),
-    URLScheme = get_scheme_from_url(URLTrimmed),
-    URLPort = get_port_from_url(URLTrimmed, ParsedHost),
-
-    {FinalScheme, FinalPort} = case {URLScheme, URLPort} of
-        {undefined, undefined} ->
-            throw(?ERROR_MALFORMED_DATA);
-        {undefined, ?DEFAULT_HTTPS_PORT} ->
-            {https, ?DEFAULT_HTTPS_PORT};
-        {undefined, ?DEFAULT_HTTP_PORT} ->
-            {http, ?DEFAULT_HTTP_PORT};
-        %% TODO: VFS-7682 - following match should throw an error
-        {undefined, CustomPort} ->
-            {http, CustomPort};
-        {http, CustomPort} ->
-            {http, utils:ensure_defined(CustomPort, ?DEFAULT_HTTP_PORT)};
-        {https, CustomPort} ->
-            {https, utils:ensure_defined(CustomPort, ?DEFAULT_HTTPS_PORT)}
-    end,
-
-    {ok, FinalScheme, str_utils:format_bin("~ts:~B~ts", [ParsedHost, FinalPort, ParsedPath])}.
-
-
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
@@ -408,21 +362,8 @@ validate_field(Field, Params) ->
 
 
 %% @private
--spec get_scheme_from_url(binary()) -> http | https | undefined.
-get_scheme_from_url(URL) ->
-    case hd(binary:split(URL, [<<"://">>])) of
-        <<"https">> -> https;
-        <<"http">> -> http;
-        _ -> undefined
-    end.
-
-
-%% @private
--spec get_port_from_url(binary(), binary()) -> integer() | undefined.
-get_port_from_url(URL, Hostname) ->
-    case binary:split(URL, Hostname) of
-        [_, <<":", PortAndPathPart/binary>>] ->
-            binary_to_integer(hd(string:split(PortAndPathPart, "/")));
-        _ ->
-            undefined
-    end.
+-spec get_scheme_and_hostname(binary()) ->
+    {ok, Scheme :: http | https, HostAndPort :: binary()} | no_return().
+get_scheme_and_hostname(Url) ->
+    #{scheme := Scheme, host := Host, port := Port, path := Path} = url_utils:infer_components(Url),
+    {ok, Scheme, str_utils:format_bin("~ts:~B~ts", [Host, Port, Path])}.
