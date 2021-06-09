bca32109f2bbbe6a7c021c5fcddc4acff6e80dba
Bartosz Walkowicz <wallko1@hotmail.com>
2021-06-09 14:38:50 +0200

    VFS-7672 Fix atm workflow execution collection tests

--- test_distributed/suites/automation/atm_workflow_executions_collection_test_SUITE.erl
+++ test_distributed/suites/automation/atm_workflow_executions_collection_test_SUITE.erl
@@ -22,7 +22,6 @@
 -export([
     groups/0, all/0,
     init_per_suite/1, end_per_suite/1,
-    init_per_group/2, end_per_group/2,
     init_per_testcase/2, end_per_testcase/2
 ]).
 
@@ -108,6 +107,8 @@ all() -> [
     ?WAITING_PHASE, ?ONGOING_PHASE, ?ENDED_PHASE
 ]).
 
+-define(DUMMY_ATM_INVENTORY_ID, <<"dummyId">>).
+
 -define(DEFAULT_PROVIDER_SYNC_TIME_SEC, 15).
 -define(ATTEMPTS, 30).
 
@@ -136,13 +137,16 @@ add_links_test_base(Phase) ->
     KrkNode = oct_background:get_random_provider_node(krakow),
     ParNode = oct_background:get_random_provider_node(paris),
 
-    AtmWorkflowExecutionDoc = gen_rand_workflow(SpaceId),
+    AtmInventoryId = str_utils:rand_hex(32),
+    AtmWorkflowExecutionDoc = gen_rand_workflow(SpaceId, AtmInventoryId),
     AtmWorkflowExecutionId = AtmWorkflowExecutionDoc#document.key,
 
     add_link(KrkNode, Phase, AtmWorkflowExecutionDoc),
 
-    ?assertEqual(true, is_member(KrkNode, SpaceId, Phase, AtmWorkflowExecutionId)),
-    ?assertEqual(false, is_member(ParNode, SpaceId, Phase, AtmWorkflowExecutionId)),
+    ?assertEqual(true, is_member(KrkNode, SpaceId, Phase, all, AtmWorkflowExecutionId)),
+    ?assertEqual(true, is_member(KrkNode, SpaceId, Phase, AtmInventoryId, AtmWorkflowExecutionId)),
+    ?assertEqual(false, is_member(KrkNode, SpaceId, Phase, ?DUMMY_ATM_INVENTORY_ID, AtmWorkflowExecutionId)),
+    ?assertEqual(false, is_member(ParNode, SpaceId, Phase, all, AtmWorkflowExecutionId)),
 
     % Assert links are not synchronized between providers
     % (wait some time to give time for potential synchronization)
@@ -172,7 +176,8 @@ delete_links_test_base(Phase) ->
     SpaceId = oct_background:get_space_id(space_krk_par_p),
     KrkNode = oct_background:get_random_provider_node(krakow),
 
-    AtmWorkflowExecutionDoc = gen_rand_workflow(SpaceId),
+    AtmInventoryId = str_utils:rand_hex(32),
+    AtmWorkflowExecutionDoc = gen_rand_workflow(SpaceId, AtmInventoryId),
     AtmWorkflowExecutionId = AtmWorkflowExecutionDoc#document.key,
 
     add_link(KrkNode, Phase, AtmWorkflowExecutionDoc),
@@ -195,7 +200,7 @@ list_with_invalid_listing_opts_test(_Config) ->
     Phase = lists_utils:random_element(?ATM_WORKFLOW_EXECUTIONS_PHASES),
 
     lists:foreach(fun(InvalidListingOpts) ->
-        ?assertEqual(?EINVAL, list_links(KrkNode, SpaceId, Phase, InvalidListingOpts))
+        ?assertEqual(?EINVAL, list_links(KrkNode, SpaceId, Phase, all, InvalidListingOpts))
     end, [
         % Either offset or start_index must be specified
         #{}, #{limit => 10},
@@ -214,88 +219,90 @@ list_with_negative_offset_test(_Config) ->
     Node = oct_background:get_random_provider_node(paris),
     Phase = lists_utils:random_element(?ATM_WORKFLOW_EXECUTIONS_PHASES),
 
-    AllLinks = populate_links(Node, SpaceId, Phase, 30),
-    ?assertEqual(AllLinks, list_links(Node, SpaceId, Phase, #{offset => -10})),
-    ?assertEqual(AllLinks, list_links(Node, SpaceId, Phase, #{offset => -10, limit => 30})),
+    AtmInventoryIds = [str_utils:rand_hex(32), str_utils:rand_hex(32)],
+    AllLinks = populate_links(Node, SpaceId, Phase, AtmInventoryIds, AtmInventoryIds, 30),
+
+    ?assertEqual(AllLinks, list_links(Node, SpaceId, Phase, all, #{offset => -10})),
+    ?assertEqual(AllLinks, list_links(Node, SpaceId, Phase, all, #{offset => -10, limit => 30})),
 
     StartIndex = element(2, lists:nth(20, AllLinks)),
     ExpLinks = lists:sublist(AllLinks, 15, 10),
     ?assertEqual(
         ExpLinks,
-        list_links(Node, SpaceId, Phase, #{start_index => StartIndex, offset => -5, limit => 10})
+        list_links(Node, SpaceId, Phase, all, #{start_index => StartIndex, offset => -5, limit => 10})
     ).
 
 
 iterate_over_100_waiting_atm_workflow_executions_using_offset_and_limit_1_test(_Config) ->
-    iterate_over_atm_workflow_executions_test_base(?WAITING_PHASE, 50, offset, 1).
+    iterate_over_atm_workflow_executions_test_base(?WAITING_PHASE, 200, offset, 1).
 
 
 iterate_over_100_waiting_atm_workflow_executions_using_offset_and_limit_10_test(_Config) ->
-    iterate_over_atm_workflow_executions_test_base(?WAITING_PHASE, 50, offset, 10).
+    iterate_over_atm_workflow_executions_test_base(?WAITING_PHASE, 200, offset, 10).
 
 
 iterate_over_100_waiting_atm_workflow_executions_using_offset_and_limit_100_test(_Config) ->
-    iterate_over_atm_workflow_executions_test_base(?WAITING_PHASE, 50, offset, 100).
+    iterate_over_atm_workflow_executions_test_base(?WAITING_PHASE, 200, offset, 100).
 
 
 iterate_over_100_waiting_atm_workflow_executions_using_start_index_and_limit_1_test(_Config) ->
-    iterate_over_atm_workflow_executions_test_base(?WAITING_PHASE, 50, start_index, 1).
+    iterate_over_atm_workflow_executions_test_base(?WAITING_PHASE, 200, start_index, 1).
 
 
 iterate_over_100_waiting_atm_workflow_executions_using_start_index_and_limit_10_test(_Config) ->
-    iterate_over_atm_workflow_executions_test_base(?WAITING_PHASE, 50, start_index, 10).
+    iterate_over_atm_workflow_executions_test_base(?WAITING_PHASE, 200, start_index, 10).
 
 
 iterate_over_100_waiting_atm_workflow_executions_using_start_index_and_limit_100_test(_Config) ->
-    iterate_over_atm_workflow_executions_test_base(?WAITING_PHASE, 50, start_index, 100).
+    iterate_over_atm_workflow_executions_test_base(?WAITING_PHASE, 200, start_index, 100).
 
 
 iterate_over_100_ongoing_atm_workflow_executions_using_offset_and_limit_1_test(_Config) ->
-    iterate_over_atm_workflow_executions_test_base(?ONGOING_PHASE, 50, offset, 1).
+    iterate_over_atm_workflow_executions_test_base(?ONGOING_PHASE, 200, offset, 1).
 
 
 iterate_over_100_ongoing_atm_workflow_executions_using_offset_and_limit_10_test(_Config) ->
-    iterate_over_atm_workflow_executions_test_base(?ONGOING_PHASE, 50, offset, 10).
+    iterate_over_atm_workflow_executions_test_base(?ONGOING_PHASE, 200, offset, 10).
 
 
 iterate_over_100_ongoing_atm_workflow_executions_using_offset_and_limit_100_test(_Config) ->
-    iterate_over_atm_workflow_executions_test_base(?ONGOING_PHASE, 50, offset, 100).
+    iterate_over_atm_workflow_executions_test_base(?ONGOING_PHASE, 200, offset, 100).
 
 
 iterate_over_100_ongoing_atm_workflow_executions_using_start_index_and_limit_1_test(_Config) ->
-    iterate_over_atm_workflow_executions_test_base(?ONGOING_PHASE, 50, start_index, 1).
+    iterate_over_atm_workflow_executions_test_base(?ONGOING_PHASE, 200, start_index, 1).
 
 
 iterate_over_100_ongoing_atm_workflow_executions_using_start_index_and_limit_10_test(_Config) ->
-    iterate_over_atm_workflow_executions_test_base(?ONGOING_PHASE, 50, start_index, 10).
+    iterate_over_atm_workflow_executions_test_base(?ONGOING_PHASE, 200, start_index, 10).
 
 
 iterate_over_100_ongoing_atm_workflow_executions_using_start_index_and_limit_100_test(_Config) ->
-    iterate_over_atm_workflow_executions_test_base(?ONGOING_PHASE, 50, start_index, 100).
+    iterate_over_atm_workflow_executions_test_base(?ONGOING_PHASE, 200, start_index, 100).
 
 
 iterate_over_100_ended_atm_workflow_executions_using_offset_and_limit_1_test(_Config) ->
-    iterate_over_atm_workflow_executions_test_base(?ENDED_PHASE, 50, offset, 1).
+    iterate_over_atm_workflow_executions_test_base(?ENDED_PHASE, 200, offset, 1).
 
 
 iterate_over_100_ended_atm_workflow_executions_using_offset_and_limit_10_test(_Config) ->
-    iterate_over_atm_workflow_executions_test_base(?ENDED_PHASE, 50, offset, 10).
+    iterate_over_atm_workflow_executions_test_base(?ENDED_PHASE, 200, offset, 10).
 
 
 iterate_over_100_ended_atm_workflow_executions_using_offset_and_limit_100_test(_Config) ->
-    iterate_over_atm_workflow_executions_test_base(?ENDED_PHASE, 50, offset, 100).
+    iterate_over_atm_workflow_executions_test_base(?ENDED_PHASE, 200, offset, 100).
 
 
 iterate_over_100_ended_atm_workflow_executions_using_start_index_and_limit_1_test(_Config) ->
-    iterate_over_atm_workflow_executions_test_base(?ENDED_PHASE, 50, start_index, 1).
+    iterate_over_atm_workflow_executions_test_base(?ENDED_PHASE, 200, start_index, 1).
 
 
 iterate_over_100_ended_atm_workflow_executions_using_start_index_and_limit_10_test(_Config) ->
-    iterate_over_atm_workflow_executions_test_base(?ENDED_PHASE, 50, start_index, 10).
+    iterate_over_atm_workflow_executions_test_base(?ENDED_PHASE, 200, start_index, 10).
 
 
 iterate_over_100_ended_atm_workflow_executions_using_start_index_and_limit_100_test(_Config) ->
-    iterate_over_atm_workflow_executions_test_base(?ENDED_PHASE, 50, start_index, 100).
+    iterate_over_atm_workflow_executions_test_base(?ENDED_PHASE, 200, start_index, 100).
 
 
 %% @private
@@ -303,7 +310,7 @@ iterate_over_100_ended_atm_workflow_executions_using_start_index_and_limit_100_t
     atm_workflow_execution:phase(),
     pos_integer(),
     listing_method(),
-    atm_workflow_executions_collection:limit()
+    atm_workflow_executions_forest:limit()
 ) ->
     ok.
 iterate_over_atm_workflow_executions_test_base(Phase, LinksNum, ListingMethod, Limit) ->
@@ -311,14 +318,21 @@ iterate_over_atm_workflow_executions_test_base(Phase, LinksNum, ListingMethod, L
     SpaceId = str_utils:rand_hex(32),
     Node = oct_background:get_random_provider_node(paris),
 
-    ExpLinks = populate_links(Node, SpaceId, Phase, LinksNum),
+    AllAtmInventoryIds = [str_utils:rand_hex(32) || _ <- lists:seq(1, 5)],
+    ListedAtmInventoryIds = lists_utils:random_element([
+        all,
+        lists_utils:random_element(AllAtmInventoryIds),
+        lists_utils:random_sublist(AllAtmInventoryIds)
+    ]),
+    ExpLinks = populate_links(Node, SpaceId, Phase, AllAtmInventoryIds, ListedAtmInventoryIds, LinksNum),
 
     ListingOpts = case ListingMethod of
         offset -> #{offset => 0, limit => Limit};
         start_index -> #{start_index => <<>>, limit => Limit}
     end,
-    ListedLinks = list_all_links_by_chunk(Node, ListingMethod, SpaceId, Phase, ListingOpts, []),
-
+    ListedLinks = list_all_links_by_chunk(
+        Node, ListingMethod, SpaceId, Phase, ListedAtmInventoryIds, ListingOpts, []
+    ),
     ?assertEqual(ExpLinks, ListedLinks).
 
 
@@ -328,17 +342,18 @@ iterate_over_atm_workflow_executions_test_base(Phase, LinksNum, ListingMethod, L
     listing_method(),
     od_space:id(),
     atm_workflow_execution:phase(),
-    atm_workflow_executions_collection:listing_opts(),
-    [{atm_workflow_execution:id(), atm_workflow_executions_collection:index()}]
+    atm_workflow_executions_forest:tree_ids(),
+    atm_workflow_executions_forest:listing_opts(),
+    [{atm_workflow_execution:id(), atm_workflow_executions_forest:index()}]
 ) ->
-    [{atm_workflow_execution:id(), atm_workflow_executions_collection:index()}].
-list_all_links_by_chunk(Node, ListingMethod, SpaceId, Phase, ListingOpts, LinksAcc) ->
-    case list_links(Node, SpaceId, Phase, ListingOpts) of
+    [{atm_workflow_execution:id(), atm_workflow_executions_forest:index()}].
+list_all_links_by_chunk(Node, ListingMethod, SpaceId, Phase, ListedAtmInventoryIds, ListingOpts, LinksAcc) ->
+    case list_links(Node, SpaceId, Phase, ListedAtmInventoryIds, ListingOpts) of
         [] ->
             LinksAcc;
         ListedLinks ->
             list_all_links_by_chunk(
-                Node, ListingMethod, SpaceId, Phase,
+                Node, ListingMethod, SpaceId, Phase, ListedAtmInventoryIds,
                 update_listing_opts(ListingMethod, ListingOpts, ListedLinks),
                 LinksAcc ++ ListedLinks
             )
@@ -348,10 +363,10 @@ list_all_links_by_chunk(Node, ListingMethod, SpaceId, Phase, ListingOpts, LinksA
 %% @private
 -spec update_listing_opts(
     listing_method(),
-    atm_workflow_executions_collection:listing_opts(),
-    [{atm_workflow_execution:id(), atm_workflow_executions_collection:index()}]
+    atm_workflow_executions_forest:listing_opts(),
+    [{atm_workflow_execution:id(), atm_workflow_executions_forest:index()}]
 ) ->
-    atm_workflow_executions_collection:listing_opts().
+    atm_workflow_executions_forest:listing_opts().
 update_listing_opts(offset, ListingOpts, ListedLinks) ->
     maps:update_with(offset, fun(Offset) -> Offset + length(ListedLinks) end, ListingOpts);
 update_listing_opts(start_index, ListingOpts, ListedLinks) ->
@@ -364,23 +379,46 @@ update_listing_opts(start_index, ListingOpts, ListedLinks) ->
 
 
 %% @private
--spec populate_links(node(), od_space:id(), atm_workflow_execution:phase(), pos_integer()) ->
-    [{atm_workflow_execution:id(), atm_workflow_executions_collection:index()}] .
-populate_links(Node, SpaceId, Phase, LinksNum) ->
-    lists:keysort(2, lists:map(fun(_) ->
-        AtmWorkflowExecutionDoc = gen_rand_workflow(SpaceId),
+-spec populate_links(
+    node(),
+    od_space:id(),
+    atm_workflow_execution:phase(),
+    [od_atm_inventory:id()],
+    atm_workflow_executions_forest:tree_ids(),
+    pos_integer()
+) ->
+    atm_workflow_execution:listing().
+populate_links(Node, SpaceId, Phase, AllAtmInventoryIds, all, LinksNum) ->
+    populate_links(Node, SpaceId, Phase, AllAtmInventoryIds, AllAtmInventoryIds, LinksNum);
+
+populate_links(Node, SpaceId, Phase, AllAtmInventoryIds, ListedAtmInventoryId, LinksNum) when
+    is_binary(ListedAtmInventoryId)
+->
+    populate_links(Node, SpaceId, Phase, AllAtmInventoryIds, [ListedAtmInventoryId], LinksNum);
+
+populate_links(Node, SpaceId, Phase, AllAtmInventoryIds, ListedAtmInventoryIds, LinksNum) ->
+    lists:keysort(2, lists:filtermap(fun(_) ->
+        AtmInventoryId = lists_utils:random_element(AllAtmInventoryIds),
+        AtmWorkflowExecutionDoc = gen_rand_workflow(SpaceId, AtmInventoryId),
         AtmWorkflowExecutionId = AtmWorkflowExecutionDoc#document.key,
         add_link(Node, Phase, AtmWorkflowExecutionDoc),
 
-        {AtmWorkflowExecutionId, index(Phase, AtmWorkflowExecutionDoc)}
+        case lists:member(AtmInventoryId, ListedAtmInventoryIds) of
+            true ->
+                {true, {AtmWorkflowExecutionId, index(Phase, AtmWorkflowExecutionDoc)}};
+            false ->
+                false
+        end
     end, lists:seq(1, LinksNum))).
 
 
 %% @private
--spec gen_rand_workflow(od_space:id()) -> atm_workflow_execution:record().
-gen_rand_workflow(SpaceId) ->
+-spec gen_rand_workflow(od_space:id(), od_atm_inventory:id()) ->
+    atm_workflow_execution:record().
+gen_rand_workflow(SpaceId, AtmInventoryId) ->
     #document{key = str_utils:rand_hex(32), value = #atm_workflow_execution{
         space_id = SpaceId,
+        atm_inventory_id = AtmInventoryId,
         schedule_time = rand:uniform(9999999),
         start_time = rand:uniform(9999999),
         finish_time = rand:uniform(9999999)
@@ -389,7 +427,7 @@ gen_rand_workflow(SpaceId) ->
 
 %% @private
 -spec index(atm_workflow_execution:phase(), atm_workflow_execution:doc()) ->
-    atm_workflow_executions_collection:index().
+    atm_workflow_executions_forest:index().
 index(?WAITING_PHASE, #document{key = AtmWorkflowExecutionId, value = #atm_workflow_execution{
     schedule_time = ScheduleTime
 }}) ->
@@ -428,14 +466,32 @@ delete_link(Node, ?ENDED_PHASE, AtmWorkflowExecutionDoc) ->
 -spec is_member(node(), od_space:id(), atm_workflow_execution:phase(), atm_workflow_execution:id()) ->
     boolean().
 is_member(Node, SpaceId, Phase, AtmWorkflowExecutionId) ->
-    lists:keymember(AtmWorkflowExecutionId, 1, list_all_links(Node, SpaceId, Phase)).
+    is_member(Node, SpaceId, Phase, all, AtmWorkflowExecutionId).
 
 
 %% @private
--spec list_all_links(node(), od_space:id(), atm_workflow_execution:phase()) ->
-    [{atm_workflow_execution:id(), atm_workflow_executions_collection:index()}].
-list_all_links(Node, SpaceId, Phase) ->
-    list_links(Node, SpaceId, Phase, #{offset => 0}).
+-spec is_member(
+    node(),
+    od_space:id(),
+    atm_workflow_execution:phase(),
+    atm_workflow_executions_forest:tree_ids(),
+    atm_workflow_execution:id()
+) ->
+    boolean().
+is_member(Node, SpaceId, Phase, TreeIds, AtmWorkflowExecutionId) ->
+    lists:keymember(AtmWorkflowExecutionId, 1, list_all_links(Node, SpaceId, Phase, TreeIds)).
+
+
+%% @private
+-spec list_all_links(
+    node(),
+    od_space:id(),
+    atm_workflow_execution:phase(),
+    atm_workflow_executions_forest:tree_ids()
+) ->
+    atm_workflow_execution:listing().
+list_all_links(Node, SpaceId, Phase, TreeIds) ->
+    list_links(Node, SpaceId, Phase, TreeIds, #{offset => 0}).
 
 
 %% @private
@@ -443,15 +499,16 @@ list_all_links(Node, SpaceId, Phase) ->
     node(),
     od_space:id(),
     atm_workflow_execution:phase(),
-    atm_workflow_executions_collection:listing_opts()
+    atm_workflow_executions_forest:tree_ids(),
+    atm_workflow_executions_forest:listing_opts()
 ) ->
-    [{atm_workflow_execution:id(), atm_workflow_executions_collection:index()}].
-list_links(Node, SpaceId, ?WAITING_PHASE, ListingOpts) ->
-    rpc:call(Node, atm_waiting_workflow_executions, list, [SpaceId, ListingOpts]);
-list_links(Node, SpaceId, ?ONGOING_PHASE, ListingOpts) ->
-    rpc:call(Node, atm_ongoing_workflow_executions, list, [SpaceId, ListingOpts]);
-list_links(Node, SpaceId, ?ENDED_PHASE, ListingOpts) ->
-    rpc:call(Node, atm_ended_workflow_executions, list, [SpaceId, ListingOpts]).
+    atm_workflow_execution:listing().
+list_links(Node, SpaceId, ?WAITING_PHASE, TreeIds, ListingOpts) ->
+    rpc:call(Node, atm_waiting_workflow_executions, list, [SpaceId, TreeIds, ListingOpts]);
+list_links(Node, SpaceId, ?ONGOING_PHASE, TreeIds, ListingOpts) ->
+    rpc:call(Node, atm_ongoing_workflow_executions, list, [SpaceId, TreeIds, ListingOpts]);
+list_links(Node, SpaceId, ?ENDED_PHASE, TreeIds, ListingOpts) ->
+    rpc:call(Node, atm_ended_workflow_executions, list, [SpaceId, TreeIds, ListingOpts]).
 
 
 %===================================================================
@@ -468,14 +525,6 @@ init_per_suite(Config) ->
 
 end_per_suite(_Config) ->
     oct_background:end_per_suite().
-
-
-init_per_group(_Group, Config) ->
-    lfm_proxy:init(Config, false).
-
-
-end_per_group(_Group, Config) ->
-    lfm_proxy:teardown(Config).
 
 
 init_per_testcase(_Case, Config) ->
