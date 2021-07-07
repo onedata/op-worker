70f3ac3292d0bde153fc1cd99aa8f499315032be
Michał Stanisz <mch.stanisz@gmail.com>
2021-07-07 11:15:05 +0200

    VFS-7780 Fixed tests

--- include/modules/dataset/archive.hrl
+++ include/modules/dataset/archive.hrl
@@ -34,7 +34,7 @@
 
 -define(DEFAULT_LAYOUT, ?ARCHIVE_PLAIN_LAYOUT).
 -define(DEFAULT_INCLUDE_DIP, false).
--define(DEFAULT_INCREMENTAL, #{<<"enable">> => false}).
+-define(DEFAULT_INCREMENTAL, #{<<"enabled">> => false}).
 -define(DEFAULT_BASE_ARCHIVE, null).
 -define(DEFAULT_ARCHIVE_DESCRIPTION, <<>>).
 -define(DEFAULT_CREATE_NESTED_ARCHIVES, false).
