cb36bd8be1d8b994f182f09a68d5802474c3e7b7
Michal Wrzeszcz <michalwrzeszcz@gmail.com>
2022-07-22 11:03:25 +0200

    VFS-9656 Fix transfer statistics flush

--- src/modules/fslogic/replication/replica_synchronizer.erl
+++ src/modules/fslogic/replication/replica_synchronizer.erl
@@ -1558,13 +1558,10 @@ flush_blocks_list(AllBlocks, ExcludeSessions, Flush) ->
 %% @end
 %%--------------------------------------------------------------------
 -spec flush_stats(#state{}, boolean()) -> #state{}.
-flush_stats(#state{cached_stats = Stats} = State, CancelTimer) when map_size(Stats) == 0 ->
-    case CancelTimer of
-        true ->
-            cancel_caching_stats_timer(State);
-        false ->
-            State
-    end;
+flush_stats(#state{cached_stats = Stats} = State, true = _CancelTimer) when map_size(Stats) == 0 ->
+    cancel_caching_stats_timer(State);
+flush_stats(#state{cached_stats = Stats} = State, false = _CancelTimer) when map_size(Stats) == 0 ->
+    State;
 flush_stats(#state{
     space_id = SpaceId,
     transfer_id_to_stats_callback_module = TidToStatsCallback
