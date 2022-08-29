c13459b36d1f299ffba9304c4224bb8e7e6c1591
Bartosz Walkowicz <wallko1@hotmail.com>
2022-08-29 13:09:51 +0200

    VFS-9671 Make atm_openfaas_monitor a gen_server

--- src/modules/automation/task/executor/platforms/openfaas/atm_openfaas_monitor.erl
+++ src/modules/automation/task/executor/platforms/openfaas/atm_openfaas_monitor.erl
@@ -12,16 +12,50 @@
 -module(atm_openfaas_monitor).
 -author("Bartosz Walkowicz").
 
+-behaviour(gen_server).
+
 -include("modules/automation/atm_execution.hrl").
 -include_lib("ctool/include/http/codes.hrl").
 
 %% API
+-export([spec/0, whereis/0, start_link/0]).
 -export([is_openfaas_available/0, assert_openfaas_available/0]).
+
+%% gen_server callbacks
+-export([
+    init/1,
+    handle_call/3, handle_cast/2, handle_info/2,
+    terminate/2, code_change/3
+]).
+
+
+-define(SERVER, {global, ?MODULE}).
 
 
 %%%===================================================================
 %%% API
 %%%===================================================================
+
+
+-spec spec() -> supervisor:child_spec().
+spec() -> #{
+    id => ?MODULE,
+    start => {?MODULE, start_link, []},
+    restart => permanent,
+    shutdown => timer:seconds(10),
+    type => worker,
+    modules => [?MODULE]
+}.
+
+
+-spec whereis() -> pid() | undefined.
+whereis() ->
+    global:whereis_name(?MODULE).
+
+
+-spec start_link() -> {ok, pid()} | {error, term()}.
+start_link() ->
+    gen_server:start_link(?SERVER, ?MODULE, [], []).
 
 
 -spec is_openfaas_available() -> boolean().
@@ -40,6 +74,92 @@ assert_openfaas_available() ->
         unreachable -> throw(?ERROR_ATM_OPENFAAS_UNREACHABLE);
         _ -> throw(?ERROR_ATM_OPENFAAS_NOT_CONFIGURED)
     end.
+
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server.
+%% @end
+%%--------------------------------------------------------------------
+-spec init(Args :: term()) -> {ok, state(), hibernate}.
+init(_) ->
+    process_flag(trap_exit, true),
+    {ok, undefined, hibernate}.
+
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handles call messages
+%% @end
+%%--------------------------------------------------------------------
+-spec handle_call(Request :: term(), From :: {pid(), Tag :: term()}, state()) ->
+    {reply, Reply :: term(), NewState :: state()} |
+    {reply, Reply :: term(), NewState :: state(), hibernate}.
+handle_call(Request, _From, State) ->
+    ?log_bad_request(Request),
+    {reply, {error, wrong_request}, State, hibernate}.
+
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handles cast messages
+%% @end
+%%--------------------------------------------------------------------
+-spec handle_cast(Request :: term(), state()) ->
+    {noreply, NewState :: state()} |
+    {noreply, NewState :: state(), hibernate}.
+handle_cast(Request, State) ->
+    ?log_bad_request(Request),
+    {noreply, State, hibernate}.
+
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handles all non call/cast messages
+%% @end
+%%--------------------------------------------------------------------
+-spec handle_info(Info :: term(), state()) ->
+    {noreply, NewState :: state()} |
+    {noreply, NewState :: state(), hibernate}.
+handle_info(Info, State) ->
+    ?log_bad_request(Info),
+    {noreply, State, hibernate}.
+
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%% @end
+%%--------------------------------------------------------------------
+-spec terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
+    state()) -> term().
+terminate(_Reason, _State) ->
+    ok.
+
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Converts process state when code is changed
+%% @end
+%%--------------------------------------------------------------------
+-spec code_change(OldVsn :: term() | {down, term()}, state(), Extra :: term()) ->
+    {ok, NewState :: state()} | error().
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
 
 
 %%%===================================================================
