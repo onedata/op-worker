76bd98f45ca03273eac69ea04871fb26fc3aff42
Bartosz Walkowicz <wallko1@hotmail.com>
2022-09-29 13:38:08 +0200

    VFS-9453 Add pause_failed_atm_workflow_execution test

--- test_distributed/suites/automation/execution/atm_workflow_execution_pause_test_base.erl
+++ test_distributed/suites/automation/execution/atm_workflow_execution_pause_test_base.erl
@@ -20,7 +20,8 @@
     pause_scheduled_atm_workflow_execution/0,
 
     pause_finishing_atm_workflow_execution/0,
-    pause_finished_atm_workflow_execution/0
+    pause_finished_atm_workflow_execution/0,
+    pause_failed_atm_workflow_execution/0
 ]).
 
 
@@ -197,6 +198,57 @@ pause_finished_atm_workflow_execution() ->
                 after_step_exp_state_diff = fun(#atm_mock_call_ctx{workflow_execution_exp_state = ExpState0}) ->
                     ExpState1 = expect_lane_runs_rerunable([{1, 1}, {2, 1}], ExpState0),
                     {true, atm_workflow_execution_exp_state_builder:expect_workflow_execution_finished(ExpState1)}
+                end
+            },
+            after_hook = fun assert_ended_atm_workflow_execution_can_not_be_paused/1
+        }]
+    }).
+
+
+pause_failed_atm_workflow_execution() ->
+    atm_workflow_execution_test_runner:run(#atm_workflow_execution_test_spec{
+        provider = ?PROVIDER_SELECTOR,
+        user = ?USER_SELECTOR,
+        space = ?SPACE_SELECTOR,
+        workflow_schema_dump_or_draft = ?ECHO_ATM_WORKFLOW_SCHEMA_DRAFT,
+        workflow_schema_revision_num = 1,
+        incarnations = [#atm_workflow_execution_incarnation_test_spec{
+            incarnation_num = 1,
+            lane_runs = [
+                #atm_lane_run_execution_test_spec{
+                    selector = {1, 1},
+                    prepare_lane = #atm_step_mock_spec{
+                        % Due to lane preparation failure 'handle_lane_execution_stopped' was called
+                        % from within lane preparation
+                        after_step_exp_state_diff = no_diff
+                    },
+                    create_run = #atm_step_mock_spec{
+                        % 'create_run' step execution is mocked entirely so that
+                        % no lane run execution component will be created
+                        strategy = {yield, {throw, ?ERROR_INTERNAL_SERVER_ERROR}}
+                    },
+                    handle_lane_execution_stopped = #atm_step_mock_spec{
+                        before_step_exp_state_diff = fun(#atm_mock_call_ctx{workflow_execution_exp_state = ExpState0}) ->
+                            ExpState1 = atm_workflow_execution_exp_state_builder:expect_lane_run_stopping({1, 1}, ExpState0),
+                            {true, atm_workflow_execution_exp_state_builder:expect_workflow_execution_stopping(ExpState1)}
+                        end,
+                        after_step_exp_state_diff = fun(#atm_mock_call_ctx{workflow_execution_exp_state = ExpState}) ->
+                            {true, atm_workflow_execution_exp_state_builder:expect_lane_run_failed({1, 1}, ExpState)}
+                        end
+                    }
+                },
+                #atm_lane_run_execution_test_spec{
+                    selector = {2, 1},
+                    prepare_lane = #atm_step_mock_spec{
+                        defer_after = {prepare_lane, after_step, {1, 1}},
+                        after_step_exp_state_diff = no_diff
+                    }
+                }
+            ],
+            handle_workflow_execution_stopped = #atm_step_mock_spec{
+                after_step_exp_state_diff = fun(#atm_mock_call_ctx{workflow_execution_exp_state = ExpState0}) ->
+                    ExpState1 = atm_workflow_execution_exp_state_builder:expect_lane_run_rerunable({1, 1}, ExpState0),
+                    {true, atm_workflow_execution_exp_state_builder:expect_workflow_execution_failed(ExpState1)}
                 end
             },
             after_hook = fun assert_ended_atm_workflow_execution_can_not_be_paused/1
