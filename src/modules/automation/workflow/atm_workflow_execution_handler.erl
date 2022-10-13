be50eac223fe2f6f75a5a512693385b2b1f1a351
Bartosz Walkowicz <wallko1@hotmail.com>
2022-10-13 14:57:54 +0200

    VFS-9916 Add interrupt_reason() to workflow_engine:abandon

--- src/modules/automation/workflow/atm_workflow_execution_handler.erl
+++ src/modules/automation/workflow/atm_workflow_execution_handler.erl
@@ -238,7 +238,7 @@ on_openfaas_down(AtmWorkflowExecutionId, Error) ->
         },
         atm_workflow_execution_logger:workflow_critical(LogContent, Logger),
 
-        workflow_engine:abandon(AtmWorkflowExecutionId),
+        workflow_engine:abandon(AtmWorkflowExecutionId, interrupt),
         atm_lane_execution_handler:stop({current, current}, interrupt, AtmWorkflowExecutionCtx),
         end_workflow_execution(AtmWorkflowExecutionId, AtmWorkflowExecutionCtx)
     catch throw:{session_acquisition_failed, _} = Reason ->
