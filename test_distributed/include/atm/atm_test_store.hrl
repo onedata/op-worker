9fd51378c16b12a9d8b6e1ae2bf1a55ba4eef407
Michal Wrzeszcz <michalwrzeszcz@gmail.com>
2022-12-15 23:49:38 +0100

    VFS-9589 Update deps

--- test_distributed/include/atm/atm_test_store.hrl
+++ test_distributed/include/atm/atm_test_store.hrl
@@ -35,8 +35,8 @@
 -define(EXP_WINDOW(__METRIC_CONFIG, __TIMESTAMP, __VALUE), #{
     <<"value">> => __VALUE,
     <<"timestamp">> => __TIMESTAMP - __TIMESTAMP rem __METRIC_CONFIG#metric_config.resolution,
-    <<"firstMeasurementTimestamp">> => undefined,
-    <<"lastMeasurementTimestamp">> => undefined
+    <<"firstMeasurementTimestamp">> => null,
+    <<"lastMeasurementTimestamp">> => null
 }).
 
 -define(MAX_FILE_SIZE_TS_NAME, <<"max_file_size">>).
