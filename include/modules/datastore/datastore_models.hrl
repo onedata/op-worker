d1413763f8a4702fd9ea285666f0eb902568e1c8
Bartosz Walkowicz <wallko1@hotmail.com>
2024-07-18 20:30:51 +0200

    VFS-12000 Serve group eff users

--- include/modules/datastore/datastore_models.hrl
+++ include/modules/datastore/datastore_models.hrl
@@ -104,6 +104,8 @@
     name :: undefined | binary(),
     type = role :: od_group:type(),
 
+    eff_users = #{} :: #{od_user:id() => [privileges:space_privilege()]},
+
     cache_state = #{} :: cache_state()
 }).
 
