410322ac579914321e4ea509995949189cf2a268
Bartosz Walkowicz <wallko1@hotmail.com>
2024-08-02 13:48:08 +0200

    VFS-12062 Mock storage_file_ctx in s3 import onenv tests

--- test_distributed/suites/storage/import/storage_import_s3_oct_test_SUITE.erl
+++ test_distributed/suites/storage/import/storage_import_s3_oct_test_SUITE.erl
@@ -29,12 +29,24 @@
 ]).
 
 all() -> [
-    import_empty_storage_test
-%%    TODO VFS-12062 works alone, but after another test or with
-%%    --no_clean option value of <<"modified">> = 1 when expected is 0
-%%    import_empty_file_test,
-%%    import_file_with_content_test
+    import_empty_storage_test,
+    import_empty_file_test,
+    import_file_with_content_test
 ].
+
+-define(IMPORTING_PROVIDER_SELECTOR, krakow).
+
+-define(MOCK_SPACE_DIR_STATBUF, #statbuf{
+    st_uid = ?ROOT_UID,
+    st_gid = ?ROOT_GID,
+    st_mode = ?DEFAULT_DIR_PERMS bor 8#40000,
+    % Return min times to ensure space root dir will not be considered
+    % as modified by storage import
+    st_mtime = 1,
+    st_atime = 1,
+    st_ctime = 1,
+    st_size = 0
+}).
 
 -define(SUITE_CTX, #storage_import_test_suite_ctx{
     storage_type = s3,
@@ -78,12 +90,29 @@ init_per_suite(Config) ->
         ]}],
         posthook = fun(NewConfig) ->
             storage_import_oct_test_base:clean_up_after_previous_run(all(), ?SUITE_CTX),
+
+            Nodes = oct_background:get_provider_nodes(?IMPORTING_PROVIDER_SELECTOR),
+            ok = test_utils:mock_new(Nodes, storage_file_ctx),
+            ok = test_utils:mock_expect(Nodes, storage_file_ctx, new_with_stat,
+                fun
+                    (StorageFileId = <<"/">>, SpaceId, StorageId, _Stat) ->
+                        storage_file_ctx:new_with_stat(
+                            StorageFileId, <<>>, SpaceId, StorageId, ?MOCK_SPACE_DIR_STATBUF
+                        );
+                    (StorageFileId, SpaceId, StorageId, Stat) ->
+                        meck:passthrough([StorageFileId, SpaceId, StorageId, Stat])
+                end
+            ),
+
             NewConfig
         end
     }).
 
 
 end_per_suite(_Config) ->
+    Nodes = oct_background:get_provider_nodes(?IMPORTING_PROVIDER_SELECTOR),
+    ok = test_utils:mock_unload(Nodes, storage_file_ctx),
+
     oct_background:end_per_suite().
 
 
