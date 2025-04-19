b1ebea5c53edaf28c13c1764fe77b7041d6b3730
Michał Stanisz <mch.stanisz@gmail.com>
2025-04-19 16:42:08 +0200

    VFS-12693 Improvements according to code review

--- src/logic/space_logic.erl
+++ src/logic/space_logic.erl
@@ -221,10 +221,14 @@ has_eff_group(#document{value = #od_space{eff_groups = EffGroups}}, GroupId) ->
 -spec get_shares(gs_client_worker:client(), od_space:id()) ->
     {ok, [od_share:id()]} | errors:error().
 get_shares(SessionId, SpaceId) ->
-    gs_client_worker:request(SessionId, #gs_req_graph{
-        operation = create,
-        gri = #gri{type = od_space, id = SpaceId, aspect = list_shares, scope = private}
-    }).
+    % fetching new space doc is always required, as due to a bug Onezone does not send updates when share list changes
+    od_space:invalidate_cache(SpaceId),
+    case get(SessionId, SpaceId) of
+        {ok, #document{value = #od_space{shares = Shares}}} ->
+            {ok, Shares};
+        {error, _} = Error ->
+            Error
+    end.
 
 %%-------------------------------------------------------------------
 %% @doc
