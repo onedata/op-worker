61e6433bc36da3bf7fa722ae36776d2d934da8ee
Bartosz Walkowicz <16596719+bwalkowi@users.noreply.github.com>
2025-07-18 16:14:55 +0200

    VFS-12865 Add oc env vars to openfaas lambda

--- include/http/space_file_events_stream.hrl
+++ include/http/space_file_events_stream.hrl
@@ -21,14 +21,17 @@
 -include_lib("ctool/include/http/headers.hrl").
 
 
+% Name belongs to ?LINK_TREE_FILE_ATTRS attr group BUT the link docs are only checked
+% when either:
+% - other attrs from that group (file name conflicts related ones) are specified
+% - `resolve_name_conflicts` attr resolution policy is specified when resolving attrs
+% In case of space file events this is not happening and getting name attr just gets it
+% from file_meta doc
+-define(OBSERVABLE_FILE_META_ATTRS, [?attr_name | ?FILE_META_ATTRS]).
+
 -define(OBSERVABLE_FILE_ATTRS,
     lists:flatten([
-        % Name belongs to ?LINK_TREE_FILE_ATTRS attr group BUT it is only relevant for other
-        % attrs in this group (file name conflicts related attrs) - if only name is no link
-        % docs are consulted (and as such we do not need to monitor them - file_meta change
-        % trigger name change event)
-        ?attr_name,
-        ?FILE_META_ATTRS, ?TIMES_FILE_ATTRS, ?LOCATION_FILE_ATTRS
+        ?OBSERVABLE_FILE_META_ATTRS, ?TIMES_FILE_ATTRS, ?LOCATION_FILE_ATTRS
     ]) -- ?INTERNAL_FILE_ATTRS
 ).
 
