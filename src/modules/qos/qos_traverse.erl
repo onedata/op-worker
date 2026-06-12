6c64b86e34598ad787b876e6466c566d3c7876b4
Michał Stanisz <mch.stanisz@gmail.com>
2026-06-12 09:13:41 +0200

    VFS-13515 Improvements according to code review

--- src/modules/qos/qos_traverse.erl
+++ src/modules/qos/qos_traverse.erl
@@ -368,10 +368,7 @@ report_file_failed_for_entries(QosEntries, FileCtx, Error) ->
         true -> 
             Error;
         false ->
-            ?report_internal_server_error(?autoformat_with_msg(
-                "Unexpected error during QoS traverse",
-                [Error]
-            ))
+            ?ERR_INTERNAL_SERVER_ERROR(?err_ctx(), str_utils:format("~tp", [Error]))
     end,
     report_to_audit_log(
         QosEntries, FileCtx, [FinalError], fun qos_entry_audit_log:report_file_synchronization_failed/4).
