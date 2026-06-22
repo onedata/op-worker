738f96486a4d9bbf348f3f6d37d9d317e56d77c1
Łukasz Opioła <ymopiola@cyfronet.pl>
2026-06-22 22:57:15 +0200

    VFS-2073 Code polishing according to PR

--- src/http/utils/download/http_download_utils.erl
+++ src/http/utils/download/http_download_utils.erl
@@ -39,13 +39,22 @@ allow_onezone_as_frame_ancestor(Req) ->
 
 -spec set_file_download_headers(cowboy_req:req(), file_meta:name()) ->
     cowboy_req:req().
-set_file_download_headers(Req, FileName) ->
-    {Type, SubType, _Params} = cow_mimetypes:all(FileName),
+set_file_download_headers(Req0, FileName) ->
+    % honour preexisting CT header if set beforehand
+    Req1 = case cowboy_req:resp_header(?HDR_CONTENT_TYPE, Req0, undefined) of
+        undefined ->
+            {Type, Subtype, _Params} = cow_mimetypes:all(FileName),
+            cowboy_req:set_resp_header(?HDR_CONTENT_TYPE, [Type, "/", Subtype], Req0);
+        _ ->
+            Req0
+    end,
+
     RFC5987Encoded = rfc5987:encode_filename(FileName),
-    cowboy_req:set_resp_headers(#{
-        ?HDR_CONTENT_TYPE => <<Type/binary, "/", SubType/binary>>,
-        ?HDR_CONTENT_DISPOSITION => <<"attachment; filename*=UTF-8''", RFC5987Encoded/binary>>
-    }, Req).
+    cowboy_req:set_resp_header(
+        ?HDR_CONTENT_DISPOSITION,
+        <<"attachment; filename*=UTF-8''", RFC5987Encoded/binary>>,
+        Req1
+    ).
 
 
 %%--------------------------------------------------------------------
