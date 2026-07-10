25a0f2b9de69c69a27846adafa578bc311cc6bc2
Bartosz Walkowicz <16596719+bwalkowi@users.noreply.github.com>
2026-07-10 17:26:59 +0200

    VFS-13687 Remove obsolete storage import envup tests

--- test_distributed/storage_import_update_s3_test_SUITE.erl
+++ test_distributed/storage_import_update_s3_test_SUITE.erl
@@ -26,38 +26,14 @@
 %% tests
 -export([
     sync_should_not_reimport_deleted_but_still_opened_file/1,
-    sync_should_not_import_recreated_file_with_suffix_on_storage/1,
-    sync_should_update_blocks_of_recreated_file_with_suffix_on_storage/1,
-    sync_should_not_import_replicated_file_with_suffix_on_storage/1,
-    sync_should_update_replicated_file_with_suffix_on_storage/1,
     create_delete_import2_test/1,
-    create_subfiles_and_delete_before_import_is_finished_test/1,
-    file_with_data_protection_should_not_be_updated_test/1,
-    file_with_data_and_metadata_protection_should_not_be_updated_test/1,
-    file_with_data_protection_should_not_be_deleted_test/1,
-    file_with_data_and_metadata_protection_should_not_be_deleted_test/1,
-    dir_and_its_child_with_data_protection_should_not_be_updated_test/1,
-    dir_and_its_child_with_data_and_metadata_protection_should_not_be_updated_test/1,
-    dir_and_its_child_with_data_protection_should_not_be_deleted_test/1,
-    dir_and_its_child_with_data_and_metadata_protection_should_not_be_deleted_test/1
+    create_subfiles_and_delete_before_import_is_finished_test/1
 ]).
 
 -define(TEST_CASES, [
     sync_should_not_reimport_deleted_but_still_opened_file,
-    sync_should_not_import_recreated_file_with_suffix_on_storage,
-    sync_should_update_blocks_of_recreated_file_with_suffix_on_storage,
-    sync_should_not_import_replicated_file_with_suffix_on_storage,
-    sync_should_update_replicated_file_with_suffix_on_storage,
     create_delete_import2_test,
-    create_subfiles_and_delete_before_import_is_finished_test,
-    file_with_data_protection_should_not_be_updated_test,
-    file_with_data_and_metadata_protection_should_not_be_updated_test,
-    file_with_data_protection_should_not_be_deleted_test,
-    file_with_data_and_metadata_protection_should_not_be_deleted_test,
-    dir_and_its_child_with_data_protection_should_not_be_updated_test,
-    dir_and_its_child_with_data_and_metadata_protection_should_not_be_updated_test,
-    dir_and_its_child_with_data_protection_should_not_be_deleted_test,
-    dir_and_its_child_with_data_and_metadata_protection_should_not_be_deleted_test
+    create_subfiles_and_delete_before_import_is_finished_test
 ]).
 
 all() -> ?ALL(?TEST_CASES).
@@ -69,47 +45,11 @@ all() -> ?ALL(?TEST_CASES).
 sync_should_not_reimport_deleted_but_still_opened_file(Config) ->
     storage_import_s3_test_base:sync_should_not_reimport_deleted_but_still_opened_file(Config, ?S3_HELPER_NAME).
 
-sync_should_not_import_recreated_file_with_suffix_on_storage(Config) ->
-    storage_import_test_base:sync_should_not_import_recreated_file_with_suffix_on_storage(Config, ?S3_HELPER_NAME).
-
-sync_should_update_blocks_of_recreated_file_with_suffix_on_storage(Config) ->
-    storage_import_test_base:sync_should_update_blocks_of_recreated_file_with_suffix_on_storage(Config, ?S3_HELPER_NAME).
-
-sync_should_not_import_replicated_file_with_suffix_on_storage(Config) ->
-    storage_import_test_base:sync_should_not_import_replicated_file_with_suffix_on_storage(Config, ?S3_HELPER_NAME).
-
-sync_should_update_replicated_file_with_suffix_on_storage(Config) ->
-    storage_import_test_base:sync_should_update_replicated_file_with_suffix_on_storage(Config, ?S3_HELPER_NAME).
-
 create_delete_import2_test(Config) ->
     storage_import_test_base:create_delete_import2_test(Config).
 
 create_subfiles_and_delete_before_import_is_finished_test(Config) ->
     storage_import_s3_test_base:create_subfiles_and_delete_before_import_is_finished_test(Config).
-
-file_with_data_protection_should_not_be_updated_test(Config) ->
-    storage_import_test_base:file_with_data_protection_should_not_be_updated_test(Config, ?S3_HELPER_NAME).
-
-file_with_data_and_metadata_protection_should_not_be_updated_test(Config) ->
-    storage_import_test_base:file_with_data_and_metadata_protection_should_not_be_updated_test(Config, ?S3_HELPER_NAME).
-
-file_with_data_protection_should_not_be_deleted_test(Config) ->
-    storage_import_test_base:file_with_data_protection_should_not_be_deleted_test(Config, ?S3_HELPER_NAME).
-
-file_with_data_and_metadata_protection_should_not_be_deleted_test(Config) ->
-    storage_import_test_base:file_with_data_and_metadata_protection_should_not_be_deleted_test(Config, ?S3_HELPER_NAME).
-
-dir_and_its_child_with_data_protection_should_not_be_updated_test(Config) ->
-    storage_import_test_base:dir_and_its_child_with_data_protection_should_not_be_updated_test(Config, ?S3_HELPER_NAME).
-
-dir_and_its_child_with_data_and_metadata_protection_should_not_be_updated_test(Config) ->
-    storage_import_test_base:dir_and_its_child_with_data_and_metadata_protection_should_not_be_updated_test(Config, ?S3_HELPER_NAME).
-
-dir_and_its_child_with_data_protection_should_not_be_deleted_test(Config) ->
-    storage_import_test_base:dir_and_its_child_with_data_protection_should_not_be_deleted_test(Config).
-
-dir_and_its_child_with_data_and_metadata_protection_should_not_be_deleted_test(Config) ->
-    storage_import_test_base:dir_and_its_child_with_data_and_metadata_protection_should_not_be_deleted_test(Config).
 
 %===================================================================
 % SetUp and TearDown functions
