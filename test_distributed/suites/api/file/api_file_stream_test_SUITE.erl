ff1be0ba96e8233cfb860d1b9549df4be5742a40
Bartosz Walkowicz <16596719+bwalkowi@users.noreply.github.com>
2026-09-22 15:24:31 +0200

    VFS-13551 Increase perf testcases timetrap

--- test_distributed/suites/api/file/api_file_stream_test_SUITE.erl
+++ test_distributed/suites/api/file/api_file_stream_test_SUITE.erl
@@ -1781,7 +1781,13 @@ init_per_testcase(bulk_download_dir_retry_teardown_test = Case, Config) ->
     end, ProviderNodes),
     init_per_testcase(?DEFAULT_CASE(Case), Config);
 init_per_testcase(_Case, Config) ->
-    ct:timetrap({minutes, 40}),
+    GroupProperties = proplists:get_value(tc_group_properties, Config, []),
+    ct:timetrap(case proplists:get_value(name, GroupProperties) of
+        % performance configurations operate on much larger file trees, which
+        % setup_fun recreates for every tested combination
+        performance_tests -> {hours, 2};
+        _ -> {minutes, 40}
+    end),
     Config.
 
 end_per_testcase(sync_first_file_block_test = Case, Config) ->
